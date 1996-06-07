f082b15eb404831c6bd3ded4f2753b761774ef0e
Jurgen Vinju <Jurgen.Vinju@cwi.nl>
1996-06-07 11:05:48 +0000

    - python-adapter: added some functionality and removed some bugs   -> The python-adapter is now fully functional! - Worked on the python-adapter section of the guide. - Fixed the tcltk-adapter picture in the guide (it was rotated by 90 degr.)

--- toolbus/demos/hello/hello.py
+++ toolbus/demos/hello/hello.py
@@ -10,6 +10,6 @@ def get_text(cid):
 	return TB.make("snd-value(<str>)", 
            "Hello World, my first ToolBus tool in Python!\n")
 
-cid = TB.parseArgs(sys.argv, "__main__")
+cid = TB.parseArgs(sys.argv, __main__)
 TB.connect(cid)
 TB.eventloop()
