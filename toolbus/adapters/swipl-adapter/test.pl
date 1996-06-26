eaa18250fcb6c1485f732c18f0277d5aca33780b
Jurgen Vinju <Jurgen.Vinju@cwi.nl>
1996-06-26 11:32:41 +0000

    Added the new swipl-adapter.

---
+++ toolbus/adapters/swipl-adapter/test.pl
@@ -0,0 +1,22 @@
+
+
+abc(a).
+abc(b).
+abc(c).
+
+xyz(abc(a),abc(c)).
+
+just_a_test(CID, 'Hello world!') :- print('just_a_test: '), print(CID), print('\n').
+rec_terminate(CID,X) :- print(CID), print('\n'), print(X), print('\n'), halt.
+
+gen_event(CID, Ev) :- print(['sending event: ',Ev,'\n']), tb_send(CID, snd_event(evt(Ev))).
+
+rec_ack_event(_, X) :- print('rec-ack-event: '), print(X), print('\n').
+
+run :-  tb_tool(Tool),
+	tb_host(Host),
+	tb_port(Port),
+	tb_tid(Tid),
+	tb_new_connection(Tool,Host,Port,Tid,Cid),
+	tb_connect(Cid),
+	tb_eventloop.
