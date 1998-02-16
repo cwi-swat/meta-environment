5e50e0ff0f3b8fa45636373a309c9c78ea626753
Mark van den Brand <M.G.J.v.d.Brand@tue.nl>
1998-02-16 08:13:48 +0000

    This file is new with respect to the previous version.

---
+++ asf/semantics/asfc/c-source/AUX-MuASF-Rules2C18.c
@@ -0,0 +1,10 @@
+#include "support.h"
+void register_AUX_MuASF_Rules2C18( ) {
+arena local;
+TinitArena( NULL , & local);
+}
+void resolve_AUX_MuASF_Rules2C18( ) {
+arena local;
+TinitArena( NULL , & local);
+}
+
