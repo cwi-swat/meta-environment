bf77b5d17bbeab259a569011dc25a63a18cd67fb
Jeroen Scheerder <js@xs4all.nl>
1998-10-06 14:50:25 +0000

    *** empty log message ***

--- sglr/sglr/mem-alloc.h
+++ sglr/sglr/mem-alloc.h
@@ -3,19 +3,19 @@
 #ifndef _MEM_ALLOC
 #define _MEM_ALLOC
 
-#ifdef DONT_USE_BOEHMGC
-  #include <stdlib.h>
-  #define SG_Calloc(m,n)  calloc(m,n)
-  #define SG_Malloc(n)    malloc(n)
-  #define SG_Realloc(o,s) realloc(o,s)
-  #define SG_free(o)      free(o)
-#else
+#ifdef HAVE_BOEHMGC
   #include <gc.h>
   #define SG_Calloc(m,n)  GC_calloc(m,n)
   #define SG_Malloc(n)    GC_malloc(n)
   #define SG_Realloc(o,s) GC_realloc(o,s)
   #define SG_free(o)      GC_free(o)
 //#define free(n)    ;
+#else
+  #include <stdlib.h>
+  #define SG_Calloc(m,n)  calloc(m,n)
+  #define SG_Malloc(n)    malloc(n)
+  #define SG_Realloc(o,s) realloc(o,s)
+  #define SG_free(o)      free(o)
 #endif
 
 #endif
