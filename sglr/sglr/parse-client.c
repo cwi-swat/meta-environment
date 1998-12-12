6e8ce7857e83cc198b423a34fd293dd8402a5699
Jeroen Scheerder <js@xs4all.nl>
1998-12-12 18:46:27 +0000

    Tidied up the interface and error handling of parse-client, making it more flexible as well as more robust -- or rather, less error-prone.
    js:q6@medusa[409]> parse-client --help        ~/petr/new-meta/sglr/client 19:44
    Usage: parse-client -T toolname -i input-file -p parse-table [-o file]  \
           [-H hostname] [-P port] [-s sort]
    
            -h|--help              : usage information
            -i|--input        file : input from |file|                   (*)
            -p|--parse-table  file : use parse table |file|              (*)
            -s|--start-symbol sort : parse over sort |sort|
            -H|--host         host : parse server runs on |host|
               --TB_HOST      host : ditto  (default: local host)
            -P|--port         port : parse server runs on |port|
               --TB_PORT      port : ditto  (default: 8999)
            -T|--tool         tool : parse server toolname is |tool|
               --TB_TOOL_NAME tool : ditto  (default: `parse-client')
            -o|--output       file : output to |file| (default: stdout)
    
    	Items marked with (*) are mandatory; all others are optional.

--- sglr/sglr/parse-client.c
+++ sglr/sglr/parse-client.c
@@ -11,17 +11,20 @@
 
 #include "parse-client.tif.c"
 
-char  *program_name   = "parse-client";
+#define  program_name  "parse-client"
 
 
 /* This is a hack, due to JS */
 
 #include <sys/stat.h>
 
-char  *input_file_name  = "-";
+char  *input_file_name  = NULL;
 char  *output_file_name = "-";
 char  *start_symbol     = NULL;
 char  *parse_table_name = NULL;
+char  *hostname         = NULL;
+char  *port             = NULL;
+char  *tool             = program_name;
 
 size_t  FileSize(char *fnam)
 {
@@ -65,6 +68,12 @@ char *ReadFile(char *fnam, size_t *size)
 void WriteFile(char *fnam, ATerm tree)
 {
   FILE *fd;
+
+  if(!strcmp("-", fnam)) {
+    ATwriteToBinaryFile(tree,stdout);
+    return;
+  }
+
   if((fd = fopen(fnam, "w")) == NULL) {
     fprintf(stderr, "could not fopen() %s\n", fnam);
   }
@@ -76,30 +85,26 @@ void WriteFile(char *fnam, ATerm tree)
 
 void rec_ack_event(int cid, ATerm t) {
   ATerm tree, message;
-  ATerm t1,t2,t3;
   char *buf;
   size_t size;
 
   if(ATmatch(t,"open-language(<str>,<str>)",NULL,NULL)) { 
-    buf = ReadFile(input_file_name,&size);
-    if (buf != NULL) {
-      t1 = ATmake("<str>", parse_table_name);
-      t2 = ATmake("<str>", start_symbol);
-      t3 = ATmake("<str>", buf);
-      free(buf); 
-      ATBwriteTerm(cid,
-                   ATmake("snd-event(parsetext(<term>,<term>,<term>))",
-                   t1,t2,t3));
-    }
-  }
-  else if(ATmatch(t,
+    if((buf = ReadFile(input_file_name,&size)) == NULL)
+      exit(1);
+
+    ATBwriteTerm(cid,
+                 ATmake("snd-event(parsetext(<str>,<str>,<str>))",
+                        parse_table_name,
+			start_symbol?start_symbol:"",
+			buf));
+    free(buf); 
+  } else if(ATmatch(t,
                   "parsetext(<str>,<str>,<str>,parse-error(<term>))",
                   NULL,NULL,NULL,&message)) {
     WriteFile(output_file_name,ATmake("parse-error(<term>)",message));
     ATBwriteTerm(cid, ATmake("snd-disconnect"));
     exit(1);
-  }
-  else if(ATmatch(t,
+  } else if(ATmatch(t,
                   "parsetext(<str>,<str>,<str>,<term>)",
                   NULL,NULL,NULL,&tree)) { 
     WriteFile(output_file_name,tree);
@@ -114,25 +119,42 @@ void rec_terminate(int cid, ATerm t) {
 
 struct option longopts[] =
 {
+  {"help",          no_argument,       NULL,               'h'},
   {"input",         required_argument, NULL,               'i'},
   {"output",        required_argument, NULL,               'o'},
   {"parse-table",   required_argument, NULL,               'p'},
   {"start-symbol",  required_argument, NULL,               's'},
+  {"tool",          required_argument, NULL,               'T'},
+  {"TB_TOOL_NAME",  required_argument, NULL,               'T'},
+  {"host",          required_argument, NULL,               'H'},
+  {"TB_HOST",       required_argument, NULL,               'H'},
+  {"port",          required_argument, NULL,               'P'},
+  {"TB_PORT",       required_argument, NULL,               'P'},
   {0, 0, 0, 0}
 };
 
 void Usage(FILE *stream, ATbool long_message)
 {
-  const char usage[] = "Usage: %s [-i file] [-o file]\n";
+  const char usage[] =
+    "Usage: %s -T toolname -i input-file -p parse-table [-o file]  \\\n"
+    "       [-H hostname] [-P port] [-s sort]\n%s";
+  const char long_usage[] = 
+    "\n"
+    "\t-h|--help              : usage information\n"
+    "\t-i|--input        file : input from |file|                   (*)\n"
+    "\t-p|--parse-table  file : use parse table |file|              (*)\n"
+    "\t-s|--start-symbol sort : parse over sort |sort|\n"
+    "\t-H|--host         host : parse server runs on |host|\n"
+    "\t   --TB_HOST      host : ditto  (default: local host)\n"
+    "\t-P|--port         port : parse server runs on |port|\n"
+    "\t   --TB_PORT      port : ditto  (default: 8999)\n"
+    "\t-T|--tool         tool : parse server toolname is |tool|\n"
+    "\t   --TB_TOOL_NAME tool : ditto  (default: `"program_name"')\n"
+    "\t-o|--output       file : output to |file| (default: stdout)\n"
+    "\n"
+    "\tItems marked with (*) are mandatory; all others are optional.\n";
 
-  ATfprintf(stream, usage, program_name);
-  if(long_message) {
-    ATfprintf
-(stream,
- "\t-i file  : input from |file| (optional, default is stdin)\n"
- "\t-o file  : output to |file| (optional, default is stdout)\n"
-             , program_name);
-  }
+  ATfprintf(stream, usage, program_name, long_message?long_usage:"");
 }
 
 void handle_options (int argc, char **argv)
@@ -140,39 +162,41 @@ void handle_options (int argc, char **argv)
   int c; /* option character */
 
   while ((c = getopt_long(argc, argv,
-                          "i:o:p:s:",
+                          "hi:o:p:s:H:P:T:",
                           longopts, NULL))
          != EOF)
     switch (c) {
-    case 'i':   input_file_name  = optarg;    break;
-    case 'o':   output_file_name = optarg;    break;
-    case 'p':   parse_table_name = optarg;    break;
-    case 's':   start_symbol     = optarg;    break;
-    default:  Usage(stderr, ATfalse);      exit(1);
+      case 'h':   Usage(stdout, ATtrue);        exit(0);
+      case 'i':   input_file_name  = optarg;    break;
+      case 'o':   output_file_name = optarg;    break;
+      case 'p':   parse_table_name = optarg;    break;
+      case 's':   start_symbol     = optarg;    break;
+      case 'H':   hostname         = optarg;    break;
+      case 'P':   port             = optarg;    break;
+      case 'T':   tool             = optarg;    break;
+      default:  Usage(stderr, ATfalse);        exit(1);
+  }
+  if(!input_file_name || !parse_table_name) {
+    Usage(stderr, ATfalse);
+    exit(1);
   }
 }
 
 int main (int argc, char **argv)
 {
-  int i, cid, use_toolbus = 0;
+  int   cid;
   ATerm bottomOfStack;
-
   ATerm t;
 
-  for(i=1; !use_toolbus && i<argc; i++)
-    if(!strcmp(argv[i], "-TB_TOOL_NAME"))
-      use_toolbus = 1;
+  handle_options(argc, argv);
 
-  if(use_toolbus) {
-    ATBinit(argc, argv, &bottomOfStack);    /* Initialize Aterm library */
-    cid = ATBconnect(NULL, NULL, -1, parse_client_handler);
+  ATBinit(argc, argv, &bottomOfStack);    /* Initialize Aterm library */
+  cid = ATBconnect(	tool, hostname?hostname:NULL, port?atoi(port):-1,
+		      parse_client_handler);
 
-    handle_options(argc-2, argv+2);
-    t = ATmake("<str>", parse_table_name);
-    ATBwriteTerm(cid,ATmake("snd-event(open-language(<term>,<term>))",t,t));
-    ATBeventloop();
-  }
+  t = ATmake("<str>", parse_table_name);
+  ATBwriteTerm(cid,ATmake("snd-event(open-language(<term>,<term>))", t, t));
+  ATBeventloop();
+
   return 0;
 }
-
-
