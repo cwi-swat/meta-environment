4ff4452a56a330de50d901366945ce53cb5506bd
Jeroen Scheerder <js@xs4all.nl>
1999-05-17 17:14:33 +0000

    * cleanup (portability, mainly) 	* redesigned parse-table dependencies in toolbus scripts / 	  in-output / module-db

--- asf/semantics/asfe/evaluator.c
+++ asf/semantics/asfe/evaluator.c
@@ -1,3 +1,6 @@
+/*
+  $Id$
+ */
 
 /*{{{   evaluator.c */
 /*
@@ -18,17 +21,17 @@ A first version of an INTERPRETER in ToolBus C.
                    [M_n,[Ofs_n1,[Eq_n11,...,Eq_n1q]],
                         ...,
                         [Ofs_np,[Eq_np1,...,Eq_npr]]]]
-   When adding a new set of equations in the database with the 
+   When adding a new set of equations in the database with the
    function ``add_equations'' some layout is removed (the layout
-   between the arguments of an ``appl'' and between the elements 
-   of a ``list''), the separators in the lists with separators 
+   between the arguments of an ``appl'' and between the elements
+   of a ``list''), the separators in the lists with separators
    is removed, and the lexicals are expanded to become lists of
    single characters. This is performed via the function
    ``remove_layout_from_equations''.
    The equations are sorted on ofs and stored in the database.
-   
+
    Before the rewriting of a term starts he layout is removed,
-   the separators in the lists with separators 
+   the separators in the lists with separators
    is removed, and the lexicals are expanded to become lists of
    single characters as well. Based on the module over which the
    term is to be reduced the appropriate set of rewrite rule is
@@ -51,7 +54,7 @@ A first version of an INTERPRETER in ToolBus C.
    3. No hashing, sharing, or caching.
 */
 
-/*}}}  */ 
+/*}}}  */
 
 /*{{{  includes */
 
@@ -230,7 +233,7 @@ ATermList prepend_slice(ATermAppl slice, ATermList list)
 {
 	ATermList first = v_get_first(slice);
 	ATermList last  = v_get_last(slice);
-	
+
 	return prepend(first, last, list);
 }
 
@@ -253,17 +256,17 @@ ATermList append_slice(ATermList list, ATermAppl slice)
 /* Functions to be called by the ToolBus:
    - ``exists'' checks whether the equations for a certain module are
      available.
-   - ``create_equations_db'' to initialize the database. 
-   - ``clear_equations_db'' to re-initialize the database. 
+   - ``create_equations_db'' to initialize the database.
+   - ``clear_equations_db'' to re-initialize the database.
    - ``add_equations'' to add a set of equations for a certain module.
    - ``interpret'' to rewrite a given term over a given module.  */
 
 ATerm equations_available(int cid, ATerm name)
-{ 
+{
   if(find_module(name))
     return ATmake("snd-value(eqs-available(<term>))", name);
-  else 
-    return ATmake("snd-value(eqs-not-available(<term>))", name); 
+  else
+    return ATmake("snd-value(eqs-not-available(<term>))", name);
 }
 
 
@@ -288,13 +291,13 @@ ATerm add_equations(int cid, ATerm name, ATerm equs)
 {
   ATermList newequs;
   int l;
-  
-  ATfprintf(stderr, "preparing equations...\n"); 
+
+  ATfprintf(stderr, "preparing equations...\n");
   newequs = RWprepareEqs((ATermList) equs);
   l = ATgetLength(newequs);
 
-  enter_equations(name, newequs); 
- 
+  enter_equations(name, newequs);
+
   ATfprintf(stderr,"Processing %d equations of module %t\n",l,name);
   return ATmake("snd-value(equ-added(<term>))",name);
 }
@@ -316,7 +319,7 @@ ATerm add_equations(int cid, ATerm name, ATerm equs)
       of equations to the global variable ``active_rules''.
    3. The term is rewritten via ``rewrite'' with an empty
       variable environment.
-   4. The result of rewriting is processed in order to 
+   4. The result of rewriting is processed in order to
       be visualized: layout and list separators are added and
       the list of lexical characters are mapped into lexicals
       again. */
@@ -327,8 +330,8 @@ ATerm interpret(int cid,ATerm modname, ATerm trm)
   ATerm atrm, realtrm;
 
   struct tms start, rewriting;
-  clock_t user, system; 
-  
+  clock_t user, system;
+
   trm = ATremoveAllAnnotations(trm);
   atrm = asfix_get_term(trm);
   realtrm = RWprepareTerm(atrm);
@@ -343,7 +346,7 @@ ATerm interpret(int cid,ATerm modname, ATerm trm)
 
   newatrm = RWrestoreTerm(newtrm);
   result = asfix_put_term(trm,newatrm);
-/* 
+/*
   ATfprintf(stderr,"result = %t\n", result);
 */
 
@@ -351,8 +354,8 @@ ATerm interpret(int cid,ATerm modname, ATerm trm)
   ATfprintf(stdout, "rewriting took %d rewrite steps.\n", rewrite_steps);
   user = rewriting.tms_utime - start.tms_utime;
   system = rewriting.tms_stime - start.tms_stime;
-  ATfprintf(stdout, "rewriting: %f user, %f system (%f steps/sec)\n", 
-	TICK2SEC(user), TICK2SEC(system), 
+  ATfprintf(stdout, "rewriting: %f user, %f system (%f steps/sec)\n",
+	TICK2SEC(user), TICK2SEC(system),
 	((double)rewrite_steps)/(TICK2SEC(user)+TICK2SEC(system)));
 #endif
 
@@ -401,11 +404,11 @@ ATbool no_new_vars(ATerm trm, ATerm env)
     return ATtrue;
 }
 
- 
+
 /*}}}  */
 /*{{{  ATermList arg_matching(env, arg1, arg2, conds, orgargs1, orgargs2) */
 
-/* Function which tries to match two arguments. 
+/* Function which tries to match two arguments.
    First it is checked whether one of the arguments is a variable
    and the other is not. If one is a variable it is checked
    whether it is a fresh one. If the variable is in the environment
@@ -414,16 +417,16 @@ ATbool no_new_vars(ATerm trm, ATerm env)
    must be aborted.  If the variable is a fresh one it is
    stored in the variable environment
    with the term of the other argument.
-   
+
    If both arguments are ``appls'' their arguments should match.
 
    If both arguments are ``lists'' their elements shoudl match.
 
    Otherwise the arguments should be equal. If the do not
    match the matching should be aborted. */
-  
+
 ATerm arg_matching(ATerm env, ATerm arg1, ATerm arg2,
-                       ATermList conds, 
+                       ATermList conds,
                        ATermList orgargs1, ATermList orgargs2)
 {
   ATerm trm, prod1, prod2, sym1, sym2;
@@ -449,7 +452,7 @@ ATfprintf(stderr, "arg_matching: %t\n with %t\n\n", arg1, arg2);
     }
     else
       newenv = fail_env;
-  } 
+  }
   else if(asfix_is_list(arg1) && asfix_is_list(arg2)) {
 		ATbool ok = ATfalse;
     sym1 = asfix_get_list_sym(arg1);
@@ -460,16 +463,16 @@ ATfprintf(stderr, "arg_matching: %t\n with %t\n\n", arg1, arg2);
 			ok = (asfix_get_itersep_sort(sym1) == asfix_get_itersep_sort(sym2));
 			ok &= (asfix_get_itersep_sep(sym1) == asfix_get_itersep_sep(sym2));
 		}
-			
+
 		if(ok) {
       elems1 = (ATermList) asfix_get_list_elems(arg1);
-      elems2 = (ATermList) asfix_get_list_elems(arg2); 
+      elems2 = (ATermList) asfix_get_list_elems(arg2);
       newenv = list_matching(sym1,newenv,elems1,elems2,
                              conds,orgargs1,orgargs2);
     }
     else
       newenv = fail_env;
-  } 
+  }
   else if(asfix_is_var(arg1)) {
     trm = v_lookup_plain(newenv, arg1);
     if(trm) {
@@ -501,9 +504,9 @@ ATfprintf(stderr, "arg_matching: %t\n with %t\n\n", arg1, arg2);
 ATerm args_matching(ATerm env, ATermList conds,
                     ATermList args1, ATermList args2)
 {
-  ATerm arg1, arg2; 
+  ATerm arg1, arg2;
   ATerm newenv = env;
- 
+
   if(!ATisEmpty(args1)) {
     arg1 = ATgetFirst(args1);
     args1 = ATgetNext(args1);
@@ -511,14 +514,14 @@ ATerm args_matching(ATerm env, ATermList conds,
       arg2 = ATgetFirst(args2);
       args2 = ATgetNext(args2);
       newenv = arg_matching(newenv,arg1,arg2,conds,args1,args2);
-    } 
-    else 
+    }
+    else
       newenv = fail_env;
   }
   else {
-    if(!ATisEmpty(args2)) 
+    if(!ATisEmpty(args2))
       newenv = fail_env;
-    else 
+    else
       newenv = conds_satisfied(conds, newenv);
   };
   return newenv;
@@ -592,12 +595,12 @@ ATermList compare_sub_lists(ATermAppl tuple, ATermList elems2)
 
 ATerm sub_list_matching(ATerm asym, ATerm env, ATerm elem,
                         ATermList elems1, ATermList elems2,
-                        ATermList conds, 
+                        ATermList conds,
                         ATermList args1, ATermList args2)
 {
   ATerm subenv, newenv;
   ATermList last;
- 
+
   if(asfix_is_star_var(elem)) {
     if(AT_getAnnotations(elem) != NULL)
       elem = AT_removeAnnotations(elem);
@@ -618,7 +621,7 @@ ATerm sub_list_matching(ATerm asym, ATerm env, ATerm elem,
     newenv = v_put_list(env, elem, elems2, last);
     subenv = list_matching(asym,newenv,elems1,last,conds,args1,args2);
   };
- 
+
   return subenv;
 }
 
@@ -632,7 +635,7 @@ ATerm list_matching(ATerm sym,
   ATerm elem1, elem2;
   ATerm newenv;
   ATerm newarg1, newarg2;
-  ATermList newargs1, newargs2; 
+  ATermList newargs1, newargs2;
 
   if(!ATisEmpty(elems1)) {
     elem1 = ATgetFirst(elems1);
@@ -656,13 +659,13 @@ ATerm list_matching(ATerm sym,
 	    newenv = args_matching(newenv,conds,args1,args2);
 					}
         }
-      } 
+      }
       else { /*is_list_var(elem1) == Tfalse */
         if(ATgetLength(elems2) == 1) {
           elem2 = ATgetFirst(elems2);
           newenv = arg_matching(env,elem1,elem2,conds,args1,args2);
         }
-        else 
+        else
           newenv = fail_env;
       }
     }
@@ -672,15 +675,15 @@ ATerm list_matching(ATerm sym,
 	ATermAppl trms = v_lookup_list(env, elem1);
         if(trms) {
           elems2 = compare_sub_lists(trms,elems2);
-          if(elems2) 
+          if(elems2)
             newenv = list_matching(sym,env,elems1,elems2,conds,args1,args2);
           else
             newenv = fail_env;
-        } 
-        else /* TdictGet(env,elem1) == Tfalse */ 
+        }
+        else /* TdictGet(env,elem1) == Tfalse */
           newenv = sub_list_matching(sym,env,elem1,elems1,elems2,
                                      conds,args1,args2);
-      } 
+      }
       else { /*is_list_var(elem1) == Tfalse */
         if(!ATisEmpty(elems2)) {
           elem2 = ATgetFirst(elems2);
@@ -709,7 +712,7 @@ ATerm list_matching(ATerm sym,
 /*{{{  ATermList conds_satisfied(ATermList conds, ATermList env) */
 
 /* Function ``conds_satisfied'' check whether the conditions
-   of an equation can be satisfied. 
+   of an equation can be satisfied.
    Remark access functions needed to retrieve the operator,
    lhs, and rhs. */
 ATerm conds_satisfied(ATermList conds, ATerm env)
@@ -717,11 +720,11 @@ ATerm conds_satisfied(ATermList conds, ATerm env)
   ATerm cond;
   ATerm lhs, rhs, lhstrm, rhstrm;
   ATerm newenv = env;
- 
+
 
   if(!ATisEmpty(conds)) {
 		/* <PO:opt> we could do with iteration instead of recursion on the
-			 list of conditions! */  
+			 list of conditions! */
 
     cond = ATgetFirst(conds);
 
@@ -738,7 +741,7 @@ ATfprintf(stderr,"conds_satisfied entered with %t\n and %t\n", cond, env);
         lhstrm = rewrite(lhs,newenv);
         if(no_new_vars(rhs,newenv)) {
           rhstrm = rewrite(rhs,newenv);
-          if(ATisEqual(lhstrm,rhstrm)) 
+          if(ATisEqual(lhstrm,rhstrm))
             newenv = conds_satisfied(conds,newenv);
           else
             newenv = fail_env;
@@ -767,7 +770,7 @@ ATfprintf(stderr,"conds_satisfied entered with %t\n and %t\n", cond, env);
       else {
         lhstrm = rewrite(lhs,newenv);
         rhstrm = rewrite(rhs,newenv);
-        if(ATisEqual(lhstrm,rhstrm)) 
+        if(ATisEqual(lhstrm,rhstrm))
           newenv = fail_env;
         else
           newenv = conds_satisfied(conds,newenv);
@@ -784,7 +787,7 @@ ATfprintf(stderr,"conds_satisfied entered with %t\n and %t\n", cond, env);
 /* The function ``apply_rule'' tries to rewrite a term
    given a list of equations. As long as there is no
    success a new equation is tried. The arguments of
-   the term and the arguments of the left hand side of 
+   the term and the arguments of the left hand side of
    the equation are matched.
 */
 
@@ -792,15 +795,15 @@ ATerm apply_rule(ATerm trm)
 {
   ATerm top_ofs, first_ofs;
   ATermList termargs, equargs, tmpargs;
-  ATerm env; 
+  ATerm env;
   ATermList conds;
   equation_entry *entry = NULL;
- 
+
   top_ofs = asfix_get_appl_ofs(trm);
   termargs = (ATermList) asfix_get_appl_args(trm);
 
   tmpargs = termargs;
-  while(!ATisEmpty(tmpargs) && !asfix_is_appl(ATgetFirst(tmpargs))) 
+  while(!ATisEmpty(tmpargs) && !asfix_is_appl(ATgetFirst(tmpargs)))
     tmpargs = ATgetNext(tmpargs);
 
   if(!ATisEmpty(tmpargs)) {
@@ -810,7 +813,7 @@ ATerm apply_rule(ATerm trm)
 			 specification, containing an entry for each first_ofs.
 			 Each entry consists of all the equations for this combination.
 		*/
-    while((entry = find_equation(entry, top_ofs, first_ofs))) { 
+    while((entry = find_equation(entry, top_ofs, first_ofs))) {
 
 
 /*ATfprintf(stderr,"Trying equation: %t\n",entry->tag);*/
@@ -818,7 +821,7 @@ ATerm apply_rule(ATerm trm)
       conds = entry->conds;
       equargs = (ATermList) asfix_get_appl_args(entry->lhs);
 
-      env = args_matching((ATerm) ATempty, conds, equargs, termargs); 
+      env = args_matching((ATerm) ATempty, conds, equargs, termargs);
       if(!is_fail_env(env)) {
 
 /*ATfprintf(stderr,"Equation: %t was successful\n",entry->tag);*/
@@ -833,8 +836,8 @@ ATerm apply_rule(ATerm trm)
 */
     }
   }
-  
-  while((entry = find_equation(entry, top_ofs, (ATerm) ATempty))) { 
+
+  while((entry = find_equation(entry, top_ofs, (ATerm) ATempty))) {
 
 
 /*ATfprintf(stderr,"Trying equation: %t\n",entry->tag);*/
@@ -842,7 +845,7 @@ ATerm apply_rule(ATerm trm)
     conds = entry->conds;
     equargs = (ATermList) asfix_get_appl_args(entry->lhs);
 
-    env = args_matching((ATerm) ATempty, conds, equargs, termargs); 
+    env = args_matching((ATerm) ATempty, conds, equargs, termargs);
     if(!is_fail_env(env)) {
 
 /*ATfprintf(stderr,"Equation: %t was successful\n",entry->tag);*/
@@ -856,7 +859,7 @@ ATfprintf(stderr,"Equation: %t failed\n",entry->tag);
     }
 */
   }
- 
+
   return (ATerm) make_cenv(trm, fail_env);
 }
 
@@ -899,7 +902,7 @@ ATermList rewrite_args(ATermList args, ATerm env)
     while(!ATisEmpty(args)) {
       arg = ATgetFirst(args);
       if(asfix_is_appl(arg) || asfix_is_var(arg) ||
-         asfix_is_list(arg) || asfix_is_lex(arg)) 
+         asfix_is_list(arg) || asfix_is_lex(arg))
         newarg = rewrite(arg,env);
       else
         newarg = arg;
@@ -908,11 +911,11 @@ ATermList rewrite_args(ATermList args, ATerm env)
     }
   } else {
     ATerm newarg_table[32];
-    int i = 0; 
+    int i = 0;
     while(!ATisEmpty(args)) {
       arg = ATgetFirst(args);
       if(asfix_is_appl(arg) || asfix_is_var(arg) ||
-         asfix_is_list(arg) || asfix_is_lex(arg)) 
+         asfix_is_list(arg) || asfix_is_lex(arg))
         newarg_table[i] = rewrite(arg, env);
       else
         newarg_table[i] = arg;
@@ -955,7 +958,7 @@ ATermList rewrite_elems(ATerm sym, ATermList elems, ATerm env)
     }
   } else {
     ATerm newelem_table[32];
-    int i = 0; 
+    int i = 0;
     while(!ATisEmpty(elems)) {
       elem = ATgetFirst(elems);
 			if(asfix_is_list_var(elem)) {
@@ -970,10 +973,10 @@ ATermList rewrite_elems(ATerm sym, ATermList elems, ATerm env)
     for(--i; i>=0; i--) {
       newelem = newelem_table[i];
       if(ATgetAFun((ATermAppl)newelem) == list_var) {
-        newelems = prepend_slice((ATermAppl)newelem, newelems); 
+        newelems = prepend_slice((ATermAppl)newelem, newelems);
       } else {
         newelems = ATinsert(newelems, newelem);
-      } 
+      }
     }
   }
   return newelems;
@@ -989,29 +992,29 @@ ATermList rewrite_elems(ATerm sym, ATermList elems, ATerm env)
    be rewritten, conform the innermost reduction strategy.
    The resulting term can then be rewritten.
    REMARK: comments should be improved.
-*/ 
+*/
 
 ATerm rewrite(ATerm trm, ATerm env)
 {
   ATerm newtrm, sym, rewtrm;
   ATermList args, newargs;
-  ATermList elems, newelems; 
- 
+  ATermList elems, newelems;
+
   if(asfix_is_appl(trm)) {
     args = (ATermList) asfix_get_appl_args(trm);
     /*if(!args)
       ATfprintf(stderr, "trm = %t\n", trm);*/
-    newargs = rewrite_args(args,env);  
+    newargs = rewrite_args(args,env);
     if(asfix_is_bracket_func(trm)) {
       newtrm = ATgetFirst(ATgetNext(newargs));
       rewtrm = newtrm;
     }
     else {
-      newtrm = (ATerm) asfix_put_appl_args(trm,newargs); 
-      rewtrm = select_and_rewrite(newtrm); 
+      newtrm = (ATerm) asfix_put_appl_args(trm,newargs);
+      rewtrm = select_and_rewrite(newtrm);
     }
   } else if(asfix_is_var(trm)) {
-      rewtrm = v_lookup_plain(env, trm); 
+      rewtrm = v_lookup_plain(env, trm);
       if(!rewtrm)
 	rewtrm = trm;
   } else if(asfix_is_list(trm)) {
@@ -1020,8 +1023,8 @@ ATerm rewrite(ATerm trm, ATerm env)
     newelems = rewrite_elems(sym, elems, env);
     rewtrm = (ATerm) asfix_put_list_elems(trm, (ATerm) newelems);
   }
-  else { 
-    rewtrm = trm; 
+  else {
+    rewtrm = trm;
   }
   return rewtrm;
 }
@@ -1037,12 +1040,12 @@ int main(int argc, char **argv)
 
   ATBinit(argc, argv, &bottomOfStack);
   AFinit(argc, argv, &bottomOfStack);
-  cid = ATBconnect(NULL, NULL, -1,evaluator_handler);  
+  cid = ATBconnect(NULL, NULL, -1,evaluator_handler);
 
   equations_db = ATdictCreate();
   ATprotect(&equations_db);
 
-  fail_env = ATparse("[fail]"); 
+  fail_env = ATparse("[fail]");
 	ATprotect(&fail_env);
 
 	list_var  = ATmakeAFun("*list-var*", 3, ATtrue);
