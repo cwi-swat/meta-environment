24b8042f9cc99d10698026aebc23088399d06944
Mark van den Brand <M.G.J.v.d.Brand@tue.nl>
1999-12-21 16:11:24 +0000

    Two new attributes added.

--- pgen/c-source/AUX-Table-To-ATerm19.c
+++ pgen/c-source/AUX-Table-To-ATerm19.c
@@ -13,15 +13,19 @@ static Symbol lf3sym ;
 static ATerm lf3 ( ATerm arg1 ) ;
 static Symbol ef4sym ;
 static funcptr ef4 ;
+static Symbol ef5sym ;
+static funcptr ef5 ;
+static Symbol ef6sym ;
+static funcptr ef6 ;
 void register_AUX_Table_To_ATerm19 ( ) {
-lf_AUX_Table_To_ATerm19_1sym = ATmakeSymbol ( "prod(id(\"Table-To-ATerm\"),w(\"\"),[l(\"reject\"),w(\"\"),l(\"(\"),w(\"\"),sort(\"Attributes\"),w(\"\"),l(\")\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"NatCon\"),w(\"\"),no-attrs)"
+lf_AUX_Table_To_ATerm19_1sym = ATmakeSymbol ( "prod(id(\"Table-To-ATerm\"),w(\"\"),[l(\"special-attr\"),w(\"\"),l(\"(\"),w(\"\"),sort(\"Attributes\"),w(\"\"),l(\")\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"NatCon\"),w(\"\"),no-attrs)"
  , 1 , ATtrue ) ;
 ATprotectSymbol ( lf_AUX_Table_To_ATerm19_1sym ) ;
 lf2sym = ATmakeSymbol ( "listtype(sort(\"CHAR\"))" , 1 , ATtrue ) ;
 ATprotectSymbol ( lf2sym ) ;
 lf3sym = ATmakeSymbol ( "listtype(sort(\"Attribute\"),ql(\",\"))" , 1 , ATtrue ) ;
 ATprotectSymbol ( lf3sym ) ;
-register_prod ( ATparse ( "prod(id(\"Table-To-ATerm\"),w(\"\"),[l(\"reject\"),w(\"\"),l(\"(\"),w(\"\"),sort(\"Attributes\"),w(\"\"),l(\")\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"NatCon\"),w(\"\"),no-attrs)" ) , lf_AUX_Table_To_ATerm19_1 , lf_AUX_Table_To_ATerm19_1sym ) ;
+register_prod ( ATparse ( "prod(id(\"Table-To-ATerm\"),w(\"\"),[l(\"special-attr\"),w(\"\"),l(\"(\"),w(\"\"),sort(\"Attributes\"),w(\"\"),l(\")\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"NatCon\"),w(\"\"),no-attrs)" ) , lf_AUX_Table_To_ATerm19_1 , lf_AUX_Table_To_ATerm19_1sym ) ;
 register_prod ( ATparse ( "listtype(sort(\"CHAR\"))" ) , lf2 , lf2sym ) ;
 register_prod ( ATparse ( "listtype(sort(\"Attribute\"),ql(\",\"))" ) , lf3 , lf3sym ) ;
 }
@@ -34,18 +38,30 @@ ef3 = lookup_func ( ATreadFromString ( "prod(id(\"Kernel-Sdf-Syntax\"),w(\"\"),[
 ef3sym = lookup_sym ( ATreadFromString ( "prod(id(\"Kernel-Sdf-Syntax\"),w(\"\"),[ql(\"{\"),w(\"\"),iter-sep(l(\"{\"),w(\"\"),sort(\"Attribute\"),w(\"\"),ql(\",\"),w(\"\"),l(\"}\"),w(\"\"),l(\"*\")),w(\"\"),ql(\"}\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attributes\"),w(\"\"),no-attrs)" ) ) ;
 ef4 = lookup_func ( ATreadFromString ( "prod(id(\"Restrictions-Sdf-Syntax\"),w(\"\"),[ql(\"reject\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attribute\"),w(\"\"),no-attrs)" ) ) ;
 ef4sym = lookup_sym ( ATreadFromString ( "prod(id(\"Restrictions-Sdf-Syntax\"),w(\"\"),[ql(\"reject\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attribute\"),w(\"\"),no-attrs)" ) ) ;
+ef5 = lookup_func ( ATreadFromString ( "prod(id(\"Restrictions-Sdf-Syntax\"),w(\"\"),[ql(\"eager\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attribute\"),w(\"\"),no-attrs)" ) ) ;
+ef5sym = lookup_sym ( ATreadFromString ( "prod(id(\"Restrictions-Sdf-Syntax\"),w(\"\"),[ql(\"eager\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attribute\"),w(\"\"),no-attrs)" ) ) ;
+ef6 = lookup_func ( ATreadFromString ( "prod(id(\"Restrictions-Sdf-Syntax\"),w(\"\"),[ql(\"uneager\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attribute\"),w(\"\"),no-attrs)" ) ) ;
+ef6sym = lookup_sym ( ATreadFromString ( "prod(id(\"Restrictions-Sdf-Syntax\"),w(\"\"),[ql(\"uneager\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attribute\"),w(\"\"),no-attrs)" ) ) ;
 }
 static ATerm constant0 = NULL ;
 static ATerm constant1 = NULL ;
 static ATerm constant2 = NULL ;
+static ATerm constant3 = NULL ;
+static ATerm constant4 = NULL ;
+static ATerm constant5 = NULL ;
+static ATerm constant6 = NULL ;
 void init_AUX_Table_To_ATerm19 ( ) {
 ATprotect ( & constant0 ) ;
 ATprotect ( & constant1 ) ;
 ATprotect ( & constant2 ) ;
+ATprotect ( & constant3 ) ;
+ATprotect ( & constant4 ) ;
+ATprotect ( & constant5 ) ;
+ATprotect ( & constant6 ) ;
 }
 ATerm lf_AUX_Table_To_ATerm19_1 ( ATerm arg0 ) {
 {
-ATerm tmp [ 2 ] ;
+ATerm tmp [ 3 ] ;
 FUNC_ENTRY ( lf_AUX_Table_To_ATerm19_1sym , ATmakeAppl ( lf_AUX_Table_To_ATerm19_1sym , arg0 ) ) ;
 {
 ATerm ltmp [ 1 ] ;
@@ -59,20 +75,33 @@ ATerm atmp00 = arg_0 ( arg0 ) ;
 if ( check_sym ( atmp00 , lf3sym ) ) {
 {
 ATerm atmp000 = arg_0 ( atmp00 ) ;
-if ( ! not_empty_list ( atmp000 ) ) {
-FUNC_EXIT_CONST ( constant0 , ( * ef2 ) ( lf2 ( make_list ( make_char ( 48 ) ) ) ) ) ;
-}
-else {
+if ( not_empty_list ( atmp000 ) ) {
 tmp [ 0 ] = list_head ( atmp000 ) ;
+if ( term_equal ( tmp [ 0 ] , ( constant1 ? constant1 : ( constant1 = ( * ef6 ) ( ) ) ) ) ) {
 tmp [ 1 ] = list_tail ( atmp000 ) ;
-if ( ! term_equal ( tmp [ 0 ] , ( * ef4 ) ( ) ) ) {
-arg0 = ( * ef3 ) ( lf3 ( make_list ( tmp [ 1 ] ) ) ) ;
+FUNC_EXIT_CONST ( constant2 , ( * ef2 ) ( lf2 ( make_list ( make_char ( 52 ) ) ) ) ) ;
+}
+if ( term_equal ( tmp [ 0 ] , ( constant3 ? constant3 : ( constant3 = ( * ef4 ) ( ) ) ) ) ) {
+tmp [ 1 ] = list_tail ( atmp000 ) ;
+FUNC_EXIT_CONST ( constant4 , ( * ef2 ) ( lf2 ( make_list ( make_char ( 49 ) ) ) ) ) ;
+}
+if ( term_equal ( tmp [ 0 ] , ( constant5 ? constant5 : ( constant5 = ( * ef5 ) ( ) ) ) ) ) {
+tmp [ 1 ] = list_tail ( atmp000 ) ;
+FUNC_EXIT_CONST ( constant6 , ( * ef2 ) ( lf2 ( make_list ( make_char ( 50 ) ) ) ) ) ;
+}
+tmp [ 1 ] = list_head ( atmp000 ) ;
+tmp [ 2 ] = list_tail ( atmp000 ) ;
+if ( ! term_equal ( tmp [ 1 ] , ( * ef4 ) ( ) ) ) {
+if ( ! term_equal ( tmp [ 1 ] , ( * ef5 ) ( ) ) ) {
+if ( ! term_equal ( tmp [ 1 ] , ( * ef6 ) ( ) ) ) {
+arg0 = ( * ef3 ) ( lf3 ( make_list ( tmp [ 2 ] ) ) ) ;
 goto lbl_lf_AUX_Table_To_ATerm19_1 ;
 }
-if ( term_equal ( list_head ( atmp000 ) , ( constant1 ? constant1 : ( constant1 = ( * ef4 ) ( ) ) ) ) ) {
-tmp [ 0 ] = list_tail ( atmp000 ) ;
-FUNC_EXIT_CONST ( constant2 , ( * ef2 ) ( lf2 ( make_list ( make_char ( 49 ) ) ) ) ) ;
 }
+}
+}
+else {
+FUNC_EXIT_CONST ( constant0 , ( * ef2 ) ( lf2 ( make_list ( make_char ( 48 ) ) ) ) ) ;
 }
 }
 }
