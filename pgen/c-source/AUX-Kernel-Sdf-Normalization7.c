4720bb8d2d7845a0f5b0e73b0aa7009a04877b3c
Mark van den Brand <M.G.J.v.d.Brand@tue.nl>
2000-01-10 13:55:06 +0000

    New code generated for dealing with prefer and uneager attributes.

--- pgen/c-source/AUX-Kernel-Sdf-Normalization7.c
+++ pgen/c-source/AUX-Kernel-Sdf-Normalization7.c
@@ -7,6 +7,10 @@ static Symbol ef1sym ;
 static funcptr ef1 ;
 static Symbol lf2sym ;
 static ATerm lf2 ( ATerm arg1 ) ;
+static Symbol ef3sym ;
+static funcptr ef3 ;
+static Symbol ef4sym ;
+static funcptr ef4 ;
 void register_AUX_Kernel_Sdf_Normalization7 ( ) {
 lf_AUX_Kernel_Sdf_Normalization7_1sym = ATmakeSymbol ( "prod(id(\"Kernel-Sdf-Normalization\"),w(\"\"),[ql(\"filter-attributes\"),w(\"\"),l(\"(\"),w(\"\"),sort(\"Attributes\"),w(\"\"),l(\")\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attributes\"),w(\"\"),no-attrs)"
  , 1 , ATtrue ) ;
@@ -19,14 +23,24 @@ register_prod ( ATparse ( "listtype(sort(\"Attribute\"),ql(\",\"))" ) , lf2 , lf
 void resolve_AUX_Kernel_Sdf_Normalization7 ( ) {
 ef1 = lookup_func ( ATreadFromString ( "prod(id(\"Kernel-Sdf-Syntax\"),w(\"\"),[ql(\"{\"),w(\"\"),iter-sep(l(\"{\"),w(\"\"),sort(\"Attribute\"),w(\"\"),ql(\",\"),w(\"\"),l(\"}\"),w(\"\"),l(\"*\")),w(\"\"),ql(\"}\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attributes\"),w(\"\"),no-attrs)" ) ) ;
 ef1sym = lookup_sym ( ATreadFromString ( "prod(id(\"Kernel-Sdf-Syntax\"),w(\"\"),[ql(\"{\"),w(\"\"),iter-sep(l(\"{\"),w(\"\"),sort(\"Attribute\"),w(\"\"),ql(\",\"),w(\"\"),l(\"}\"),w(\"\"),l(\"*\")),w(\"\"),ql(\"}\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attributes\"),w(\"\"),no-attrs)" ) ) ;
-ef2 = lookup_func ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[l(\"id\"),w(\"\"),l(\"(\"),w(\"\"),sort(\"ModuleName\"),w(\"\"),l(\")\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attribute\"),w(\"\"),no-attrs)" ) ) ;
-ef2sym = lookup_sym ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[l(\"id\"),w(\"\"),l(\"(\"),w(\"\"),sort(\"ModuleName\"),w(\"\"),l(\")\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attribute\"),w(\"\"),no-attrs)" ) ) ;
+ef2 = lookup_func ( ATreadFromString ( "prod(id(\"Restrictions-Sdf-Syntax\"),w(\"\"),[ql(\"prefer\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attribute\"),w(\"\"),no-attrs)" ) ) ;
+ef2sym = lookup_sym ( ATreadFromString ( "prod(id(\"Restrictions-Sdf-Syntax\"),w(\"\"),[ql(\"prefer\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attribute\"),w(\"\"),no-attrs)" ) ) ;
+ef3 = lookup_func ( ATreadFromString ( "prod(id(\"Restrictions-Sdf-Syntax\"),w(\"\"),[ql(\"uneager\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attribute\"),w(\"\"),no-attrs)" ) ) ;
+ef3sym = lookup_sym ( ATreadFromString ( "prod(id(\"Restrictions-Sdf-Syntax\"),w(\"\"),[ql(\"uneager\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attribute\"),w(\"\"),no-attrs)" ) ) ;
+ef4 = lookup_func ( ATreadFromString ( "prod(id(\"Kernel-Sdf-Syntax\"),w(\"\"),[],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attributes\"),w(\"\"),no-attrs)" ) ) ;
+ef4sym = lookup_sym ( ATreadFromString ( "prod(id(\"Kernel-Sdf-Syntax\"),w(\"\"),[],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attributes\"),w(\"\"),no-attrs)" ) ) ;
 }
+static ATerm constant0 = NULL ;
+static ATerm constant1 = NULL ;
+static ATerm constant2 = NULL ;
 void init_AUX_Kernel_Sdf_Normalization7 ( ) {
+ATprotect ( & constant0 ) ;
+ATprotect ( & constant1 ) ;
+ATprotect ( & constant2 ) ;
 }
 ATerm lf_AUX_Kernel_Sdf_Normalization7_1 ( ATerm arg0 ) {
 {
-ATerm tmp [ 2 ] ;
+ATerm tmp [ 3 ] ;
 FUNC_ENTRY ( lf_AUX_Kernel_Sdf_Normalization7_1sym , ATmakeAppl ( lf_AUX_Kernel_Sdf_Normalization7_1sym , arg0 ) ) ;
 if ( check_sym ( arg0 , ef1sym ) ) {
 {
@@ -34,16 +48,29 @@ ATerm atmp00 = arg_0 ( arg0 ) ;
 if ( check_sym ( atmp00 , lf2sym ) ) {
 tmp [ 0 ] = arg_0 ( atmp00 ) ;
 {
-ATerm atmp00010 ;
 ATerm atmp0000 [ 2 ] ;
 atmp0000 [ 0 ] = tmp [ 0 ] ;
 atmp0000 [ 1 ] = tmp [ 0 ] ;
 while ( not_empty_list ( tmp [ 0 ] ) ) {
-atmp00010 = list_head ( tmp [ 0 ] ) ;
+tmp [ 1 ] = list_head ( tmp [ 0 ] ) ;
 tmp [ 0 ] = list_tail ( tmp [ 0 ] ) ;
-if ( check_sym ( atmp00010 , ef2sym ) ) {
-tmp [ 1 ] = arg_0 ( atmp00010 ) ;
-FUNC_EXIT ( ( * ef1 ) ( lf2 ( cons ( slice ( atmp0000 [ 0 ] , atmp0000 [ 1 ] ) , tmp [ 0 ] ) ) ) ) ;
+if ( check_sym ( tmp [ 1 ] , ef2sym ) ) {
+FUNC_EXIT_CONST ( constant0 , ( * ef1 ) ( lf2 ( make_list ( ( * ef2 ) ( ) ) ) ) ) ;
+}
+atmp0000 [ 1 ] = list_tail ( atmp0000 [ 1 ] ) ;
+tmp [ 0 ] = atmp0000 [ 1 ] ;
+}
+}
+tmp [ 0 ] = arg_0 ( atmp00 ) ;
+{
+ATerm atmp0000 [ 2 ] ;
+atmp0000 [ 0 ] = tmp [ 0 ] ;
+atmp0000 [ 1 ] = tmp [ 0 ] ;
+while ( not_empty_list ( tmp [ 0 ] ) ) {
+tmp [ 2 ] = list_head ( tmp [ 0 ] ) ;
+tmp [ 0 ] = list_tail ( tmp [ 0 ] ) ;
+if ( check_sym ( tmp [ 2 ] , ef3sym ) ) {
+FUNC_EXIT_CONST ( constant1 , ( * ef1 ) ( lf2 ( make_list ( ( * ef3 ) ( ) ) ) ) ) ;
 }
 atmp0000 [ 1 ] = list_tail ( atmp0000 [ 1 ] ) ;
 tmp [ 0 ] = atmp0000 [ 1 ] ;
@@ -52,7 +79,7 @@ tmp [ 0 ] = atmp0000 [ 1 ] ;
 }
 }
 }
-FUNC_EXIT ( arg0 ) ;
+FUNC_EXIT_CONST ( constant2 , ( * ef4 ) ( ) ) ;
 FUNC_EXIT ( make_nf1 ( lf_AUX_Kernel_Sdf_Normalization7_1sym , arg0 ) ) ;
 }
 }
