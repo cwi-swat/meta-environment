e2a5a198e2ac2ea88d35614daaefaa88aaffa85a
Jeroen Scheerder <js@xs4all.nl>
2000-02-24 15:08:04 +0000

    * Changed usage information display (not all option arguments 	  are file arguments)

--- sglr/tree2dot/tree2dot-main.c
+++ sglr/tree2dot/tree2dot-main.c
@@ -19,27 +19,66 @@
 
 #include "tree-to-dot.h"
 
-#define  program_name  "tree2dot"
+static char myname[] = "tree2dot";
+static char myversion[] = "1.0";
 
-char   *input_file_name  = "-";
-char   *output_file_name = "-";
-ATbool suppress_lex = ATfalse;
+/*
+    The argument vector: list of option letters, colons denote option
+    arguments.  See Usage function, immediately below, for option
+    explanation.
+ */
 
-void Usage(FILE *stream, ATbool long_message)
+static char myarguments[] = "bD:hi:o:tvVx";
+
+
+/*
+    Usage: displays helpful usage information
+ */
+void usage(void)
 {
-  const char usage[] =
-    "Usage: %s [-i input-file] [(-o|-D) output-file] [-x]\n%s";
-  const char long_usage[] =
-    "\n"
-    "\t-h        : usage information\n"
-    "\t-i   file : input from |file|               (default: stdin)\n"
-    "\t-o   file : output to |file|                (default: stdout)\n"
-    "\t-D   file : output to |file|                (default: stdout)\n"
-    "\t-x        : suppress lexicals               (default: off)\n";
+    static char *myargumentsexplained = NULL;
 
-  ATfprintf(stream, usage, program_name, long_message?long_usage:"");
+    /*  Represent the argument string in a slightly friendlier manner  */
+    if(!myargumentsexplained && *myarguments) {
+        int  i, hyphen = 0;
+        char *ptr0, *ptr1;
+
+        for(ptr0 = myarguments, i=0; *ptr0; ptr0++)
+            if(*ptr0 == ':')
+                i++;
+        ptr1 = myargumentsexplained =
+            (char *) malloc(strlen(myarguments) + 7*i + 2);
+        for(ptr0 = myarguments; *ptr0; ptr0++)
+            if(!*(ptr0+1) || *(ptr0+1) != ':') {
+                if(!hyphen++) {
+                    *ptr1++ = ' ';
+                    *ptr1++ = '-';
+                }
+                *ptr1++ = *ptr0;
+            } else {
+                hyphen = 0;
+                if(*(ptr1-1) != ' ')
+                    *ptr1++ = ' ';
+                *ptr1++ = '-'; *ptr1++ = *ptr0++; *ptr1++ = ' ';
+                *ptr1++ = 'a'; *ptr1++ = 'r'; *ptr1++ = 'g';
+            }
+        *ptr1++ = '\0';
+    }
+
+    fprintf(stderr,
+        "Usage: %s%s . . .\n"
+        "Options:\n"
+        "\t-b              ignored\n"
+        "\t-D filename     output to file (default stdout)\n"
+        "\t-h              display help information (usage)\n"
+        "\t-i filename     input from file (default stdin)\n"
+        "\t-o filename     output to file (default stdout)\n"
+        "\t-t              ignored\n"
+        "\t-v              ignored\n"
+        "\t-V              reveal program version (i.e. %s)\n"
+        "\t-x              suppress lexicals               (default: off)\n",
+        myname, myargumentsexplained, myversion);
 }
-
 
 int main (int argc, char **argv)
 {
@@ -48,28 +87,31 @@ int main (int argc, char **argv)
   ATerm t;
   char  *ATlibArgv[] = { "", "-silent"};
 
+  char   *input_file_name  = "-";
+  char   *output_file_name = "-";
+  ATbool suppress_lex = ATfalse;
+
   while ((c = getopt(argc, argv, "hi:o:D:x")) != EOF)
     switch (c) {
-      case 'h':  Usage(stdout, ATtrue);        exit(0);
+      case 'h':  usage();                      exit(0);
       case 'i':  input_file_name  = optarg;    break;
       case 'o':
       case 'D':  output_file_name = optarg;    break;
       case 'x':  suppress_lex = !suppress_lex; break;
-      default :  Usage(stderr, ATfalse);       exit(1);
+
+      case 'b':
+      case 't':
+      case 'v':                                break;
+      default :  usage();                      exit(1);
   }
   ATinit(2, ATlibArgv, &bottomOfStack);    /* Initialize Aterm library */
 
-  if(strcmp(input_file_name, "-"))
-    t = ATreadFromNamedFile(input_file_name);
-  else
-    t = ATreadFromFile(stdin);
+  t = ATreadFromNamedFile(input_file_name);
   if(!t) {
-    ATfprintf(stderr, "%s: could not read term from input file %s",
-              program_name, input_file_name);
-    return 1;
+    ATerror("%s: could not read term from input file %s\n", myname, input_file_name);
   }
 
-  SGtreeToDotFile(program_name, output_file_name, t, suppress_lex);
+  SGtreeToDotFile(myname, output_file_name, t, suppress_lex);
 
   return 0;
 }
