8195c355e8da0c7a07ce4c272ca37cc3a332ff0b
Tobias Kuipers <tobias.kuipers@gmail.com>
2000-02-29 13:39:54 +0000

    * Added init() function for var AFun symbols.

--- toolbus/adapters/java-adapter/toolbus/aterm/AFun.java
+++ toolbus/adapters/java-adapter/toolbus/aterm/AFun.java
@@ -14,35 +14,36 @@ public class AFun extends ATermAppl {
 <p>
 
 So far the following AFuns are defined:
-<ul> <li> prod <li> appl <li> list </ul>
+<ul> <li> prod <li> appl <li> list <li> lex <li> term <li> var </ul>
 */
 
   public ATermAppl init() {
     String af = this.getFun();
     ATermAppl result = null;
     try {
-    if (af.equals("prod")) {
-      result = (ATermAppl) world.makeSimple("prod(module-name,w(\"\"),syms,w(\"\"),l(\"->\"),w(\"\"),sym,w(\"\"),attrs)");
-    } else if (af.equals("appl")) {
-      result = (ATermAppl) world.makeSimple("appl(prod,w(\"\"),args)");
-    } else if (af.equals("list")) {
-      result = (ATermAppl) world.makeSimple("list(sym,w(\"\"),elems)");
-      //    } else {
-      //      throw new Exception("Undefined AFun being initialized: "+af);
-    } else if (af.equals("lex")) {
-	result = (ATermAppl) world.makeSimple("lex(name,sym)");
-    } else if (af.equals("term")) {
-	result = (ATermAppl) world.makeSimple("term(l(\"term\"),w(\"\"),name,w(\"\"),module-name,w-left,term,w-right,abbrevs)");
-    } else {
-	System.err.println("AFun.init: Tried to init undefined afun");
-	System.exit(1);
-    }
+    	if (af.equals("prod")) {
+      	result = (ATermAppl) world.makeSimple("prod(module-name,w(\"\"),syms,w(\"\"),l(\"->\"),w(\"\"),sym,w(\"\"),attrs)");
+    	} else if (af.equals("appl")) {
+    	  result = (ATermAppl) world.makeSimple("appl(prod,w(\"\"),args)");
+    	} else if (af.equals("list")) {
+      	result = (ATermAppl) world.makeSimple("list(sym,w(\"\"),elems)");
+      	//    } else {
+      	//      throw new Exception("Undefined AFun being initialized: "+af);
+    	} else if (af.equals("lex")) {
+				result = (ATermAppl) world.makeSimple("lex(name,sym)");
+    	} else if (af.equals("term")) {
+				result = (ATermAppl) world.makeSimple("term(l(\"term\"),w(\"\"),name,w(\"\"),module-name,w-left,term,w-right,abbrevs)");
+    	} else if (af.equals("var")) {
+				result = (ATermAppl) world.makeSimple("var(name,sym)");
+    	} else {
+				System.err.println("AFun.init: Tried to init undefined afun");
+				System.exit(1);
+    	}
     }
     catch (ParseError e) {
-	System.err.println(e);
-	System.exit(1);
+			System.err.println(e);
+			System.exit(1);
     }
     return result;
   }
-      
 }	
