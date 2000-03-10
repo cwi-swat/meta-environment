7de26ae0faeba230e87685dc6125312656dafd8b
Pieter Olivier <pieter@gamesquare.nl>
2000-03-10 10:48:52 +0000

    - AsFix2C is no longer installed in the bindir - Added -h checking in editor.in - Cleaned up variable naming in some of the TB scripts. - Added check for correct argc with -h option in dump-skel.c - test-help now checks for exit code == 0 instead of 1

--- sglr/dump/dump-skel.c
+++ sglr/dump/dump-skel.c
@@ -22,38 +22,38 @@ void usage(char *prg)
 
 int main (int argc, char **argv)
 {
-    ATerm       bottomOfStack;
-    char        *ATlibArgv[] = {"", "-silent"};
-    char        *err;
-    char        *pt_name;
-    parse_table *pt;
-    int         requested = -1;
+  ATerm       bottomOfStack;
+  char        *ATlibArgv[] = {"", "-silent"};
+  char        *err;
+  char        *pt_name;
+  parse_table *pt;
+  int         requested = -1;
 
-    if(strcmp(argv[1], "-h") == 0) {
-        usage(argv[0]);
-    }
+  if(argc == 2 && strcmp(argv[1], "-h") == 0) {
+    usage(argv[0]);
+  }
 
-    ATinit(argc, argv, &bottomOfStack);     /* Initialize Aterm library */
-    switch(argc) {
-        case 1:
-            pt_name = "-";
-            break;
-        case 3:
-            requested = atoi(argv[2]);
-        default:
-            pt_name = argv[1];
-            break;
-    }
-    if(ATmatch(SGopenLanguage("dump", 0, pt_name, pt_name),
-               "snd-value(open-language-failed(<str>,<str>))", &err, NULL)) {
-        ATfprintf(stderr, "could not open %s as a parse table\n", err);
-        return 1;
-    }
+  ATinit(argc, argv, &bottomOfStack);     /* Initialize Aterm library */
+  switch(argc) {
+    case 1:
+      pt_name = "-";
+      break;
+    case 3:
+      requested = atoi(argv[2]);
+    default:
+      pt_name = argv[1];
+      break;
+  }
+  if(ATmatch(SGopenLanguage("dump", 0, pt_name, pt_name),
+             "snd-value(open-language-failed(<str>,<str>))", &err, NULL)) {
+    ATfprintf(stderr, "could not open %s as a parse table\n", err);
+    return 1;
+  }
 
-    if(!(pt = SG_LookupParseTable(pt_name, ATtrue))) {
-        ATfprintf(stderr, "failed to find parse table for languate %s\n", pt_name);
-        return 1;
-    }
-    DoDump(pt, requested);
-    return 0;
+  if(!(pt = SG_LookupParseTable(pt_name, ATtrue))) {
+    ATfprintf(stderr, "failed to find parse table for languate %s\n", pt_name);
+    return 1;
+  }
+  DoDump(pt, requested);
+  return 0;
 }
