c9993383cfc4fe0697fdb5f0cb9a09bf55cc2660
Pieter Olivier <pieter@gamesquare.nl>
2000-07-26 14:02:19 +0000

    - Removed some 'extern' definitions: evaluator.h is included instead - Fixed error handling code to use new api.

--- asf/semantics/asfe/test/whitespace.c
+++ asf/semantics/asfe/test/whitespace.c
@@ -29,32 +29,22 @@
 #include <stdlib.h>
 #include "preparation.h"
 #include "asfix_utils.h"
+#include "evaluator.h"
 #include <AsFix.h>
-
-extern ATbool run_verbose;
 
 ATerm reduce_test(ATerm eqs, ATerm term, ATbool with_whitespace);
 
-/* externs */
-extern ATerm equations_db;
-extern ATerm fail_env;
-extern ATerm posinfo;
-extern AFun list_var, plain_var;
-extern ATbool keep_layout; 
-extern ATermList rewrite_errors;
-extern ATerm rewrite(ATerm trm, ATerm env);
-
 int main(int argc, char *argv[])
 {
-	ATbool usage = ATfalse;
-	FILE *eqsfile, *termfile;
-	ATerm eqs, term, result[2];
-	ATerm bottomOfStack;
+  ATbool usage = ATfalse;
+  FILE *eqsfile, *termfile;
+  ATerm eqs, term, result[2];
+  ATerm bottomOfStack;
 
-	AFinit(argc, argv, &bottomOfStack);
+  AFinit(argc, argv, &bottomOfStack);
 	
-	/* init */
-	equations_db = ATdictCreate();
+  /* init */
+  equations_db = ATdictCreate();
   ATprotect(&equations_db);
 
   fail_env = ATparse("[fail]");
@@ -69,78 +59,79 @@ int main(int argc, char *argv[])
   plain_var = ATmakeAFun("*plain-var*", 2, ATtrue);
   ATprotectAFun(plain_var);
 
-	if(argc != 3) {
-		ATerror("Usage: whitespace equationsfile termfile\n");
-	}
-	else {
-		if(!(eqsfile = fopen(argv[1],"r")))
-			ATerror("Could not open %s.\n", argv[1]);
+  if(argc != 3) {
+    ATerror("Usage: whitespace equationsfile termfile\n");
+  }
+  else {
+    if(!(eqsfile = fopen(argv[1],"r")))
+      ATerror("Could not open %s.\n", argv[1]);
 
-		if(!(termfile = fopen(argv[2],"r")))
-			ATerror("Could not open %s.\n", argv[2]);
-	}
+    if(!(termfile = fopen(argv[2],"r")))
+      ATerror("Could not open %s.\n", argv[2]);
+  }
 
 	
-	eqs = ATreadFromFile(eqsfile);
-	term = ATreadFromFile(termfile);
+  eqs = ATreadFromFile(eqsfile);
+  term = ATreadFromFile(termfile);
 
-	fclose(eqsfile);
-	fclose(termfile);
+  fclose(eqsfile);
+  fclose(termfile);
 
 
-        if(run_verbose) {
-           ATwarning("=====================%s=======================\n",argv[2]);
-        }
+  if(run_verbose) {
+    ATwarning("=====================%s=======================\n",argv[2]);
+  }
 
-	/* first we use normal reduction */
-	result[0] = reduce_test(eqs,term, ATfalse);
+  /* first we use normal reduction */
+  result[0] = reduce_test(eqs,term, ATfalse);
 
-	/* re-init db */
-	equations_db = ATdictCreate();
+  /* re-init db */
+  equations_db = ATdictCreate();
 
-	if(run_verbose) {
-           ATwarning("=====================%s=======================\n",argv[2]);
-        }
+  if(run_verbose) {
+    ATwarning("=====================%s=======================\n",argv[2]);
+  }
 
-	/* then we reduce with whitespace */
-	result[1] = reduce_test(eqs, term,ATtrue);
+  /* then we reduce with whitespace */
+  result[1] = reduce_test(eqs, term,ATtrue);
 	
-	if(!isEqualModuloWhitespace(result[0],result[1]))		{
-		ATerror("Terms %t AND %t are different modulo whitespace.\n",
-						result[0],result[1]);
-	}
+  if(!isEqualModuloWhitespace(result[0],result[1]))		{
+    ATerror("Terms %t AND %t are different modulo whitespace.\n",
+	    result[0],result[1]);
+  }
 	
-	return 0;
+  return 0;
 }
 
 ATerm reduce_test(ATerm eqs, ATerm term, ATbool with_whitespace)
 {
-	ATermList preparedeqs;
-	ATerm oldterm;
-	char name[] = "Reduce_test";
+  ATermList preparedeqs;
+  ATerm oldterm;
+  char name[] = "Reduce_test";
 
-	/* set the global */
-	keep_layout = with_whitespace;
+  /* set the global */
+  keep_layout = with_whitespace;
 
-	/* prepare equations */
-	preparedeqs =  RWprepareEqs((ATermList) eqs);
-	enter_equations(name, preparedeqs);
+  /* prepare equations */
+  preparedeqs =  RWprepareEqs((ATermList) eqs);
+  enter_equations(name, preparedeqs);
 
-	/* prepare term */
-	term = ATremoveAllAnnotations(term);
-	oldterm = term;
-	term = asfix_get_term(term);
-	term = RWprepareTerm(term);
+  /* prepare term */
+  term = ATremoveAllAnnotations(term);
+  oldterm = term;
+  term = asfix_get_term(term);
+  term = RWprepareTerm(term);
 
-	/* rewrite */
-	select_equations(name);
-	rewrite_errors = ATempty;
+  /* rewrite */
+  select_equations(name);
+  RWclearError();
 	
-	term = rewrite(term,(ATerm) ATempty);
-	term = RWrestoreTerm(term);
-	
-	if(!ATisEmpty(rewrite_errors))
-		ATerror("errors during rewriting: %l", rewrite_errors);
+  term = rewrite(term,(ATerm) ATempty, 0);
+  term = RWrestoreTerm(term);
 
-	return term;
+  if (RWgetError()) {
+    ATerror("error during rewriting: %l", RWgetError());
+  }
+
+  return term;
 }
