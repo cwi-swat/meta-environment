c239c2775395148d35cb97e5bbe30bc938cf1a0b
Hayco de Jong <jong@metamud.org>
2000-08-25 08:07:43 +0000

    * In verbose mode, animated counter is only displayed if stderr is a tty.

--- sglr/libsglr/parser.c
+++ sglr/libsglr/parser.c
@@ -1,7 +1,8 @@
 /*
 
     SGLR - the Scannerless Generalized LR parser.
-    Copyright (C) 2000  Stichting Mathematisch Centrum, Amsterdam, The Netherlands.
+    Copyright (C) 2000  Stichting Mathematisch Centrum, Amsterdam,
+                        The Netherlands.
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -28,6 +29,7 @@
  input.
  */
 
+#include <unistd.h>
 #include <string.h>
 
 #include <aterm2.h>
@@ -403,12 +405,23 @@ forest SG_Parse(parse_table *ptable, char *sort, int(*get_next_token)(void))
              SG_PrintToken(SG_log(), current_token);
              fprintf(SG_log(), "\n");
              )
-    IF_VERBOSE(fprintf(stderr, "\rParsing  %6d", sg_tokens_read))
+
+    IF_VERBOSE(
+	       if (isatty(fileno(stderr)))
+	         fprintf(stderr, "\rParsing  %6d", sg_tokens_read);
+	      )
+
     SG_ParseToken();
-    IF_VERBOSE(fprintf(stderr, "\rShifting %6d", sg_tokens_read))
+
+    IF_VERBOSE(
+	       if (isatty(fileno(stderr)))
+	         fprintf(stderr, "\rShifting %6d", sg_tokens_read);
+	      )
+
     SG_Shifter();
   } while (current_token != SG_EOF_Token && active_stacks);
-  IF_VERBOSE(fprintf(stderr, ".\n"))
+
+  IF_VERBOSE( if (isatty(fileno(stderr))) fprintf(stderr, ".\n"); )
 
   /*  Core parsing done!  */
   SG_PostParse();
