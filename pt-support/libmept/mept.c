105d3ea793b13922eb78d5572b00d711179f0d1d
Mark van den Brand <M.G.J.v.d.Brand@tue.nl>
2001-02-08 16:46:09 +0000

    - Yet another bunch of patterns removed and transformed into access functions.

--- pt-support/libmept/mept.c
+++ pt-support/libmept/mept.c
@@ -28,20 +28,31 @@
 #include "mept.h"
 
 static ATerm asfix_appl_pattern = NULL;
+static ATerm asfix_amb_pattern = NULL;
 static ATerm asfix_prod_pattern = NULL;
 static ATerm asfix_parsetree_pattern = NULL;
 static ATerm asfix_lit_pattern = NULL;
 static ATerm asfix_lex_pattern = NULL;
 static ATerm asfix_cf_pattern = NULL;
 static ATerm asfix_opt_pattern = NULL;
+static ATerm asfix_iter_pattern = NULL;
+static ATerm asfix_iterstar_pattern = NULL;
+static ATerm asfix_itersep_pattern = NULL;
+static ATerm asfix_iterstarsep_pattern = NULL;
 static ATerm asfix_sort_pattern = NULL;
+static ATerm asfix_varsym_pattern = NULL;
 static ATerm asfix_layout_pattern = NULL;
+static ATerm asfix_flatlayout_pattern = NULL;
 
 static void init_asfix_patterns()
 {
   ATprotect(&asfix_appl_pattern);
   asfix_appl_pattern =
     ATparse("appl(<term>,[<list>])");
+
+  ATprotect(&asfix_amb_pattern);
+  asfix_amb_pattern =
+    ATparse("amb([<list>])");
 
   ATprotect(&asfix_prod_pattern);
   asfix_prod_pattern =
@@ -67,13 +78,37 @@ static void init_asfix_patterns()
   asfix_opt_pattern =
     ATparse("opt(<term>)");
 
+  ATprotect(&asfix_iter_pattern);
+  asfix_iter_pattern =
+    ATparse("iter(<term>)");
+
+  ATprotect(&asfix_iterstar_pattern);
+  asfix_iterstar_pattern =
+    ATparse("iter-star(<term>)");
+
+  ATprotect(&asfix_itersep_pattern);
+  asfix_itersep_pattern =
+    ATparse("iter-sep(<term>,<term>)");
+
+  ATprotect(&asfix_iterstarsep_pattern);
+  asfix_iterstarsep_pattern =
+    ATparse("iter-star-sep(<term>,<term>)");
+
   ATprotect(&asfix_sort_pattern);
   asfix_sort_pattern =
     ATparse("sort(<term>)");
 
+  ATprotect(&asfix_varsym_pattern);
+  asfix_varsym_pattern =
+    ATparse("varsym(<term>)");
+
   ATprotect(&asfix_layout_pattern);
   asfix_layout_pattern =
     ATparse("layout");
+
+  ATprotect(&asfix_flatlayout_pattern);
+  asfix_flatlayout_pattern =
+    ATparse("flatlayout([<list>])");
 
 }
 
@@ -97,6 +132,11 @@ ATerm PT_makeParseTreeTree(ATerm tree, ATerm cnt)
 ATerm PT_makeTreeAppl(ATerm prod, ATermList args)
 {
   return ATmakeTerm(asfix_appl_pattern, prod, args);
+}
+
+ATerm PT_makeTreeAmb(ATermList args)
+{
+  return ATmakeTerm(asfix_amb_pattern, args);
 }
 
 ATerm PT_makeSymbolLit(char *str)
@@ -124,9 +164,19 @@ ATerm PT_makeSymbolSort(ATerm symbol)
   return ATmakeTerm(asfix_sort_pattern, symbol);
 }
 
+ATerm PT_makeSymbolVarSym(ATerm symbol)
+{
+  return ATmakeTerm(asfix_varsym_pattern, symbol);
+}
+
 ATerm PT_makeSymbolLayout()
 {
   return ATmakeTerm(asfix_layout_pattern);
+}
+
+ATerm PT_makeLayout(ATermList args)
+{
+  return ATmakeTerm(asfix_flatlayout_pattern, args);
 }
 
 ATbool PT_isParseTreeTree(ATerm tree)
@@ -137,6 +187,11 @@ ATbool PT_isParseTreeTree(ATerm tree)
 ATbool PT_isTreeAppl(ATerm tree)
 {
   return ATmatchTerm(tree, asfix_appl_pattern, NULL, NULL);
+}
+
+ATbool PT_isTreeAmb(ATerm tree)
+{
+  return ATmatchTerm(tree, asfix_amb_pattern, NULL);
 }
 
 ATbool PT_isProductionDefault(ATerm tree)
@@ -164,9 +219,34 @@ ATbool PT_isSymbolOpt(ATerm tree)
   return ATmatchTerm(tree, asfix_opt_pattern, NULL);
 }
 
+ATbool PT_isSymbolIterPlus(ATerm tree)
+{
+  return ATmatchTerm(tree, asfix_iter_pattern, NULL);
+}
+
+ATbool PT_isSymbolIterStar(ATerm tree)
+{
+  return ATmatchTerm(tree, asfix_iterstar_pattern, NULL);
+}
+
+ATbool PT_isSymbolIterPlusSep(ATerm tree)
+{
+  return ATmatchTerm(tree, asfix_itersep_pattern, NULL, NULL);
+}
+
+ATbool PT_isSymbolIterStarSep(ATerm tree)
+{
+  return ATmatchTerm(tree, asfix_iterstarsep_pattern, NULL, NULL);
+}
+
 ATbool PT_isSymbolSort(ATerm tree)
 {
   return ATmatchTerm(tree, asfix_sort_pattern, NULL);
+}
+
+ATbool PT_isSymbolVarSym(ATerm tree)
+{
+  return ATmatchTerm(tree, asfix_varsym_pattern, NULL);
 }
 
 ATbool PT_isSymbolLayout(ATerm tree)
@@ -174,6 +254,10 @@ ATbool PT_isSymbolLayout(ATerm tree)
   return ATmatchTerm(tree, asfix_layout_pattern);
 }
 
+ATbool PT_isLayout(ATerm tree)
+{
+  return ATmatchTerm(tree, asfix_flatlayout_pattern, NULL);
+}
 
 ATerm PT_getParseTreeTree(ATerm tree)
 {
@@ -227,6 +311,19 @@ ATermList PT_getTreeArgs(ATerm tree)
   return NULL;
 }
 
+ATermList PT_getAmbArgs(ATerm tree)
+{
+  ATermList args;
+
+  if (PT_isTreeAmb(tree)) {
+    ATmatchTerm(tree, asfix_amb_pattern, &args);
+    return args;
+  }
+
+  ATerror("PT_getAmbArgs: not an ambiguity: %t\n", tree);
+  return NULL;
+}
+
 ATerm PT_getProductionRhs(ATerm tree)
 {
   ATerm rhs;
@@ -237,6 +334,19 @@ ATerm PT_getProductionRhs(ATerm tree)
   }
 
   ATerror("PT_getProductionRhs: not a production: %t\n", tree);
+  return NULL;
+}
+
+ATermList PT_getProductionLhs(ATerm tree)
+{
+  ATermList lhs;
+
+  if (PT_isProductionDefault(tree)) {
+    ATmatchTerm(tree, asfix_prod_pattern, &lhs, NULL, NULL);
+    return lhs;
+  }
+
+  ATerror("PT_getProductionLhs: not a production: %t\n", tree);
   return NULL;
 }
 
@@ -292,6 +402,84 @@ ATerm PT_getOptSymbol(ATerm tree)
   return NULL;
 }
         
+ATerm PT_getIterPlusSymbol(ATerm tree)
+{
+  ATerm symbol;
+
+  if (PT_isSymbolIterPlus(tree)) {
+    ATmatchTerm(tree, asfix_iter_pattern, &symbol);
+    return symbol;
+  }
+
+  ATerror("PT_getIterPlusSymbol: not an iter symbol: %t\n", tree);
+  return NULL;
+}
+ 
+ATerm PT_getIterStarSymbol(ATerm tree)
+{
+  ATerm symbol;
+
+  if (PT_isSymbolIterStar(tree)) {
+    ATmatchTerm(tree, asfix_iterstar_pattern, &symbol);
+    return symbol;
+  }
+
+  ATerror("PT_getIterStarSymbol: not an iter-star symbol: %t\n", tree);
+  return NULL;
+} 
+
+ATerm PT_getIterPlusSepSymbol(ATerm tree)
+{
+  ATerm symbol;
+
+  if (PT_isSymbolIterPlusSep(tree)) {
+    ATmatchTerm(tree, asfix_itersep_pattern, &symbol, NULL);
+    return symbol;
+  }
+
+  ATerror("PT_getIterPlusSepSymbol: not an iter-sep symbol: %t\n", tree);
+  return NULL;
+}
+
+ATerm PT_getIterStarSepSymbol(ATerm tree)
+{
+  ATerm symbol;
+
+  if (PT_isSymbolIterStarSep(tree)) {
+    ATmatchTerm(tree, asfix_iterstarsep_pattern, &symbol, NULL);
+    return symbol;
+  }
+
+  ATerror("PT_getIterStarSepSymbol: not an iter-star-sep symbol: %t\n", tree);
+  return NULL;
+}   
+
+ATerm PT_getIterPlusSepSeparator(ATerm tree)
+{
+  ATerm symbol;
+
+  if (PT_isSymbolIterPlusSep(tree)) {
+    ATmatchTerm(tree, asfix_itersep_pattern, NULL, &symbol);
+    return symbol;
+  }
+
+  ATerror("PT_getIterPlusSepSeparator: not an iter-sep symbol: %t\n", tree);
+  return NULL;
+}
+
+ATerm PT_getIterStarSepSeparator(ATerm tree)
+{
+  ATerm symbol;
+
+  if (PT_isSymbolIterStarSep(tree)) {
+    ATmatchTerm(tree, asfix_iterstarsep_pattern, NULL, &symbol);
+    return symbol;
+  }
+
+  ATerror("PT_getIterStarSepSeparator: not an iter-star-sep symbol: %t\n", tree);
+  return NULL;
+}
+
 ATerm PT_getSortSymbol(ATerm tree)
 {                                   
   ATerm symbol;
@@ -304,6 +492,32 @@ ATerm PT_getSortSymbol(ATerm tree)
   ATerror("PT_getSortSymbol: not a sort: %t\n", tree);
   return NULL;
 }  
+
+ATerm PT_getVarSymSymbol(ATerm tree)
+{                                   
+  ATerm symbol;
+
+  if (PT_isSymbolVarSym(tree)) {
+    ATmatchTerm(tree, asfix_varsym_pattern, &symbol);
+    return symbol;
+  }
+
+  ATerror("PT_getVarSymSymbol: not a varsym: %t\n", tree);
+  return NULL;
+}  
+
+ATermList PT_getLayoutElems(ATerm tree)
+{
+  ATermList elems;
+  
+  if (PT_isLayout(tree)) {
+    ATmatchTerm(tree, asfix_flatlayout_pattern, &elems);
+    return elems;
+  }
+ 
+  ATerror("PT_getLayoutElems: not layout: %t\n", tree);
+  return NULL;
+}
 
 ATbool PT_prodHasLitAsRhs(ATerm prod)
 {
@@ -365,7 +579,70 @@ ATbool PT_prodHasCfLayoutAsRhs(ATerm  prod)
   return ATfalse;
 }
 
+ATbool PT_prodHasCfOptLayoutAsRhs(ATerm  prod)
+{
+  /* This implements: "prod([<list>],cf(layout),<term>)" */
+  if (PT_isProductionDefault(prod)) {
+    ATerm rhs = PT_getProductionRhs(prod);
+    if (PT_isSymbolCf(rhs)) {
+      ATerm cfsym = PT_getCfSymbol(rhs);
+      if (PT_isSymbolOpt(cfsym)) {
+        ATerm optsym = PT_getOptSymbol(cfsym);
+        return PT_isSymbolLayout(optsym);
+      }
+    }
+    return ATfalse;
+  }
 
+  ATerror("PT_prodHasCfLayoutAsRhs: not a production: %t\n", prod);
+  return ATfalse;
+}
+
+ATbool PT_prodHasVarSymAsRhs(ATerm prod)
+{
+  /* This implements: "prod([<list>],varsym(<str>),no-attrs)" */
+  if (PT_isProductionDefault(prod)) {
+    ATerm rhs = PT_getProductionRhs(prod);
+    return PT_isSymbolVarSym(rhs);
+  }
+
+  ATerror("PT_prodHasVarSymAsRhs: not a production: %t\n", prod);
+  return ATfalse;
+}
+
+ATbool PT_prodHasVarSymAsLhs(ATerm prod)
+{
+  /* This implements: "prod([varsym(<term>)],cf(<term>),<term>)" */
+  if (PT_isProductionDefault(prod)) {
+    ATermList lhs = PT_getProductionLhs(prod);
+    ATerm rhs = PT_getProductionRhs(prod);
+    if (PT_isSymbolCf(rhs) && ATgetLength(lhs) == 1) {
+      ATerm lhssym = ATgetFirst(lhs);
+      return PT_isSymbolVarSym(lhssym);
+    }
+    return ATfalse;
+  }
+
+  ATerror("PT_prodHasVarSymAsLhs: not a production: %t\n", prod);
+  return ATfalse;
+}
+
+ATbool PT_prodHasLexAsLhs(ATerm prod)
+{
+  /* This implements: "prod([lex(<term>)],cf(<term>),<term>)" */
+  if (PT_isProductionDefault(prod)) {
+    ATermList lhs = PT_getProductionLhs(prod);
+    ATerm rhs = PT_getProductionRhs(prod);
+    if (PT_isSymbolCf(rhs) && ATgetLength(lhs) == 1) {
+      ATerm lhssym = ATgetFirst(lhs);
+      return PT_isSymbolLex(lhssym);
+    }
+    return ATfalse;
+  }
+
+  ATerror("PT_prodHasVarSymAsLhs: not a production: %t\n", prod);
+  return ATfalse;
+}
 
 void PT_initMEPTApi()
 {
--- pt-support/libmept/mept.h
+++ pt-support/libmept/mept.h
@@ -32,38 +32,61 @@ void   PT_initMEPTApi(void);
 
 ATerm PT_makeParseTreeTree(ATerm tree, ATerm cnt);
 ATerm PT_makeTreeAppl(ATerm prod, ATermList args);
+ATerm PT_makeTreeAmb(ATermList args);
 ATerm PT_makeSymbolLit(char *str);
 ATerm PT_makeSymbolLex(ATerm arg);
 ATerm PT_makeSymbolCf(ATerm arg);
 ATerm PT_makeSymbolOpt(ATerm arg);
 ATerm PT_makeSymbolSort(ATerm arg);
+ATerm PT_makeSymbolVarSym(ATerm arg);
 ATerm PT_makeSymbolLayout();
+ATerm PT_makeLayout(ATermList args);
 
 ATbool PT_isParseTreeTree(ATerm arg);
 ATbool PT_isTreeAppl(ATerm arg);
+ATbool PT_isTreeAmb(ATerm arg);
 ATbool PT_isProductionDefault(ATerm arg);
 ATbool PT_isSymbolLit(ATerm arg);
 ATbool PT_isSymbolLex(ATerm arg);
 ATbool PT_isSymbolCf(ATerm arg);
+ATbool PT_isSymbolIterPlus(ATerm arg);
+ATbool PT_isSymbolIterStar(ATerm arg);
+ATbool PT_isSymbolIterPlusSep(ATerm arg);
+ATbool PT_isSymbolIterStarSep(ATerm arg);
 ATbool PT_isSymbolOpt(ATerm arg);
 ATbool PT_isSymbolSort(ATerm arg);
+ATbool PT_isSymbolVarSym(ATerm arg);
 ATbool PT_isSymbolLayout(ATerm arg);
+ATbool PT_isLayout(ATerm arg);
 
 ATerm     PT_getParseTreeTree(ATerm arg);
 ATerm     PT_getParseTreeAmbCnt(ATerm arg);
 ATerm     PT_getTreeProd(ATerm arg);
 ATermList PT_getTreeArgs(ATerm arg);
+ATermList PT_getAmbArgs(ATerm arg);
 ATerm     PT_getProductionRhs(ATerm arg);
+ATermList PT_getProductionLhs(ATerm arg);
 char     *PT_getLitString(ATerm arg);
 ATerm     PT_getLexSymbol(ATerm arg);
 ATerm     PT_getCfSymbol(ATerm arg);
 ATerm     PT_getOptSymbol(ATerm arg);
+ATerm     PT_getIterPlusSymbol(ATerm arg);
+ATerm     PT_getIterStarSymbol(ATerm arg);
+ATerm     PT_getIterPlusSepSymbol(ATerm arg);
+ATerm     PT_getIterStarSepSymbol(ATerm arg);
+ATerm     PT_getIterPlusSepSeparator(ATerm arg);
+ATerm     PT_getIterStarSepSeparator(ATerm arg);
 ATerm     PT_getSortSymbol(ATerm arg);
+ATerm     PT_getVarSymSymbol(ATerm arg);
+ATermList PT_getLayoutElems(ATerm arg);
 
 ATbool PT_prodHasLitAsRhs(ATerm arg);
 ATbool PT_prodHasLexSortAsRhs(ATerm arg);
 ATbool PT_prodHasLexLayoutAsRhs(ATerm arg);
 ATbool PT_prodHasCfLayoutAsRhs(ATerm arg);
 ATbool PT_prodHasCfOptLayoutAsRhs(ATerm arg);
+ATbool PT_prodHasVarSymAsRhs(ATerm arg);
+ATbool PT_prodHasVarSymAsLhs(ATerm arg);
+ATbool PT_prodHasLexAsLhs(ATerm arg);
 
 #endif /* _ME_PT_H */ 
