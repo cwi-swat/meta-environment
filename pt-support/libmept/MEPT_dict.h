2b07290b0967be1e65172bf977d6b127c137a177
Mark van den Brand <M.G.J.v.d.Brand@tue.nl>
2001-02-22 14:29:57 +0000

    - Files added in order to be independent of APIgen

---
+++ pt-support/libmept/MEPT_dict.h
@@ -0,0 +1,45 @@
+/*
+ * Generated at Wed Feb 21 16:22:41 2001
+ */
+
+#ifndef __MEPT_dict_H
+#define __MEPT_dict_H
+
+#include <aterm2.h>
+
+
+extern ATerm PT_patternSymbolsEmpty;
+extern ATerm PT_patternSymbolsList;
+extern ATerm PT_patternSymbolLayout;
+extern ATerm PT_patternSymbolVarSym;
+extern ATerm PT_patternSymbolIterStarSep;
+extern ATerm PT_patternSymbolIterPlusSep;
+extern ATerm PT_patternSymbolIterStar;
+extern ATerm PT_patternSymbolIterPlus;
+extern ATerm PT_patternSymbolSort;
+extern ATerm PT_patternSymbolAlt;
+extern ATerm PT_patternSymbolOpt;
+extern ATerm PT_patternSymbolLex;
+extern ATerm PT_patternSymbolCf;
+extern ATerm PT_patternSymbolLit;
+extern ATerm PT_patternArgsEmpty;
+extern ATerm PT_patternArgsList;
+extern ATerm PT_patternAttrBracket;
+extern ATerm PT_patternAttrCons;
+extern ATerm PT_patternAttrsSingle;
+extern ATerm PT_patternAttrsMany;
+extern ATerm PT_patternAttributesAttrs;
+extern ATerm PT_patternAttributesNoAttrs;
+extern ATerm PT_patternProductionDefault;
+extern ATerm PT_patternTreeAmb;
+extern ATerm PT_patternTreeFlatLexical;
+extern ATerm PT_patternTreeFlatLayout;
+extern ATerm PT_patternTreeLit;
+extern ATerm PT_patternTreeChar;
+extern ATerm PT_patternTreeList;
+extern ATerm PT_patternTreeAppl;
+extern ATerm PT_patternParseTreeTree;
+
+extern void init_MEPT_dict();
+
+#endif /* __MEPT_dict_H */
