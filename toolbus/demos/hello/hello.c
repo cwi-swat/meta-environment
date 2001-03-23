6060ccd256dfddf328bbe8f8b21b066b6f5588ec
Paul Klint <Paul.Klint@cwi.nl>
2001-03-23 14:34:28 +0000

    - Revision of Guide continued. - Removed double occurrence of java-adapter in ALL_ADAPTERS (configure.in)

--- toolbus/demos/hello/hello.c
+++ toolbus/demos/hello/hello.c
@@ -1,30 +1,27 @@
 /*  hello.c -- hello tool in C */
 
 #include <stdio.h>
-#include <aterm1.h>                   /* ATerms, level 1 interface */
-#include <atb-tool.h>                 /* ToolBus tool interface */
+#include <aterm1.h>                       /* ATerms, level 1 interface */
+#include <atb-tool.h>                     /* ToolBus tool interface */
 
-ATerm hello_handler(int conn, ATerm inp)      /* Handle input from ToolBus */
-{
-  ATerm arg, isig, osig;
+ATerm hello_handler(int conn, ATerm inp)  /* Handle input from ToolBus */
+{ ATerm arg, isig, osig;
 
   if(ATmatch(inp, "rec-eval(get-text)"))
     return ATmake("snd-value(\"Hello World, my first ToolBus tool in C!\n\")");
   if(ATmatch(inp, "rec-terminate(<term>)", &arg))
     exit(0);
   if(ATmatch(inp, "rec-do(signature(<term>,<term>))", &isig, &osig)){
-    return NULL;                      /* we are sloppy and don't do a signature check */
-  }
-  
+    return NULL;                         /* we don't do a signature check */
+  }  
   ATerror("hello: wrong input %t received\n", inp);
-  return NULL;                    /* Return NULL to indicate that no term
-                                       has to be sent back to the ToolBus */
+  return NULL;
 }
 
-int main(int argc, char *argv[])   /* main program of hello tool */
-{ ATerm bottomOfStack;
+int main(int argc, char *argv[])         /* main program of hello tool */
+{ ATerm bottomOfStack;                   /* marks stack bottom for ATerms */
 
-  ATBinit(argc, argv, &bottomOfStack);
+  ATBinit(argc, argv, &bottomOfStack);  /* initialize ToolBus library */
   if(ATBconnect(NULL, NULL, -1, hello_handler) >= 0){
     ATBeventloop();
   } else {
