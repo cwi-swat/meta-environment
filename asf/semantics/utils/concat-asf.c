62f8573cf3375dd89db212c09ee0e10b41672852
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2001-03-23 13:42:36 +0000

    * added concat-eqs tool

---
+++ asf/semantics/utils/concat-asf.c
@@ -0,0 +1,116 @@
+/*
+ * $Id$
+ */
+
+#include <stdio.h>
+#include <aterm1.h>
+#include <aterm2.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <ASF-utils.h>
+#include <PT-utils.h>
+
+static char myname[]    = "concat-asf";
+static char myversion[] = "1.0";
+static char myarguments[] = "ho:V";
+
+
+/*
+    Usage: displays helpful usage information
+ */
+void 
+usage(void)
+{
+    fprintf(stderr,
+	"\nConcat-asf concatenates parse trees of asf modules creating a\n"
+	"single list of equations. This list can be given to the evaluator.\n\n"
+        "Usage: concat-asf -h <file-1> ... <file-n> -o <output> -V"
+        "Options:\n"
+        "\t-h              display help information (usage)\n"
+        "\t-o filename     output to file (default stdout)\n"
+        "\t-V              reveal program version (i.e. %s)\n"
+        "\n"
+	"For example:\n" 
+	"concat-asf -o Module.eqs.baf Hoi.asf.asfix1 Hallo.asf.asfix1",
+        myversion);
+}
+
+#define MAX_MODULES 2500
+
+int 
+main (int argc, char **argv)
+{
+  int c; /* option character */
+  ATerm bottomOfStack;
+  char *inputs[MAX_MODULES] = { "-" };
+  int  nInputs = 0;
+  char *output = "-";
+  ASF_Equations equations;
+  ASF_CondEquationList list, alleqs;
+  PT_ParseTree parseTree;
+  PT_Tree tree;
+  int i;
+
+  if(argc == 1) { /* no arguments */
+    usage();
+    exit(1);
+  }
+
+  while ((c = getopt(argc, argv, myarguments)) != EOF) {
+    switch (c) {
+    case 'h':  
+      usage();                      
+      exit(0);
+    case 'o':  
+      output = strdup(optarg);    
+      break;
+    case 'V':  fprintf(stdout, "%s %s\n", myname, myversion);
+      exit(0);
+    default:
+      usage();
+      exit(1);
+    }
+  }
+
+  /* The optind variable indicates where getopt has stopped */
+  for(i = optind; i < argc; i++) {
+    if (nInputs < MAX_MODULES) {
+      inputs[nInputs++] = strdup(argv[i]);  
+    } else {
+      ATerror("Maximum number of %s modules exceeded.\n", MAX_MODULES);
+      exit(1);
+    }
+  }
+
+  if (nInputs == 0) {
+    nInputs = 1;
+    inputs[0] = strdup("-");
+  }
+
+  ATinit(argc, argv, &bottomOfStack); 
+  PT_initPTApi();
+  ASF_initASFApi();
+
+  alleqs = ASF_makeCondEquationListEmpty();
+
+  for (--nInputs; nInputs >= 0; nInputs--) {
+    parseTree = PT_makeParseTreeFromTerm(ATreadFromNamedFile(inputs[nInputs]));
+    if (parseTree == NULL) {
+      ATerror("Unable to read in %s\n", inputs[nInputs]);
+      exit(1);
+    }
+    free(inputs[nInputs]);
+
+    tree = PT_getParseTreeTree(parseTree);
+    equations = ASF_makeEquationsFromTerm(PT_makeTermFromTree(tree));
+
+    list = ASF_getEquationsList(equations);
+    alleqs = ASF_concatCondEquationList(list,alleqs);
+  }
+ 
+  ATwriteToNamedBinaryFile(ASF_makeTermFromCondEquationList(alleqs), output);
+ 
+  return 0;
+}
