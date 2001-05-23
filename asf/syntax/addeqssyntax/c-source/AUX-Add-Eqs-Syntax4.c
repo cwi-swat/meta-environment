f8bfcf78c8c67e38ac95212072caebc403fd6252
Mark van den Brand <M.G.J.v.d.Brand@tue.nl>
2001-05-23 12:54:10 +0000

    * Released version 0.2, and bumped into 0.3.

--- asf/syntax/addeqssyntax/c-source/AUX-Add-Eqs-Syntax4.c
+++ asf/syntax/addeqssyntax/c-source/AUX-Add-Eqs-Syntax4.c
@@ -3,30 +3,32 @@
 #include  "asc-support.h"
 static Symbol lf_AUX_Add_Eqs_Syntax4_1sym ;
 static ATerm lf_AUX_Add_Eqs_Syntax4_1 ( ATerm arg1 , ATerm arg2 ) ;
-static Symbol ef1sym ;
-static funcptr ef1 ;
-static Symbol lf2sym ;
-static ATerm lf2 ( ATerm arg1 ) ;
-static Symbol ef2sym ;
-static funcptr ef2 ;
-static Symbol lf3sym ;
-static ATerm lf3 ( ATerm arg1 ) ;
 static Symbol ef4sym ;
 static funcptr ef4 ;
-static Symbol ef5sym ;
-static funcptr ef5 ;
-static Symbol lf5sym ;
-static ATerm lf5 ( ATerm arg1 ) ;
+static Symbol ef2sym ;
+static funcptr ef2 ;
+static Symbol lf2sym ;
+static ATerm lf2 ( ATerm arg1 ) ;
+static Symbol ef3sym ;
+static funcptr ef3 ;
+static Symbol lf3sym ;
+static ATerm lf3 ( ATerm arg1 ) ;
 static Symbol ef6sym ;
 static funcptr ef6 ;
 static Symbol ef7sym ;
 static funcptr ef7 ;
-static Symbol lf6sym ;
-static ATerm lf6 ( ATerm arg1 ) ;
+static Symbol lf5sym ;
+static ATerm lf5 ( ATerm arg1 ) ;
 static Symbol ef8sym ;
 static funcptr ef8 ;
-static Symbol ef3sym ;
-static funcptr ef3 ;
+static Symbol ef1sym ;
+static funcptr ef1 ;
+static Symbol lf6sym ;
+static ATerm lf6 ( ATerm arg1 ) ;
+static Symbol ef9sym ;
+static funcptr ef9 ;
+static Symbol ef5sym ;
+static funcptr ef5 ;
 static Symbol lf4sym ;
 static ATerm lf4 ( ATerm arg1 ) ;
 void register_AUX_Add_Eqs_Syntax4 ( ) {
@@ -51,30 +53,35 @@ register_prod ( ATparse ( "listtype(sort(\"Import\"))" ) , lf5 , lf5sym ) ;
 register_prod ( ATparse ( "listtype(sort(\"CHAR\"))" ) , lf6 , lf6sym ) ;
 }
 void resolve_AUX_Add_Eqs_Syntax4 ( ) {
-ef1 = lookup_func ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[iter(sort(\"Module\"),w(\"\"),l(\"*\"))],w(\"\"),l(\"->\"),w(\"\"),sort(\"Definition\"),w(\"\"),no-attrs)" ) ) ;
-ef1sym = lookup_sym ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[iter(sort(\"Module\"),w(\"\"),l(\"*\"))],w(\"\"),l(\"->\"),w(\"\"),sort(\"Definition\"),w(\"\"),no-attrs)" ) ) ;
-ef2 = lookup_func ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[ql(\"module\"),w(\"\"),sort(\"ModuleName\"),w(\"\"),iter(sort(\"ImpSection\"),w(\"\"),l(\"*\")),w(\"\"),sort(\"Sections\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Module\"),w(\"\"),no-attrs)" ) ) ;
-ef2sym = lookup_sym ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[ql(\"module\"),w(\"\"),sort(\"ModuleName\"),w(\"\"),iter(sort(\"ImpSection\"),w(\"\"),l(\"*\")),w(\"\"),sort(\"Sections\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Module\"),w(\"\"),no-attrs)" ) ) ;
-ef3 = lookup_func ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[iter(sort(\"Section\"),w(\"\"),l(\"*\"))],w(\"\"),l(\"->\"),w(\"\"),sort(\"Sections\"),w(\"\"),no-attrs)" ) ) ;
-ef3sym = lookup_sym ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[iter(sort(\"Section\"),w(\"\"),l(\"*\"))],w(\"\"),l(\"->\"),w(\"\"),sort(\"Sections\"),w(\"\"),no-attrs)" ) ) ;
-ef4 = lookup_func ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[ql(\"imports\"),w(\"\"),sort(\"Imports\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"ImpSection\"),w(\"\"),no-attrs)" ) ) ;
-ef4sym = lookup_sym ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[ql(\"imports\"),w(\"\"),sort(\"Imports\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"ImpSection\"),w(\"\"),no-attrs)" ) ) ;
-ef5 = lookup_func ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[iter(sort(\"Import\"),w(\"\"),l(\"*\"))],w(\"\"),l(\"->\"),w(\"\"),sort(\"Imports\"),w(\"\"),no-attrs)" ) ) ;
-ef5sym = lookup_sym ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[iter(sort(\"Import\"),w(\"\"),l(\"*\"))],w(\"\"),l(\"->\"),w(\"\"),sort(\"Imports\"),w(\"\"),no-attrs)" ) ) ;
-ef6 = lookup_func ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[sort(\"ModuleName\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Import\"),w(\"\"),no-attrs)" ) ) ;
-ef6sym = lookup_sym ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[sort(\"ModuleName\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Import\"),w(\"\"),no-attrs)" ) ) ;
-ef7 = lookup_func ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[sort(\"ModuleId\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"ModuleName\"),w(\"\"),no-attrs)" ) ) ;
-ef7sym = lookup_sym ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[sort(\"ModuleId\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"ModuleName\"),w(\"\"),no-attrs)" ) ) ;
-ef8 = lookup_func ( ATreadFromString ( "prod(id(\"GEN-LexConsFuncs\"),w(\"\"),[ql(\"moduleid\"),w(\"\"),ql(\"(\"),w(\"\"),iter(sort(\"CHAR\"),w(\"\"),l(\"*\")),w(\"\"),ql(\")\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"ModuleId\"),w(\"\"),no-attrs)" ) ) ;
-ef8sym = lookup_sym ( ATreadFromString ( "prod(id(\"GEN-LexConsFuncs\"),w(\"\"),[ql(\"moduleid\"),w(\"\"),ql(\"(\"),w(\"\"),iter(sort(\"CHAR\"),w(\"\"),l(\"*\")),w(\"\"),ql(\")\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"ModuleId\"),w(\"\"),no-attrs)" ) ) ;
+ef1 = lookup_func ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[sort(\"ModuleId\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"ModuleName\"),w(\"\"),no-attrs)" ) ) ;
+ef1sym = lookup_sym ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[sort(\"ModuleId\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"ModuleName\"),w(\"\"),no-attrs)" ) ) ;
+ef2 = lookup_func ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[iter(sort(\"Module\"),w(\"\"),l(\"*\"))],w(\"\"),l(\"->\"),w(\"\"),sort(\"Definition\"),w(\"\"),no-attrs)" ) ) ;
+ef2sym = lookup_sym ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[iter(sort(\"Module\"),w(\"\"),l(\"*\"))],w(\"\"),l(\"->\"),w(\"\"),sort(\"Definition\"),w(\"\"),no-attrs)" ) ) ;
+ef3 = lookup_func ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[ql(\"module\"),w(\"\"),sort(\"ModuleName\"),w(\"\"),iter(sort(\"ImpSection\"),w(\"\"),l(\"*\")),w(\"\"),sort(\"Sections\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Module\"),w(\"\"),no-attrs)" ) ) ;
+ef3sym = lookup_sym ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[ql(\"module\"),w(\"\"),sort(\"ModuleName\"),w(\"\"),iter(sort(\"ImpSection\"),w(\"\"),l(\"*\")),w(\"\"),sort(\"Sections\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Module\"),w(\"\"),no-attrs)" ) ) ;
+ef4 = lookup_func ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[sort(\"ModuleId\"),w(\"\"),ql(\"[\"),w(\"\"),sort(\"Symbols\"),w(\"\"),ql(\"]\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"ModuleName\"),w(\"\"),no-attrs)" ) ) ;
+ef4sym = lookup_sym ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[sort(\"ModuleId\"),w(\"\"),ql(\"[\"),w(\"\"),sort(\"Symbols\"),w(\"\"),ql(\"]\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"ModuleName\"),w(\"\"),no-attrs)" ) ) ;
+ef5 = lookup_func ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[iter(sort(\"Section\"),w(\"\"),l(\"*\"))],w(\"\"),l(\"->\"),w(\"\"),sort(\"Sections\"),w(\"\"),no-attrs)" ) ) ;
+ef5sym = lookup_sym ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[iter(sort(\"Section\"),w(\"\"),l(\"*\"))],w(\"\"),l(\"->\"),w(\"\"),sort(\"Sections\"),w(\"\"),no-attrs)" ) ) ;
+ef6 = lookup_func ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[ql(\"imports\"),w(\"\"),sort(\"Imports\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"ImpSection\"),w(\"\"),no-attrs)" ) ) ;
+ef6sym = lookup_sym ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[ql(\"imports\"),w(\"\"),sort(\"Imports\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"ImpSection\"),w(\"\"),no-attrs)" ) ) ;
+ef7 = lookup_func ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[iter(sort(\"Import\"),w(\"\"),l(\"*\"))],w(\"\"),l(\"->\"),w(\"\"),sort(\"Imports\"),w(\"\"),no-attrs)" ) ) ;
+ef7sym = lookup_sym ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[iter(sort(\"Import\"),w(\"\"),l(\"*\"))],w(\"\"),l(\"->\"),w(\"\"),sort(\"Imports\"),w(\"\"),no-attrs)" ) ) ;
+ef8 = lookup_func ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[sort(\"ModuleName\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Import\"),w(\"\"),no-attrs)" ) ) ;
+ef8sym = lookup_sym ( ATreadFromString ( "prod(id(\"Modular-Sdf-Syntax\"),w(\"\"),[sort(\"ModuleName\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Import\"),w(\"\"),no-attrs)" ) ) ;
+ef9 = lookup_func ( ATreadFromString ( "prod(id(\"GEN-LexConsFuncs\"),w(\"\"),[ql(\"moduleid\"),w(\"\"),ql(\"(\"),w(\"\"),iter(sort(\"CHAR\"),w(\"\"),l(\"*\")),w(\"\"),ql(\")\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"ModuleId\"),w(\"\"),no-attrs)" ) ) ;
+ef9sym = lookup_sym ( ATreadFromString ( "prod(id(\"GEN-LexConsFuncs\"),w(\"\"),[ql(\"moduleid\"),w(\"\"),ql(\"(\"),w(\"\"),iter(sort(\"CHAR\"),w(\"\"),l(\"*\")),w(\"\"),ql(\")\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"ModuleId\"),w(\"\"),no-attrs)" ) ) ;
 }
 void init_AUX_Add_Eqs_Syntax4 ( ) {
 }
 ATerm lf_AUX_Add_Eqs_Syntax4_1 ( ATerm arg0 , ATerm arg1 ) {
 {
-ATerm tmp [ 11 ] ;
+ATerm tmp [ 13 ] ;
 FUNC_ENTRY ( lf_AUX_Add_Eqs_Syntax4_1sym , ATmakeAppl ( lf_AUX_Add_Eqs_Syntax4_1sym , arg0 , arg1 ) ) ;
-if ( check_sym ( arg1 , ef1sym ) ) {
+if ( check_sym ( arg0 , ef1sym ) ) {
+{
+ATerm atmp00 = arg_0 ( arg0 ) ;
+if ( check_sym ( arg1 , ef2sym ) ) {
 {
 ATerm atmp10 = arg_0 ( arg1 ) ;
 if ( check_sym ( atmp10 , lf2sym ) ) {
@@ -88,37 +95,81 @@ while ( not_empty_list ( tmp [ 0 ] ) ) {
 ( atmp10010 = list_head ( tmp [ 0 ] ) ) ;
 ( tmp [ 0 ] = list_tail ( tmp [ 0 ] ) ) ;
 {
-if ( check_sym ( atmp10010 , ef2sym ) ) {
+if ( check_sym ( atmp10010 , ef3sym ) ) {
 ( tmp [ 1 ] = arg_0 ( atmp10010 ) ) ;
 ( tmp [ 2 ] = arg_1 ( atmp10010 ) ) ;
 ( tmp [ 3 ] = arg_2 ( atmp10010 ) ) ;
+if ( check_sym ( tmp [ 1 ] , ef1sym ) ) {
+( tmp [ 4 ] = arg_0 ( tmp [ 1 ] ) ) ;
 {
 if ( check_sym ( tmp [ 2 ] , lf3sym ) ) {
-( tmp [ 4 ] = arg_0 ( tmp [ 2 ] ) ) ;
+( tmp [ 5 ] = arg_0 ( tmp [ 2 ] ) ) ;
 {
-if ( check_sym ( tmp [ 3 ] , ef3sym ) ) {
-( tmp [ 5 ] = arg_0 ( tmp [ 3 ] ) ) ;
+if ( check_sym ( tmp [ 3 ] , ef5sym ) ) {
+( tmp [ 6 ] = arg_0 ( tmp [ 3 ] ) ) ;
 {
-if ( check_sym ( tmp [ 5 ] , lf4sym ) ) {
-( tmp [ 6 ] = arg_0 ( tmp [ 5 ] ) ) ;
+if ( check_sym ( tmp [ 6 ] , lf4sym ) ) {
+( tmp [ 7 ] = arg_0 ( tmp [ 6 ] ) ) ;
 {
-if ( ! not_empty_list ( tmp [ 4 ] ) ) {
-if ( term_equal ( arg0 , tmp [ 1 ] ) ) {
-FUNC_EXIT ( make_nf1 ( ef1sym , lf2 ( cons ( slice ( atmp1000 [ 0 ] , atmp1000 [ 1 ] ) , cons ( make_list ( ( * ef2 ) ( arg0 , lf3 ( make_list ( ( * ef4 ) ( make_nf1 ( ef5sym , lf5 ( make_list ( ( * ef6 ) ( ( * ef7 ) ( ( * ef8 ) ( lf6 ( ( ATerm ) ATmakeList ( 13 , char_table [ 71 ] , char_table [ 69 ] , char_table [ 78 ] , char_table [ 45 ] , char_table [ 69 ] , char_table [ 113 ] , char_table [ 117 ] , char_table [ 97 ] , char_table [ 116 ] , char_table [ 105 ] , char_table [ 111 ] , char_table [ 110 ] , char_table [ 115 ] ) ) ) ) ) ) ) ) ) ) ) , make_nf1 ( ef3sym , lf4 ( make_list ( tmp [ 6 ] ) ) ) ) ) , tmp [ 0 ] ) ) ) ) ) ;
+if ( ! not_empty_list ( tmp [ 5 ] ) ) {
+if ( term_equal ( atmp00 , tmp [ 4 ] ) ) {
+FUNC_EXIT ( make_nf1 ( ef2sym , lf2 ( cons ( slice ( atmp1000 [ 0 ] , atmp1000 [ 1 ] ) , cons ( make_list ( ( * ef3 ) ( arg0 , lf3 ( make_list ( ( * ef6 ) ( make_nf1 ( ef7sym , lf5 ( make_list ( ( * ef8 ) ( ( * ef1 ) ( ( * ef9 ) ( lf6 ( ( ATerm ) ATmakeList ( 13 , char_table [ 71 ] , char_table [ 69 ] , char_table [ 78 ] , char_table [ 45 ] , char_table [ 69 ] , char_table [ 113 ] , char_table [ 117 ] , char_table [ 97 ] , char_table [ 116 ] , char_table [ 105 ] , char_table [ 111 ] , char_table [ 110 ] , char_table [ 115 ] ) ) ) ) ) ) ) ) ) ) ) , make_nf1 ( ef5sym , lf4 ( make_list ( tmp [ 7 ] ) ) ) ) ) , tmp [ 0 ] ) ) ) ) ) ;
 }
 }
-if ( is_single_element ( tmp [ 4 ] ) ) {
-( tmp [ 7 ] = list_head ( tmp [ 4 ] ) ) ;
-if ( check_sym ( tmp [ 7 ] , ef4sym ) ) {
-( tmp [ 8 ] = arg_0 ( tmp [ 7 ] ) ) ;
-if ( check_sym ( tmp [ 8 ] , ef5sym ) ) {
+if ( is_single_element ( tmp [ 5 ] ) ) {
+( tmp [ 8 ] = list_head ( tmp [ 5 ] ) ) ;
+if ( check_sym ( tmp [ 8 ] , ef6sym ) ) {
 ( tmp [ 9 ] = arg_0 ( tmp [ 8 ] ) ) ;
-if ( check_sym ( tmp [ 9 ] , lf5sym ) ) {
+if ( check_sym ( tmp [ 9 ] , ef7sym ) ) {
 ( tmp [ 10 ] = arg_0 ( tmp [ 9 ] ) ) ;
-if ( term_equal ( arg0 , tmp [ 1 ] ) ) {
-FUNC_EXIT ( make_nf1 ( ef1sym , lf2 ( cons ( slice ( atmp1000 [ 0 ] , atmp1000 [ 1 ] ) , cons ( make_list ( ( * ef2 ) ( arg0 , lf3 ( make_list ( ( * ef4 ) ( make_nf1 ( ef5sym , lf5 ( cons ( make_list ( tmp [ 10 ] ) , make_list ( ( * ef6 ) ( ( * ef7 ) ( ( * ef8 ) ( lf6 ( ( ATerm ) ATmakeList ( 13 , char_table [ 71 ] , char_table [ 69 ] , char_table [ 78 ] , char_table [ 45 ] , char_table [ 69 ] , char_table [ 113 ] , char_table [ 117 ] , char_table [ 97 ] , char_table [ 116 ] , char_table [ 105 ] , char_table [ 111 ] , char_table [ 110 ] , char_table [ 115 ] ) ) ) ) ) ) ) ) ) ) ) ) , make_nf1 ( ef3sym , lf4 ( make_list ( tmp [ 6 ] ) ) ) ) ) , tmp [ 0 ] ) ) ) ) ) ;
+if ( check_sym ( tmp [ 10 ] , lf5sym ) ) {
+( tmp [ 11 ] = arg_0 ( tmp [ 10 ] ) ) ;
+if ( term_equal ( atmp00 , tmp [ 4 ] ) ) {
+FUNC_EXIT ( make_nf1 ( ef2sym , lf2 ( cons ( slice ( atmp1000 [ 0 ] , atmp1000 [ 1 ] ) , cons ( make_list ( ( * ef3 ) ( arg0 , lf3 ( make_list ( ( * ef6 ) ( make_nf1 ( ef7sym , lf5 ( cons ( make_list ( tmp [ 11 ] ) , make_list ( ( * ef8 ) ( ( * ef1 ) ( ( * ef9 ) ( lf6 ( ( ATerm ) ATmakeList ( 13 , char_table [ 71 ] , char_table [ 69 ] , char_table [ 78 ] , char_table [ 45 ] , char_table [ 69 ] , char_table [ 113 ] , char_table [ 117 ] , char_table [ 97 ] , char_table [ 116 ] , char_table [ 105 ] , char_table [ 111 ] , char_table [ 110 ] , char_table [ 115 ] ) ) ) ) ) ) ) ) ) ) ) ) , make_nf1 ( ef5sym , lf4 ( make_list ( tmp [ 7 ] ) ) ) ) ) , tmp [ 0 ] ) ) ) ) ) ;
 }
 }
+}
+}
+}
+}
+}
+}
+}
+}
+}
+}
+}
+if ( check_sym ( tmp [ 1 ] , ef4sym ) ) {
+( tmp [ 4 ] = arg_0 ( tmp [ 1 ] ) ) ;
+( tmp [ 5 ] = arg_1 ( tmp [ 1 ] ) ) ;
+{
+if ( check_sym ( tmp [ 2 ] , lf3sym ) ) {
+( tmp [ 6 ] = arg_0 ( tmp [ 2 ] ) ) ;
+{
+if ( check_sym ( tmp [ 3 ] , ef5sym ) ) {
+( tmp [ 7 ] = arg_0 ( tmp [ 3 ] ) ) ;
+{
+if ( check_sym ( tmp [ 7 ] , lf4sym ) ) {
+( tmp [ 8 ] = arg_0 ( tmp [ 7 ] ) ) ;
+{
+if ( is_single_element ( tmp [ 6 ] ) ) {
+( tmp [ 9 ] = list_head ( tmp [ 6 ] ) ) ;
+if ( check_sym ( tmp [ 9 ] , ef6sym ) ) {
+( tmp [ 10 ] = arg_0 ( tmp [ 9 ] ) ) ;
+if ( check_sym ( tmp [ 10 ] , ef7sym ) ) {
+( tmp [ 11 ] = arg_0 ( tmp [ 10 ] ) ) ;
+if ( check_sym ( tmp [ 11 ] , lf5sym ) ) {
+( tmp [ 12 ] = arg_0 ( tmp [ 11 ] ) ) ;
+if ( term_equal ( atmp00 , tmp [ 4 ] ) ) {
+FUNC_EXIT ( make_nf1 ( ef2sym , lf2 ( cons ( slice ( atmp1000 [ 0 ] , atmp1000 [ 1 ] ) , cons ( make_list ( ( * ef3 ) ( ( * ef4 ) ( atmp00 , tmp [ 5 ] ) , lf3 ( make_list ( ( * ef6 ) ( make_nf1 ( ef7sym , lf5 ( cons ( make_list ( tmp [ 12 ] ) , make_list ( ( * ef8 ) ( ( * ef1 ) ( ( * ef9 ) ( lf6 ( ( ATerm ) ATmakeList ( 13 , char_table [ 71 ] , char_table [ 69 ] , char_table [ 78 ] , char_table [ 45 ] , char_table [ 69 ] , char_table [ 113 ] , char_table [ 117 ] , char_table [ 97 ] , char_table [ 116 ] , char_table [ 105 ] , char_table [ 111 ] , char_table [ 110 ] , char_table [ 115 ] ) ) ) ) ) ) ) ) ) ) ) ) , make_nf1 ( ef5sym , lf4 ( make_list ( tmp [ 8 ] ) ) ) ) ) , tmp [ 0 ] ) ) ) ) ) ;
+}
+}
+}
+}
+}
+if ( ! not_empty_list ( tmp [ 6 ] ) ) {
+if ( term_equal ( atmp00 , tmp [ 4 ] ) ) {
+FUNC_EXIT ( make_nf1 ( ef2sym , lf2 ( cons ( slice ( atmp1000 [ 0 ] , atmp1000 [ 1 ] ) , cons ( make_list ( ( * ef3 ) ( ( * ef4 ) ( atmp00 , tmp [ 5 ] ) , lf3 ( make_list ( ( * ef6 ) ( make_nf1 ( ef7sym , lf5 ( make_list ( ( * ef8 ) ( ( * ef1 ) ( ( * ef9 ) ( lf6 ( ( ATerm ) ATmakeList ( 13 , char_table [ 71 ] , char_table [ 69 ] , char_table [ 78 ] , char_table [ 45 ] , char_table [ 69 ] , char_table [ 113 ] , char_table [ 117 ] , char_table [ 97 ] , char_table [ 116 ] , char_table [ 105 ] , char_table [ 111 ] , char_table [ 110 ] , char_table [ 115 ] ) ) ) ) ) ) ) ) ) ) ) , make_nf1 ( ef5sym , lf4 ( make_list ( tmp [ 8 ] ) ) ) ) ) , tmp [ 0 ] ) ) ) ) ) ;
 }
 }
 }
@@ -136,6 +187,8 @@ FUNC_EXIT ( make_nf1 ( ef1sym , lf2 ( cons ( slice ( atmp1000 [ 0 ] , atmp1000 [
 }
 }
 ( tmp [ 0 ] = arg_0 ( atmp10 ) ) ;
+}
+}
 }
 }
 }
