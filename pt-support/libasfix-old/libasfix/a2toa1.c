99d31932bf40939374616ec08ffd339509b5736a
Mark van den Brand <M.G.J.v.d.Brand@tue.nl>
2001-05-29 11:10:17 +0000

    * added a function symbol to the result of a2toa1 tool, in order      to be ToolBus-0.16 compliant.

--- pt-support/libasfix-old/libasfix/a2toa1.c
+++ pt-support/libasfix-old/libasfix/a2toa1.c
@@ -1,23 +1,3 @@
-/*
-
-    AsFix -- The AsFix (ASF+SDF Prefix) library
-    Copyright (C) 2000  Stichting Mathematisch Centrum, Amsterdam, The Netherlands. 
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
-
-*/
 /*  $Id$  */
 
 #include <stdlib.h>
@@ -1029,7 +1009,7 @@ ATerm internal_trans_top(ATerm t, ATbool standalone, ATbool interpret_cons)
                     trm_yield(arg[0]),
                     internal_trans(arg[1],interpret_cons), trm_yield(arg[2]));
     if (standalone == ATfalse)
-      return ATmake("snd-value(<term>)", result);
+      return ATmake("snd-value(converted(<term>))", result);
     else
       return result;
   }
