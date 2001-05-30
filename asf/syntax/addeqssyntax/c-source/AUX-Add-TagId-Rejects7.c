c9ef99ca7c3923d466ae1d8157ad8dbc17d2db58
Mark van den Brand <M.G.J.v.d.Brand@tue.nl>
2001-05-30 10:05:00 +0000

    * Fixed a "bug" in addeqssyntax with respect to prefix functions.

--- asf/syntax/addeqssyntax/c-source/AUX-Add-TagId-Rejects7.c
+++ asf/syntax/addeqssyntax/c-source/AUX-Add-TagId-Rejects7.c
@@ -9,18 +9,26 @@ static Symbol ef4sym ;
 static funcptr ef4 ;
 static Symbol ef5sym ;
 static funcptr ef5 ;
-static Symbol ef2sym ;
-static funcptr ef2 ;
-static Symbol ef3sym ;
-static funcptr ef3 ;
-static Symbol lf3sym ;
-static ATerm lf3 ( ATerm arg1 ) ;
 static Symbol ef6sym ;
 static funcptr ef6 ;
 static Symbol lf4sym ;
 static ATerm lf4 ( ATerm arg1 ) ;
 static Symbol ef7sym ;
 static funcptr ef7 ;
+static Symbol ef8sym ;
+static funcptr ef8 ;
+static Symbol ef2sym ;
+static funcptr ef2 ;
+static Symbol ef3sym ;
+static funcptr ef3 ;
+static Symbol lf3sym ;
+static ATerm lf3 ( ATerm arg1 ) ;
+static Symbol ef9sym ;
+static funcptr ef9 ;
+static Symbol lf5sym ;
+static ATerm lf5 ( ATerm arg1 ) ;
+static Symbol ef10sym ;
+static funcptr ef10 ;
 static Symbol ef1sym ;
 static funcptr ef1 ;
 static Symbol lf2sym ;
@@ -31,17 +39,20 @@ lf_AUX_Add_TagId_Rejects7_1_recursivesym = ATmakeSymbol ( "prod(id(\"Add-TagId-R
 ATprotectSymbol ( lf_AUX_Add_TagId_Rejects7_1_recursivesym ) ;
 lf_AUX_Add_TagId_Rejects7_1sym = ATmakeSymbol ( "prod(id(\"Add-TagId-Rejects\"),w(\"\"),[ql(\"add-tir-for-productions\"),w(\"\"),ql(\"(\"),w(\"\"),sort(\"Productions\"),w(\"\"),ql(\")\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Productions\"),w(\"\"),no-attrs)" , 1 , ATtrue ) ;
 ATprotectSymbol ( lf_AUX_Add_TagId_Rejects7_1sym ) ;
+lf4sym = ATmakeSymbol ( "listtype(sort(\"Symbol\"),ql(\",\"))" , 1 , ATtrue ) ;
+ATprotectSymbol ( lf4sym ) ;
 lf3sym = ATmakeSymbol ( "listtype(sort(\"Symbol\"))" , 1 , ATtrue ) ;
 ATprotectSymbol ( lf3sym ) ;
-lf4sym = ATmakeSymbol ( "listtype(sort(\"Attribute\"),ql(\",\"))" , 1 , ATtrue ) ;
-ATprotectSymbol ( lf4sym ) ;
+lf5sym = ATmakeSymbol ( "listtype(sort(\"Attribute\"),ql(\",\"))" , 1 , ATtrue ) ;
+ATprotectSymbol ( lf5sym ) ;
 lf2sym = ATmakeSymbol ( "listtype(sort(\"Production\"))" , 1 , ATtrue ) ;
 ATprotectSymbol ( lf2sym ) ;
 register_prod ( ATparse ( "prod(id(\"Add-TagId-Rejects\"),w(\"\"),[ql(\"add-tir-for-productions\"),w(\"\"),ql(\"(\"),w(\"\"),sort(\"Productions\"),w(\"\"),ql(\")\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Productions\"),w(\"\"),no-attrs)" ) , lf_AUX_Add_TagId_Rejects7_1 , lf_AUX_Add_TagId_Rejects7_1sym ) ;
 register_prod ( ATparse ( "prod(id(\"Add-TagId-Rejects\"),w(\"\"),[ql(\"add-tir-for-productions\"),w(\"\"),ql(\"(\"),w(\"\"),sort(\"Productions\"),w(\"\"),ql(\")\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Productions\"),w(\"\"),no-attrs)" ) , lf_AUX_Add_TagId_Rejects7_1_recursive , lf_AUX_Add_TagId_Rejects7_1_recursivesym ) ;
 register_prod ( ATparse ( "listtype(sort(\"Production\"))" ) , lf2 , lf2sym ) ;
 register_prod ( ATparse ( "listtype(sort(\"Symbol\"))" ) , lf3 , lf3sym ) ;
-register_prod ( ATparse ( "listtype(sort(\"Attribute\"),ql(\",\"))" ) , lf4 , lf4sym ) ;
+register_prod ( ATparse ( "listtype(sort(\"Symbol\"),ql(\",\"))" ) , lf4 , lf4sym ) ;
+register_prod ( ATparse ( "listtype(sort(\"Attribute\"),ql(\",\"))" ) , lf5 , lf5sym ) ;
 }
 void resolve_AUX_Add_TagId_Rejects7 ( ) {
 ef1 = lookup_func ( ATreadFromString ( "prod(id(\"Kernel-Sdf-Syntax\"),w(\"\"),[iter(sort(\"Production\"),w(\"\"),l(\"*\"))],w(\"\"),l(\"->\"),w(\"\"),sort(\"Productions\"),w(\"\"),no-attrs)" ) ) ;
@@ -54,10 +65,16 @@ ef4 = lookup_func ( ATreadFromString ( "prod(id(\"Add-TagId-Rejects\"),w(\"\"),[
 ef4sym = lookup_sym ( ATreadFromString ( "prod(id(\"Add-TagId-Rejects\"),w(\"\"),[ql(\"add-tir-for-production\"),w(\"\"),ql(\"(\"),w(\"\"),sort(\"Production\"),w(\"\"),ql(\")\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Productions\"),w(\"\"),no-attrs)" ) ) ;
 ef5 = lookup_func ( ATreadFromString ( "prod(id(\"Kernel-Sdf-Syntax\"),w(\"\"),[],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attributes\"),w(\"\"),no-attrs)" ) ) ;
 ef5sym = lookup_sym ( ATreadFromString ( "prod(id(\"Kernel-Sdf-Syntax\"),w(\"\"),[],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attributes\"),w(\"\"),no-attrs)" ) ) ;
-ef6 = lookup_func ( ATreadFromString ( "prod(id(\"Kernel-Sdf-Syntax\"),w(\"\"),[ql(\"{\"),w(\"\"),iter-sep(l(\"{\"),w(\"\"),sort(\"Attribute\"),w(\"\"),ql(\",\"),w(\"\"),l(\"}\"),w(\"\"),l(\"*\")),w(\"\"),ql(\"}\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attributes\"),w(\"\"),no-attrs)" ) ) ;
-ef6sym = lookup_sym ( ATreadFromString ( "prod(id(\"Kernel-Sdf-Syntax\"),w(\"\"),[ql(\"{\"),w(\"\"),iter-sep(l(\"{\"),w(\"\"),sort(\"Attribute\"),w(\"\"),ql(\",\"),w(\"\"),l(\"}\"),w(\"\"),l(\"*\")),w(\"\"),ql(\"}\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attributes\"),w(\"\"),no-attrs)" ) ) ;
-ef7 = lookup_func ( ATreadFromString ( "prod(id(\"Restrictions-Sdf-Syntax\"),w(\"\"),[ql(\"reject\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attribute\"),w(\"\"),no-attrs)" ) ) ;
-ef7sym = lookup_sym ( ATreadFromString ( "prod(id(\"Restrictions-Sdf-Syntax\"),w(\"\"),[ql(\"reject\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attribute\"),w(\"\"),no-attrs)" ) ) ;
+ef6 = lookup_func ( ATreadFromString ( "prod(id(\"Literals-Sdf-Syntax\"),w(\"\"),[sort(\"Literal\"),w(\"\"),ql(\"(\"),w(\"\"),iter-sep(l(\"{\"),w(\"\"),sort(\"Symbol\"),w(\"\"),ql(\",\"),w(\"\"),l(\"}\"),w(\"\"),l(\"*\")),w(\"\"),ql(\")\"),w(\"\"),ql(\"->\"),w(\"\"),sort(\"Symbol\"),w(\"\"),sort(\"Attributes\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Production\"),w(\"\"),attrs(l(\"{\"),w(\"\"),[l(\"avoid\")],w(\"\"),l(\"}\")))" ) ) ;
+ef6sym = lookup_sym ( ATreadFromString ( "prod(id(\"Literals-Sdf-Syntax\"),w(\"\"),[sort(\"Literal\"),w(\"\"),ql(\"(\"),w(\"\"),iter-sep(l(\"{\"),w(\"\"),sort(\"Symbol\"),w(\"\"),ql(\",\"),w(\"\"),l(\"}\"),w(\"\"),l(\"*\")),w(\"\"),ql(\")\"),w(\"\"),ql(\"->\"),w(\"\"),sort(\"Symbol\"),w(\"\"),sort(\"Attributes\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Production\"),w(\"\"),attrs(l(\"{\"),w(\"\"),[l(\"avoid\")],w(\"\"),l(\"}\")))" ) ) ;
+ef7 = lookup_func ( ATreadFromString ( "prod(id(\"Add-TagId-Rejects\"),w(\"\"),[ql(\"add-tir-for-symbol\"),w(\"\"),ql(\"(\"),w(\"\"),sort(\"Symbol\"),w(\"\"),ql(\")\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Productions\"),w(\"\"),no-attrs)" ) ) ;
+ef7sym = lookup_sym ( ATreadFromString ( "prod(id(\"Add-TagId-Rejects\"),w(\"\"),[ql(\"add-tir-for-symbol\"),w(\"\"),ql(\"(\"),w(\"\"),sort(\"Symbol\"),w(\"\"),ql(\")\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Productions\"),w(\"\"),no-attrs)" ) ) ;
+ef8 = lookup_func ( ATreadFromString ( "prod(id(\"Literals-Sdf-Syntax\"),w(\"\"),[sort(\"Literal\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Symbol\"),w(\"\"),no-attrs)" ) ) ;
+ef8sym = lookup_sym ( ATreadFromString ( "prod(id(\"Literals-Sdf-Syntax\"),w(\"\"),[sort(\"Literal\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Symbol\"),w(\"\"),no-attrs)" ) ) ;
+ef9 = lookup_func ( ATreadFromString ( "prod(id(\"Kernel-Sdf-Syntax\"),w(\"\"),[ql(\"{\"),w(\"\"),iter-sep(l(\"{\"),w(\"\"),sort(\"Attribute\"),w(\"\"),ql(\",\"),w(\"\"),l(\"}\"),w(\"\"),l(\"*\")),w(\"\"),ql(\"}\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attributes\"),w(\"\"),no-attrs)" ) ) ;
+ef9sym = lookup_sym ( ATreadFromString ( "prod(id(\"Kernel-Sdf-Syntax\"),w(\"\"),[ql(\"{\"),w(\"\"),iter-sep(l(\"{\"),w(\"\"),sort(\"Attribute\"),w(\"\"),ql(\",\"),w(\"\"),l(\"}\"),w(\"\"),l(\"*\")),w(\"\"),ql(\"}\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attributes\"),w(\"\"),no-attrs)" ) ) ;
+ef10 = lookup_func ( ATreadFromString ( "prod(id(\"Restrictions-Sdf-Syntax\"),w(\"\"),[ql(\"reject\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attribute\"),w(\"\"),no-attrs)" ) ) ;
+ef10sym = lookup_sym ( ATreadFromString ( "prod(id(\"Restrictions-Sdf-Syntax\"),w(\"\"),[ql(\"reject\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Attribute\"),w(\"\"),no-attrs)" ) ) ;
 }
 static ATerm constant0 = NULL ;
 static ATerm constant1 = NULL ;
@@ -75,10 +92,7 @@ ATerm atmp00 = arg_0 ( arg0 ) ;
 if ( check_sym ( atmp00 , lf2sym ) ) {
 {
 ATerm atmp000 = arg_0 ( atmp00 ) ;
-if ( ! not_empty_list ( atmp000 ) ) {
-FUNC_EXIT_CONST ( constant0 , make_nf1 ( ef1sym , lf2 ( make_list ( null ( ) ) ) ) ) ;
-}
-else {
+if ( not_empty_list ( atmp000 ) ) {
 ( tmp [ 0 ] = list_head ( atmp000 ) ) ;
 ( tmp [ 1 ] = list_tail ( atmp000 ) ) ;
 if ( check_sym ( tmp [ 0 ] , ef2sym ) ) {
@@ -89,14 +103,14 @@ if ( check_sym ( tmp [ 2 ] , ef3sym ) ) {
 ( tmp [ 5 ] = arg_0 ( tmp [ 2 ] ) ) ;
 if ( check_sym ( tmp [ 5 ] , lf3sym ) ) {
 ( tmp [ 6 ] = arg_0 ( tmp [ 5 ] ) ) ;
-if ( check_sym ( tmp [ 4 ] , ef6sym ) ) {
+if ( check_sym ( tmp [ 4 ] , ef9sym ) ) {
 ( tmp [ 7 ] = arg_0 ( tmp [ 4 ] ) ) ;
-if ( check_sym ( tmp [ 7 ] , lf4sym ) ) {
+if ( check_sym ( tmp [ 7 ] , lf5sym ) ) {
 ( tmp [ 8 ] = arg_0 ( tmp [ 7 ] ) ) ;
 if ( is_single_element ( tmp [ 6 ] ) ) {
 ( tmp [ 9 ] = list_head ( tmp [ 6 ] ) ) ;
 if ( is_single_element ( tmp [ 8 ] ) ) {
-if ( term_equal ( list_head ( tmp [ 8 ] ) , ( constant1 ? constant1 : ( constant1 = ( * ef7 ) ( ) ) ) ) ) {
+if ( term_equal ( list_head ( tmp [ 8 ] ) , ( constant0 ? constant0 : ( constant0 = ( * ef10 ) ( ) ) ) ) ) {
 ( tmp [ 10 ] = lf_AUX_Add_TagId_Rejects7_1_recursive ( make_list ( tmp [ 1 ] ) ) ) ;
 ( tmp [ 11 ] = arg_0 ( tmp [ 10 ] ) ) ;
 ( tmp [ 12 ] = arg_0 ( tmp [ 11 ] ) ) ;
@@ -109,6 +123,29 @@ FUNC_EXIT ( make_nf1 ( ef1sym , lf2 ( make_list ( tmp [ 12 ] ) ) ) ) ;
 }
 }
 }
+if ( check_sym ( tmp [ 0 ] , ef6sym ) ) {
+( tmp [ 2 ] = arg_0 ( tmp [ 0 ] ) ) ;
+( tmp [ 3 ] = arg_1 ( tmp [ 0 ] ) ) ;
+( tmp [ 4 ] = arg_2 ( tmp [ 0 ] ) ) ;
+( tmp [ 5 ] = arg_3 ( tmp [ 0 ] ) ) ;
+if ( check_sym ( tmp [ 3 ] , lf4sym ) ) {
+( tmp [ 6 ] = arg_0 ( tmp [ 3 ] ) ) ;
+( tmp [ 7 ] = ( * ef7 ) ( ( * ef8 ) ( tmp [ 2 ] ) ) ) ;
+if ( check_sym ( tmp [ 7 ] , ef1sym ) ) {
+( tmp [ 8 ] = arg_0 ( tmp [ 7 ] ) ) ;
+if ( check_sym ( tmp [ 8 ] , lf2sym ) ) {
+( tmp [ 9 ] = arg_0 ( tmp [ 8 ] ) ) ;
+( tmp [ 10 ] = lf_AUX_Add_TagId_Rejects7_1_recursive ( make_list ( tmp [ 1 ] ) ) ) ;
+( tmp [ 11 ] = arg_0 ( tmp [ 10 ] ) ) ;
+( tmp [ 12 ] = arg_0 ( tmp [ 11 ] ) ) ;
+FUNC_EXIT ( make_nf1 ( ef1sym , lf2 ( cons ( make_list ( tmp [ 9 ] ) , make_list ( tmp [ 12 ] ) ) ) ) ) ;
+}
+}
+}
+}
+}
+else {
+FUNC_EXIT_CONST ( constant1 , make_nf1 ( ef1sym , lf2 ( make_list ( null ( ) ) ) ) ) ;
 }
 }
 }
@@ -161,10 +198,7 @@ FUNC_ENTRY ( lf_AUX_Add_TagId_Rejects7_1_recursivesym , ATmakeAppl ( lf_AUX_Add_
 ATerm atmp00 = arg0 ;
 {
 ATerm atmp000 = atmp00 ;
-if ( ! not_empty_list ( atmp000 ) ) {
-FUNC_EXIT_CONST ( constant0 , make_nf1 ( ef1sym , lf2 ( make_list ( null ( ) ) ) ) ) ;
-}
-else {
+if ( not_empty_list ( atmp000 ) ) {
 ( tmp [ 0 ] = list_head ( atmp000 ) ) ;
 ( tmp [ 1 ] = list_tail ( atmp000 ) ) ;
 if ( check_sym ( tmp [ 0 ] , ef2sym ) ) {
@@ -175,14 +209,14 @@ if ( check_sym ( tmp [ 2 ] , ef3sym ) ) {
 ( tmp [ 5 ] = arg_0 ( tmp [ 2 ] ) ) ;
 if ( check_sym ( tmp [ 5 ] , lf3sym ) ) {
 ( tmp [ 6 ] = arg_0 ( tmp [ 5 ] ) ) ;
-if ( check_sym ( tmp [ 4 ] , ef6sym ) ) {
+if ( check_sym ( tmp [ 4 ] , ef9sym ) ) {
 ( tmp [ 7 ] = arg_0 ( tmp [ 4 ] ) ) ;
-if ( check_sym ( tmp [ 7 ] , lf4sym ) ) {
+if ( check_sym ( tmp [ 7 ] , lf5sym ) ) {
 ( tmp [ 8 ] = arg_0 ( tmp [ 7 ] ) ) ;
 if ( is_single_element ( tmp [ 6 ] ) ) {
 ( tmp [ 9 ] = list_head ( tmp [ 6 ] ) ) ;
 if ( is_single_element ( tmp [ 8 ] ) ) {
-if ( term_equal ( list_head ( tmp [ 8 ] ) , ( constant1 ? constant1 : ( constant1 = ( * ef7 ) ( ) ) ) ) ) {
+if ( term_equal ( list_head ( tmp [ 8 ] ) , ( constant0 ? constant0 : ( constant0 = ( * ef10 ) ( ) ) ) ) ) {
 ( tmp [ 10 ] = lf_AUX_Add_TagId_Rejects7_1_recursive ( make_list ( tmp [ 1 ] ) ) ) ;
 ( tmp [ 11 ] = arg_0 ( tmp [ 10 ] ) ) ;
 ( tmp [ 12 ] = arg_0 ( tmp [ 11 ] ) ) ;
@@ -195,6 +229,29 @@ FUNC_EXIT ( make_nf1 ( ef1sym , lf2 ( make_list ( tmp [ 12 ] ) ) ) ) ;
 }
 }
 }
+if ( check_sym ( tmp [ 0 ] , ef6sym ) ) {
+( tmp [ 2 ] = arg_0 ( tmp [ 0 ] ) ) ;
+( tmp [ 3 ] = arg_1 ( tmp [ 0 ] ) ) ;
+( tmp [ 4 ] = arg_2 ( tmp [ 0 ] ) ) ;
+( tmp [ 5 ] = arg_3 ( tmp [ 0 ] ) ) ;
+if ( check_sym ( tmp [ 3 ] , lf4sym ) ) {
+( tmp [ 6 ] = arg_0 ( tmp [ 3 ] ) ) ;
+( tmp [ 7 ] = ( * ef7 ) ( ( * ef8 ) ( tmp [ 2 ] ) ) ) ;
+if ( check_sym ( tmp [ 7 ] , ef1sym ) ) {
+( tmp [ 8 ] = arg_0 ( tmp [ 7 ] ) ) ;
+if ( check_sym ( tmp [ 8 ] , lf2sym ) ) {
+( tmp [ 9 ] = arg_0 ( tmp [ 8 ] ) ) ;
+( tmp [ 10 ] = lf_AUX_Add_TagId_Rejects7_1_recursive ( make_list ( tmp [ 1 ] ) ) ) ;
+( tmp [ 11 ] = arg_0 ( tmp [ 10 ] ) ) ;
+( tmp [ 12 ] = arg_0 ( tmp [ 11 ] ) ) ;
+FUNC_EXIT ( make_nf1 ( ef1sym , lf2 ( cons ( make_list ( tmp [ 9 ] ) , make_list ( tmp [ 12 ] ) ) ) ) ) ;
+}
+}
+}
+}
+}
+else {
+FUNC_EXIT_CONST ( constant1 , make_nf1 ( ef1sym , lf2 ( make_list ( null ( ) ) ) ) ) ;
 }
 }
 }
@@ -237,12 +294,16 @@ ATerm lf2 ( ATerm arg0 ) {
 CONS_ENTRY ( lf2sym , ATmakeAppl ( lf2sym , arg0 ) ) ;
 CONS_EXIT ( make_nf1 ( lf2sym , arg0 ) ) ;
 }
-ATerm lf4 ( ATerm arg0 ) {
-CONS_ENTRY ( lf4sym , ATmakeAppl ( lf4sym , arg0 ) ) ;
-CONS_EXIT ( make_nf1 ( lf4sym , arg0 ) ) ;
+ATerm lf5 ( ATerm arg0 ) {
+CONS_ENTRY ( lf5sym , ATmakeAppl ( lf5sym , arg0 ) ) ;
+CONS_EXIT ( make_nf1 ( lf5sym , arg0 ) ) ;
 }
 ATerm lf3 ( ATerm arg0 ) {
 CONS_ENTRY ( lf3sym , ATmakeAppl ( lf3sym , arg0 ) ) ;
 CONS_EXIT ( make_nf1 ( lf3sym , arg0 ) ) ;
+}
+ATerm lf4 ( ATerm arg0 ) {
+CONS_ENTRY ( lf4sym , ATmakeAppl ( lf4sym , arg0 ) ) ;
+CONS_EXIT ( make_nf1 ( lf4sym , arg0 ) ) ;
 }
 
