d11b434cfc95ca23e1bff0ddd88b3ea695632456
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2001-06-25 15:17:36 +0000

    * forgot to add the dict

---
+++ asf/semantics/asfc/src/muasf/MuASF_dict.h
@@ -0,0 +1,142 @@
+/*
+ * Generated at Tue Jun 19 18:00:59 2001
+ */
+
+#ifndef __MuASF_dict_H
+#define __MuASF_dict_H
+
+#include <aterm2.h>
+
+extern AFun MA_afun0;
+extern AFun MA_afun1;
+extern AFun MA_afun2;
+extern AFun MA_afun3;
+extern AFun MA_afun4;
+extern AFun MA_afun5;
+extern AFun MA_afun6;
+extern AFun MA_afun7;
+extern AFun MA_afun8;
+extern AFun MA_afun9;
+extern AFun MA_afun10;
+extern AFun MA_afun11;
+extern AFun MA_afun12;
+extern AFun MA_afun13;
+extern AFun MA_afun14;
+extern AFun MA_afun15;
+extern AFun MA_afun16;
+extern AFun MA_afun17;
+extern AFun MA_afun18;
+extern AFun MA_afun19;
+extern AFun MA_afun20;
+extern AFun MA_afun21;
+extern AFun MA_afun22;
+extern AFun MA_afun23;
+extern AFun MA_afun24;
+extern AFun MA_afun25;
+extern AFun MA_afun26;
+extern AFun MA_afun27;
+extern AFun MA_afun28;
+extern AFun MA_afun29;
+extern AFun MA_afun30;
+extern AFun MA_afun31;
+extern AFun MA_afun32;
+extern AFun MA_afun33;
+extern AFun MA_afun34;
+extern AFun MA_afun35;
+extern AFun MA_afun36;
+extern AFun MA_afun37;
+extern AFun MA_afun38;
+extern AFun MA_afun39;
+extern AFun MA_afun40;
+extern AFun MA_afun41;
+extern AFun MA_afun42;
+extern AFun MA_afun43;
+extern AFun MA_afun44;
+extern AFun MA_afun45;
+extern AFun MA_afun46;
+extern AFun MA_afun47;
+extern AFun MA_afun48;
+extern AFun MA_afun49;
+extern AFun MA_afun50;
+extern AFun MA_afun51;
+extern AFun MA_afun52;
+extern AFun MA_afun53;
+extern AFun MA_afun54;
+extern AFun MA_afun55;
+extern AFun MA_afun56;
+extern AFun MA_afun57;
+extern AFun MA_afun58;
+extern AFun MA_afun59;
+extern AFun MA_afun60;
+extern AFun MA_afun61;
+extern AFun MA_afun62;
+extern AFun MA_afun63;
+extern AFun MA_afun64;
+
+extern ATerm MA_patternPropIdLexToCf;
+extern ATerm MA_patternModIdLexToCf;
+extern ATerm MA_patternVarIdLexToCf;
+extern ATerm MA_patternFunIdLexToCf;
+extern ATerm MA_patternQCharLexToCf;
+extern ATerm MA_patternEscCharLexToCf;
+extern ATerm MA_patternModuleModule;
+extern ATerm MA_patternRulesOptPresent;
+extern ATerm MA_patternRulesOptAbsent;
+extern ATerm MA_patternSignatureOptPresent;
+extern ATerm MA_patternSignatureOptAbsent;
+extern ATerm MA_patternCondUnequal;
+extern ATerm MA_patternCondEqual;
+extern ATerm MA_patternCondAssign;
+extern ATerm MA_patternCondElemsMany;
+extern ATerm MA_patternCondElemsSingle;
+extern ATerm MA_patternCondElemsEmpty;
+extern ATerm MA_patternCondListDefault;
+extern ATerm MA_patternRuleElemsMany;
+extern ATerm MA_patternRuleElemsSingle;
+extern ATerm MA_patternRuleElemsEmpty;
+extern ATerm MA_patternRuleListDefault;
+extern ATerm MA_patternRuleWithConds;
+extern ATerm MA_patternRuleDefaultWithConds;
+extern ATerm MA_patternRuleDefaultNoConds;
+extern ATerm MA_patternRuleNoConds;
+extern ATerm MA_patternFuncDefElemsMany;
+extern ATerm MA_patternFuncDefElemsSingle;
+extern ATerm MA_patternFuncDefElemsEmpty;
+extern ATerm MA_patternFuncDefListDefault;
+extern ATerm MA_patternSigArgListDefault;
+extern ATerm MA_patternTermTermsMany;
+extern ATerm MA_patternTermTermsSingle;
+extern ATerm MA_patternAnnotationsDefault;
+extern ATerm MA_patternSigArgElemsMany;
+extern ATerm MA_patternSigArgElemsSingle;
+extern ATerm MA_patternFuncDefFuncWithAnnos;
+extern ATerm MA_patternFuncDefFuncNoAnnos;
+extern ATerm MA_patternFuncDefConstantWithAnnos;
+extern ATerm MA_patternFuncDefConstantNoAnnos;
+extern ATerm MA_patternSigArgPlus;
+extern ATerm MA_patternSigArgStar;
+extern ATerm MA_patternSigArgNormal;
+extern ATerm MA_patternTermElemsMany;
+extern ATerm MA_patternTermElemsSingle;
+extern ATerm MA_patternTermElemsEmpty;
+extern ATerm MA_patternTermListDefault;
+extern ATerm MA_patternTermArgsMany;
+extern ATerm MA_patternTermArgsSingle;
+extern ATerm MA_patternTermFunc;
+extern ATerm MA_patternTermInt;
+extern ATerm MA_patternTermConstant;
+extern ATerm MA_patternTermVar;
+extern ATerm MA_patternVarPlus;
+extern ATerm MA_patternVarStar;
+extern ATerm MA_patternVarNormal;
+extern ATerm MA_patternNatConAbs;
+extern ATerm MA_patternIntConNeg;
+extern ATerm MA_patternIntConPos;
+extern ATerm MA_patternIntConNat;
+extern ATerm MA_patternIntConCon;
+extern ATerm MA_patternIntPlus;
+extern ATerm MA_patternIntCon;
+
+extern void init_MuASF_dict();
+
+#endif /* __MuASF_dict_H */
