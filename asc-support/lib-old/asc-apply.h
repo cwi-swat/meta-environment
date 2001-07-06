33a82237bc3ae2b51a17882b65cba8714f13af0e
Mark van den Brand <M.G.J.v.d.Brand@tue.nl>
2001-07-06 11:17:49 +0000

    * Remove all AsFix1 depending code.

--- asc-support/lib-old/asc-apply.h
+++
@@ -1,7 +0,0 @@
-#include <PT.h>
-
-PT_Tree
-ASC_applyFunctionToArgs(char *function, char* module, char* sort, PT_Args args);
-
-PT_Tree 
-ASC_applyFunction(char *function, char* module, char* sort, int nArgs, ...);
