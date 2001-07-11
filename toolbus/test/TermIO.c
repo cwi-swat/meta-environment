6aabab1b4d3ebc87e22a84837dd7f78d9f5ee675
Hayco de Jong <jong@metamud.org>
2001-07-11 12:23:58 +0000

    * warning free

--- toolbus/test/TermIO.c
+++ toolbus/test/TermIO.c
@@ -1,27 +1,37 @@
+/*{{{  header */
+
 /*
 
-    ToolBus -- The ToolBus Application Architecture
-    Copyright (C) 1998-2000  Stichting Mathematisch Centrum, Amsterdam, 
-                             The  Netherlands.
+   ToolBus -- The ToolBus Application Architecture
+   Copyright (C) 1998-2000  Stichting Mathematisch Centrum, Amsterdam, 
+   The  Netherlands.
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 
 */
+
+/*}}}  */
+
+/*{{{  includes */
+
 #include "TB.h"
 #include <fcntl.h>
 
+/*}}}  */
+
+/*{{{  void write_read(char *name, term *t1) */
 
 void write_read(char *name, term *t1)
 {
@@ -41,21 +51,24 @@ void write_read(char *name, term *t1)
     unlink(name);
     err_sys_fatal("Can't open %s for reading\n", name);
   }
-  
+
   t2 =TBread(in);
   close(in);
-/*  unlink(name);*/
-  
+  /*  unlink(name);*/
+
   if(!t2)
     err_fatal("Parse errors during reading\n");
   else if(!term_equal(t1,t2))
     err_fatal("Terms are unequal\n");
 }
 
+/*}}}  */
+/*{{{  int main() */
+
 int main()
 {
   char cbuf[50000];
-  int i, n;
+  int i;
 
   init_utils();
   write_read("fa", mk_int(13));
@@ -64,7 +77,7 @@ int main()
   write_read("fd", mk_str("ab\\\"c"));  
   write_read("fe", mk_str("ab\\\nc")); 
   write_read("ff", mk_str("a\b\f\n\r\vc")); 
- 
+
   for(i = 0; i < 20000; i++)
     cbuf[i] = i % 256;
 
@@ -78,3 +91,5 @@ int main()
   TBprintf(stderr, "Done\n");
   exit(0);
 }
+
+/*}}}  */
