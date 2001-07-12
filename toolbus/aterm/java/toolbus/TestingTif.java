a157d3a22afaadeda7c1a2cebefabd22cfd84b4f
Hayco de Jong <jong@metamud.org>
2001-07-12 08:50:52 +0000

    * Moved (that is copied) java-sources for ATB support from ATerm pkg   to ToolBus pkg.

---
+++ toolbus/aterm/java/toolbus/TestingTif.java
@@ -0,0 +1,19 @@
+// Java tool interface TestingTif
+// This file is generated automatically, please do not edit!
+// generation time: Jul 12, 2001 10:48:23 AM
+
+package toolbus;
+
+import aterm.*;
+import toolbus.*;
+
+public interface TestingTif
+{
+  public void listtest(ATerm t0);
+  public void testit(String s0);
+  public void testit(String s0, ATerm t1);
+  public void testit(String s0, int i1);
+  public ATerm question(ATerm t0);
+  public void recAckEvent(ATerm t0);
+  public void recTerminate(ATerm t0);
+}
