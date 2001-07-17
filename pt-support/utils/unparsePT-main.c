4dd7d6ae8ba371ff72703bdd15e6614142468d8c
Hayco de Jong <jong@metamud.org>
2001-07-17 09:48:41 +0000

    * first attempt at transforming "unparsePT" to a ToolBus-tool to replace   old "asource"

--- pt-support/utils/unparsePT-main.c
+++ pt-support/utils/unparsePT-main.c
@@ -1,4 +1,4 @@
-/*  $Id$  */
+/*{{{  includes */
 
 #include <stdio.h>
 #include <unistd.h>
@@ -6,23 +6,24 @@
 #include <string.h>
 
 #include <aterm2.h>
-
+#include <atb-tool.h>
 #include <MEPT-utils.h>
+
+#include "unparsePT.tif.h"
+
+/*}}}  */
+
+/*{{{  variables */
 
 static char myname[] = "unparsePT";
 static char myversion[] = "1.0";
 
-/*
- The argument vector: list of option letters, colons denote option
- arguments.  See Usage function, immediately below, for option
- explanation.
- */
-
 static char myarguments[] = "hi:o:vV";
 
-/*
- Usage: displays helpful usage information
- */
+/*}}}  */
+
+/*{{{  void usage(void) */
+
 void usage(void)
 {
   ATwarning("Usage: %s [%s]\n"
@@ -35,13 +36,37 @@ void usage(void)
             myname, myarguments, myversion);
 }
 
+/*}}}  */
+/*{{{  void version(void) */
+
 void version(void)
 {
   fprintf(stderr, "%s v%s\n", myname, myversion);
 }
 
+/*}}}  */
 
-int main(int argc, char **argv)
+/*{{{  void rec_terminate(int cid, ATerm t) */
+
+void rec_terminate(int cid, ATerm t)
+{
+  exit(0);
+}
+
+/*}}}  */
+/*{{{  void rec_terminate(int cid, ATerm t) */
+
+ATerm unparse_asfix(int cid, ATerm t)
+{
+  char *text = PT_yieldParseTree(PT_makeParseTreeFromTerm(t));
+  return ATmake("snd-value(unparsed-text(<str>))", text);
+}
+
+/*}}}  */
+
+/*{{{  int main(int argc, char *argv[]) */
+
+int main(int argc, char *argv[])
 {
   int c;
   ATerm bottomOfStack;
@@ -50,51 +75,76 @@ int main(int argc, char **argv)
   char   *inputName = "-";
   char   *outputName = "-";
   FILE *outputFile = NULL; 
-  ATbool verbose = ATfalse;
+  ATbool use_toolbus = ATfalse;
   ATbool proceed = ATtrue;
+  ATbool verbose = ATfalse;
+  int i, cid;
 
-  extern char *optarg;
-  extern int   optind;
-
-  /*  Commandline mode  */
-
-  while ((c = getopt(argc, argv, myarguments)) != -1) {
-    switch (c) {
-      case 'i':  inputName = optarg;             break;
-      case 'o':  outputName = optarg;            break;
-      case 'v':  verbose = ATtrue;               break;
-      case 'V':  version(); proceed = ATfalse;   break;
-
-      case 'h':
-      default:   usage(); proceed = ATfalse;     break;
-    }
+  for (i=1; !use_toolbus && i < argc; i++) {
+    use_toolbus = !strcmp(argv[i], "-TB_TOOL_NAME");
   }
-  argc -= optind;
-  argv += optind;
 
-  ATinit(argc, argv, &bottomOfStack);       /* Initialize Aterm library */
-  PT_initMEPTApi();
+  if (use_toolbus) {
+    ATBinit(argc, argv, &bottomOfStack);
+    PT_initMEPTApi();
+    cid = ATBconnect(NULL, NULL, -1, unparsePT_handler);
+    ATBeventloop();
+  }
+  else {
+    extern char *optarg;
+    extern int   optind;
 
-  if (proceed) {
-    ATerm term;
-
-    if (!strcmp(outputName, "") || !strcmp(outputName, "-")) {
-      outputFile = stdout;
-    } 
-    else if (!(outputFile = fopen(outputName, "w"))) {
-      ATerror("%s: cannot open %s for writing\n", argv[0], outputName);
+    while ((c = getopt(argc, argv, myarguments)) != -1) {
+      switch (c) {
+	case 'i':
+	  inputName = optarg;
+	  break;
+	case 'o':
+	  outputName = optarg;
+	  break;
+	case 'v':
+	  verbose = ATtrue;
+	  break;
+	case 'V':
+	  version();
+	  proceed = ATfalse;
+	  break;
+	case 'h':
+	default:
+	  usage();
+	  proceed = ATfalse;
+	  break;
+      }
     }
+    argc -= optind;
+    argv += optind;
 
-    term = ATreadFromNamedFile(inputName);
-    if (term == NULL) {
-      ATerror("%s: parse error in input term.\n", argv[0]);
+    ATinit(argc, argv, &bottomOfStack);
+    PT_initMEPTApi();
+
+    if (proceed) {
+      ATerm term;
+
+      if (!strcmp(outputName, "") || !strcmp(outputName, "-")) {
+	outputFile = stdout;
+      } 
+      else if (!(outputFile = fopen(outputName, "w"))) {
+	ATerror("%s: cannot open %s for writing\n", argv[0], outputName);
+      }
+
+      term = ATreadFromNamedFile(inputName);
+      if (term == NULL) {
+	ATerror("%s: parse error in input term.\n", argv[0]);
+      }
+      else {
+	char *text = PT_yieldParseTree(PT_makeParseTreeFromTerm(term));
+	fprintf(outputFile, "%s", text);
+	fclose(outputFile);
+     }
     }
-    else {
-      char *text = PT_yieldParseTree(PT_makeParseTreeFromTerm(term));
-      fprintf(outputFile, "%s", text);
-      fclose(outputFile);
-   }
   }
 
   return 0;
 }
+
+/*}}}  */
