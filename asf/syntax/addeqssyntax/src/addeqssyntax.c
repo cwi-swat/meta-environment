853a8581549930fb968f76f87c582af9f9ede032
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2001-07-20 22:26:51 +0000

    * bumped version to 2.1

--- asf/syntax/addeqssyntax/src/addeqssyntax.c
+++ asf/syntax/addeqssyntax/src/addeqssyntax.c
@@ -20,7 +20,7 @@ static char *name;
 ATbool run_verbose;      
 
 static char myname[] = "addeqssyntax";
-static char myversion[] = "2.0";  
+static char myversion[] = "2.1";  
 
 /*
     The argument vector: list of option letters, colons denote option
