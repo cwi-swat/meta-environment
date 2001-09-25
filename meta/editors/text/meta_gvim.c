fedab4ef800d9b70b0437b9e41211fdb97700de5
Hayco de Jong <jong@metamud.org>
2001-09-25 11:42:17 +0000

    * Introduced gvim support as text-editor.    Requires gvim (v6.0) in $PATH, invoke "meta -g" to use it.  * Renamed emacs-support to "emacs-" like names (rather than "editor")

---
+++ meta/editors/text/meta_gvim.c
@@ -0,0 +1,47 @@
+/*{{{  includes */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+/*}}}  */
+/*{{{  defines */
+
+#define SEPARATOR '#'
+
+/*}}}  */
+
+/*{{{  int handshake(int fd) */
+
+int handshake(int fd)
+{
+  return write(fd, "handshake", 9);
+}
+
+/*}}}  */
+/*{{{  int send(const char *cmd) */
+
+int send(const char *cmd)
+{
+  char *p;
+  int fd;
+  int len;
+
+  fd = atoi(cmd);
+  if (fd <= 0) {
+    return 1;
+  }
+
+  p = strchr(cmd, SEPARATOR);
+  if (p == NULL) {
+    return 2;
+  }
+
+  p++; /* skip separator */
+  len = strlen(p) + 1;
+
+  return write(fd, p, len) == len;
+}
+
+/*}}}  */
