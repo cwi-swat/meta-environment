a666ae7bc95748ba758aa01a9a9669fcd17a67c8
Pieter Olivier <pieter@gamesquare.nl>
2002-03-09 15:51:55 +0000

    Added support for type operation 'hash-code'.

--- autocode/org/autocode/generator/java/plugin/Equality.java
+++ autocode/org/autocode/generator/java/plugin/Equality.java
@@ -49,7 +49,7 @@ public class Equality
       String getter = fieldContext.getString("getter");
       if (getter == null) {
 	throw new RuntimeException("operation 'equals' requires that a "
-				   + "setter is defined for field "
+				   + "getter is defined for field "
 				   + fieldName);
       }
       if (fieldTypeContext.getBoolean("reference-type")) {
@@ -80,8 +80,74 @@ public class Equality
   }
 
   //}}}
+  //{{{ public void generateHashCode()
 
-  public void generateHashCode()
+  public void generateHashCode(JavaGenerator generator,
+			       PropertyContext typeContext,
+			       PropertyContext operationContext)
   {
+    CodeBlock body = new CodeBlock();
+
+    JavaMethod method
+      = createMethod(operationContext, "hashCode", "int", body);
+    
+    body.addLine("int code = 0;");
+    body.addLine("");
+
+    //{{{ Add field equality checks
+
+    Iterator iter = typeContext.getValueSet("field").iterator();
+    while (iter.hasNext()) {
+      String fieldName = (String)iter.next();
+      PropertyContext fieldContext
+	= new PropertyContext(typeContext, "field", fieldName);
+      PropertyContext fieldTypeContext = new PropertyContext(fieldContext, "type");
+
+      if (fieldContext.getBoolean("hash")) {
+	String getter = fieldContext.getString("getter");
+	if (getter == null) {
+	  throw new RuntimeException("operation 'equals' requires that a "
+				     + "getter is defined for field "
+				     + fieldName);
+	}
+	String expr;
+	if (fieldTypeContext.getBoolean("reference-type")) {
+	  expr = getter + "().hashCode()";
+	} else {
+	  String impl = fieldTypeContext.getString("implementation");
+	  if ("boolean".equals(impl)) {
+	    // Constants as specified in JDK-1.4 API documentation
+	    expr = "(" + getter + "() ? 1231 : 1237)";
+	  } else if ("integer".equals(impl)) {
+	    expr = getter + "()";
+	  } else if ("float".equals(impl)) {
+	    expr = "new Float("+ getter + "()).hashCode()";
+	  } else if ("double".equals(impl)) {
+	    expr = "new Double("+ getter + "()).hashCode()";
+	  } else if ("char".equals(impl)) {
+	    expr = "(int)" + getter + "()";
+	  } else if ("long".equals(impl)) {
+	    expr = "(int)(" + getter + "() ^ " + getter + "()>>>32)";
+	  } else {
+	    throw new RuntimeException("illegal non-reference type: "
+				       + impl);
+	  }
+	}
+	body.addLine("code = (code<<1) ^ " + expr + ";");
+      }
+    }
+
+    //}}}
+
+    body.addLine("");
+    body.addLine("return code;");
+
+    String typeName = generator.typeName(typeContext.getName());
+    method.setDescription("calculate the hascode based on the hashcodes "
+			  + " of the attributes.");
+
+    generator.getCompilationUnit().addMethod(method);
   }
+
+  //}}}
 }
