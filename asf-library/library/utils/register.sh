4c50548acb763f6a0c602343a8f127e7e5906710
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2002-05-22 09:56:18 +0000

    * factored out a script that registers something

---
+++ asf-library/library/utils/register.sh
@@ -0,0 +1,41 @@
+#! /bin/sh
+
+ACTION="register"
+
+for arg in $*
+do
+   case $1 in
+      -u ) ACTION="unregister"; shift ;;
+      -r ) ACTION="register"; shift ;;
+      * ) break ;;
+   esac
+done
+
+registry=$1
+tag=$2
+values=$3
+
+unregister ()
+{
+   if [ -f ${registry} ]; then
+      cp ${registry} ${registry}.bak
+      grep -v "${tag}:" ${registry}.bak > ${registry} || true
+   fi
+}
+
+register ()
+{
+   unregister
+   if test "a${value}" = "a"; then
+     for value in ${values} ; do
+       echo "${tag}:${value}" >> ${registry}
+     done
+   fi   
+}
+
+
+if [ "${ACTION}" = "register" ] ; then
+   register
+else
+   unregister
+fi
