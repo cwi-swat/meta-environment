baf397ea9abe5c3b1d02833a36a13141c6f139bc
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2002-05-23 16:32:54 +0000

    * fixed writing of parse tree

--- asf-library/library/system/fileio.c
+++ asf-library/library/system/fileio.c
@@ -48,13 +48,14 @@ PT_Tree write_term_to_file(ATerm builtin, PT_Tree input)
 {
   PT_Tree file_arg = PT_getArgsArgumentAt(PT_getTreeArgs(input),4);
   PT_Tree tree_arg = PT_getArgsArgumentAt(PT_getTreeArgs(input),8);
+  PT_ParseTree pt = PT_makeValidParseTreeFromTree(tree_arg);
   char *filestr = NULL;
 
   filestr = PT_yieldTree(file_arg);
 
   if (filestr != NULL && strlen(filestr) > 2) {
     filestr[strlen(filestr) -1 ] = '\0';
-    ATwriteToNamedTextFile(PT_TreeToTerm(tree_arg), filestr+1);
+    ATwriteToNamedTextFile(PT_ParseTreeToTerm(pt), filestr+1);
   }
 
   return tree_arg;
