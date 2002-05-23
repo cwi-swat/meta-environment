f6586945dc21a274d87e9b8e98148bd57b8f5491
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2002-05-23 09:13:53 +0000

    * added id and const

---
+++ asf-library/library/functions/link-test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <MEPT-utils.h>
+#include <aterm2.h>
+
+/* DO NOT EDIT: This file is generated */
+
+
+
+/* This code is not meant for execution, 
+ * it is used to test if there is an implementation for every built-in
+ */
+int main(void)
+{
+
+  if (fprintf(stderr, "This program does nothing\n") == 0) {
+
+  }
+
+  return 1; 
+}
