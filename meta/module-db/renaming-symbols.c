385150d56439db96df4fb88152a8ebd2af860cd8
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2002-07-05 12:59:30 +0000

    * not only variables but all NEW lists after symbol replacement should   be encapsulated by a list production

--- meta/module-db/renaming-symbols.c
+++ meta/module-db/renaming-symbols.c
@@ -220,17 +220,14 @@ PT_Tree renameInTree(PT_Tree tree,
 
     newTree = PT_setTreeArgs(PT_setTreeProd(tree, newProd), newArgs);
 
-
-    /* Wrap new list variables in a proper list production */
-    if (PT_isVarDefault(newProd)) {
-      if ((PT_isIterSymbol(newRhs) || PT_isIterSepSymbol(newRhs)) && 
-	  !(PT_isIterSymbol(rhs) || PT_isIterSepSymbol(rhs))) {
-	PT_Production listProd = PT_makeProductionList(newRhs);
-	PT_Tree listTree = PT_makeTreeAppl(listProd,
-					   PT_makeArgsList(newTree,
-							   PT_makeArgsEmpty()));
-	newTree = listTree;
-      }
+    /* Wrap new lists in a proper list production */
+    if ((PT_isIterSymbol(newRhs) || PT_isIterSepSymbol(newRhs)) && 
+	!(PT_isIterSymbol(rhs) || PT_isIterSepSymbol(rhs))) {
+      PT_Production listProd = PT_makeProductionList(newRhs);
+      PT_Tree listTree = PT_makeTreeAppl(listProd,
+					 PT_makeArgsList(newTree,
+							 PT_makeArgsEmpty()));
+      newTree = listTree;
     }
 
     return newTree;
