ad6119f08890afdec3aa4c07143a1d2ad0ab206b
Paul Klint <Paul.Klint@cwi.nl>
2002-08-15 18:42:27 +0000

    Completed with simplification of the Atom class and its interface
    AtomSet renamed into State: one state of the automaton that
    consists of a set of StateElements.
    
    StateElements is an interface thats is implemented by:
    	- Atom
    	- Merge
    	- ProcCall
    
    All three can thus appear in a State:
    	- Atom as before
    	- Merge: does alternating steps in its two arguments
    	- ProcCall: instead of generating assignments for formal/actual
    	  correspondence, ProcCall does this dynamically and then
    	  switches to the (alread expanded and compiled) process expression.
    
    Cleaned up where information related to snd/rec occurs:
    	CommAtom (renamed to  MsgAtom) is now the only placed
    	where opreations related to communication occur.
    
    Mosr test cases are running.

---
+++ toolbusNG/toolbus/process/StateElement.java
@@ -0,0 +1,11 @@
+package toolbus.process;
+
+import toolbus.ToolBusException;
+
+public interface StateElement {
+
+  public boolean contains(StateElement a);
+  public ProcessInstance getProcess();
+  public boolean nextState();
+  public boolean execute() throws ToolBusException;
+}
