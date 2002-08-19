c17d604e41841f91a4f56c1dcc8763c10dd256c4
Paul Klint <Paul.Klint@cwi.nl>
2002-08-19 15:44:01 +0000

    Continued with parser

--- toolbusNG/toolbus/ToolBus.java
+++ toolbusNG/toolbus/ToolBus.java
@@ -12,7 +12,7 @@ import toolbus.process.*;
 import aterm.*;
 
 public class ToolBus {
-  
+
   private static Random rand = new Random();
   private ATermFactory factory;
   private Vector processes;
@@ -31,33 +31,93 @@ public class ToolBus {
   public Vector getProcesses() {
     return processes;
   }
-  
-  public static int nextInt(int n){
+
+  public static int nextInt(int n) {
     return rand.nextInt(n);
   }
-  
-  public static boolean nextBoolean(){
+
+  public static boolean nextBoolean() {
     return rand.nextBoolean();
   }
-  
-  public void parse(String filename)throws ToolBusException {
+
+  private ATerm build(ATerm t) throws ToolBusException {
+
+    switch (t.getType()) {
+      case ATerm.BLOB :
+      case ATerm.INT :
+      case ATerm.PLACEHOLDER :
+      case ATerm.REAL :
+        return t;
+
+      case ATerm.APPL :
+        String name = ((ATermAppl) t).getName();
+        ATerm args[] = ((ATermAppl) t).getArgumentArray();
+
+        if (name == "vardecl") { // vardecl(name, type) => var(-1,type,name)
+          AFun afun = factory.makeAFun("var", 3, false);
+          ATerm cargs[] = new ATerm[] { factory.makeInt(-1), args[1], args[0] };
+          return factory.makeAppl(afun, cargs);
+          
+        } else if (name == "resvardecl") { // resvardecl(name, type) => rvar(-1,type,name)
+          AFun afun = factory.makeAFun("rvar", 3, false);
+          ATerm cargs[] = new ATerm[] { factory.makeInt(-1), args[1], args[0] };
+          return factory.makeAppl(afun, cargs);
+        } else if (name == "var") { // var(name) => var(-1, -1, name)
+          AFun afun = factory.makeAFun("var", 3, false);
+          ATerm cargs[] = new ATerm[] { factory.makeInt(-1), factory.makeInt(-1), args[0] };
+          return factory.makeAppl(afun, cargs);
+        } else if (name == "resvar") { // resvar(name) => rvar(-1, -1, name)
+          AFun afun = factory.makeAFun("rvar", 3, false);
+          ATerm cargs[] = new ATerm[] { factory.makeInt(-1), factory.makeInt(-1), args[0] };
+          return factory.makeAppl(afun, cargs);
+        } else if (name == "apply") {   // apply(name, arglist)
+          if (args.length != 2)
+            throw new ToolBusException("apply: wrong number of args");
+ 
+          ATermList args1 = (ATermList) args[1];
+          String fname = ((ATermAppl) args[0]).getName();
+          AFun afun = factory.makeAFun(fname, args1.getLength(), false);
+          ATermList realargs = factory.makeList();
+          
+          for(int i = 0; i < args1.getLength(); i++){
+            realargs.append(build(args1.elementAt(i)));
+          }
+          return factory.makeAppl(afun, realargs);
+
+        } else if (name == "natcon") {  // natcon(str)
+        } else if (name == "realcon") { // realcon(str, str)
+        } else if (name == "posint") {  // posint(str)
+        } else if (name == "negint") {   //negint(str)
+        } else
+          throw new ToolBusException("illegal symbol " + name);
+      case ATerm.LIST :
+        ATermList lst = t.getFactory().makeList();
+        for (int i = 0; i < ((ATermList) t).getLength(); i++) {
+          lst = lst.append(build(((ATermList) t).elementAt(i)));
+        }
+        return lst;
+    }
+    throw new ToolBusInternalError("illegal ATerm in build: " + t);
+  }
+
+  public void parse(String filename) throws ToolBusException {
     ATerm interm;
     try {
       interm = factory.readFromFile(filename);
     } catch (IOException e) {
       throw new ToolBusException(e.getMessage());
     }
-    
-    if(interm.getType() != ATerm.APPL || ((ATermAppl) interm).getName()!= "Tscript" || interm.getChildCount() != 2){
+
+    if (interm.getType() != ATerm.APPL || ((ATermAppl) interm).getName() != "Tscript" || interm.getChildCount() != 2) {
       throw new ToolBusException("ill-formed tree");
     }
-    
+
     ATermList decls = (ATermList) interm.getChildAt(0);
     ATermList calls = (ATermList) interm.getChildAt(1);
-    
+
     System.out.print("decls = " + decls);
     System.out.println("calls = " + calls);
-      
+
   }
 
   public void addProcessDefinition(ProcessDefinition PD) throws ToolBusException {
