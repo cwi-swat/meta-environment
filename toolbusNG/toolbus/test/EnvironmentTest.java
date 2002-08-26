78095fd6145eec7d0199b7435177e782295916f7
Paul Klint <Paul.Klint@cwi.nl>
2002-08-26 07:23:31 +0000

    Extended EnvironmentTest

--- toolbusNG/toolbus/test/EnvironmentTest.java
+++ toolbusNG/toolbus/test/EnvironmentTest.java
@@ -22,6 +22,9 @@ public class EnvironmentTest extends TestCase {
     ATerm varX = factory.make("var(-1,int,X)");
     ATerm varY = factory.make("var(-1,str,Y)");
 
+    ATerm intVal = factory.make("123");
+    ATerm strVal = factory.make("abc");
+
     ATermList vars = factory.makeList(varY);
     vars = factory.makeList(varX, vars);
 
@@ -33,9 +36,36 @@ public class EnvironmentTest extends TestCase {
     assertEquals(env.getVarType(varX), TBTerm.IntType);
     assertEquals(env.getVarType(varY), TBTerm.StrType);
 
+    varX = TBTerm.resolveVars(varX, env);
+    assertEquals(TBTerm.getVarIndex(varX), 0);
+
+    varY = TBTerm.resolveVars(varY, env);
+    assertEquals(TBTerm.getVarIndex(varY), 1);
+
+    env.setExecuting();
+
+    try {
+      assertEquals(env.getValue(varX), null);
+      assertEquals(env.getValue(varY), null);
+      fail();
+    } catch (ToolBusException e) {
+    }
+
+    env.assignVar(varX, intVal);
+    assertEquals(env.getValue(varX), intVal);
+    try {
+      assertEquals(env.getValue(varY), null);
+      fail();
+    } catch (ToolBusException e) {
+    }
+
+    env.assignVar(varY, strVal);
+    assertEquals(env.getValue(varX), intVal);
+    assertEquals(env.getValue(varY), strVal);
+
   }
-  
-   public void testEnv2() throws ToolBusException {
+
+  public void testEnv2() throws ToolBusException {
     Environment env = new Environment();
     ATerm varX1 = factory.make("var(-1,int,X)");
     ATerm varX2 = factory.make("var(-1,str,X)");
@@ -50,7 +80,21 @@ public class EnvironmentTest extends TestCase {
 
     assertEquals(env.getVarType(varX1), TBTerm.StrType);
     assertEquals(env.getVarType(varX2), TBTerm.StrType);
-
   }
+  
+  
+//  public void testEnv3() throws ToolBusException {
+//    Environment env = new Environment();
+//    ATerm varX = factory.make("var(-1,int,X)");
+//    ATerm varX2 = factory.make("rvar(-1,str,Y)");
+//
+//    ATermList vars = factory.makeList(varX2);
+//    vars = factory.makeList(varX1, vars);
+//    
+    
+    
+    
+    
+    
 
 }
