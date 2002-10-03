152adf350e74d2785601f92922c8aed699751c0c
Hayco de Jong <jong@metamud.org>
2002-10-03 08:30:50 +0000

    add interface AddressBook for use with RMI add implementation AddressBookImpl (also RMI)

---
+++ toolbus-ng/casestudy/rmi/AddressBook.java
@@ -0,0 +1,9 @@
+package casestudy.rmi;
+
+import java.rmi.*;
+
+public interface AddressBook extends Remote {
+  public void add(String name, String address) throws RemoteException;
+  public void remove(String name) throws RemoteException;
+  public String getAddress(String name) throws RemoteException;
+}
