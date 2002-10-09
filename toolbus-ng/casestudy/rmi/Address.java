4f8df6b9cb8601a59249093c0d427c953e884fc1
Hayco de Jong <jong@metamud.org>
2002-10-09 06:45:15 +0000

    add separate class for Address (Serializable) to further investigate Remote Objects in RMI

---
+++ toolbus-ng/casestudy/rmi/Address.java
@@ -0,0 +1,32 @@
+package casestudy.rmi;
+
+import java.io.Serializable;
+
+public class Address implements Serializable {
+  private String street;
+  private String number;
+
+  public Address(String street, String number) {
+    this.street = street;
+    this.number = number;
+  }
+
+  public String getStreet() {
+    return street;
+  }
+  public String getNumber() {
+    return number;
+  }
+
+  public boolean equals(Object o) {
+    if (o instanceof Address) {
+      Address peer = (Address) o;
+      return peer.getStreet().equals(street) && peer.getNumber().equals(number);
+    }
+    return false;
+  }
+
+  public int hashCode() {
+    return (street.hashCode() << 2) ^ number.hashCode();
+  }
+}
--- toolbus-ng/casestudy/rmi/AddressBookImpl.java
+++ toolbus-ng/casestudy/rmi/AddressBookImpl.java
@@ -4,32 +4,6 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.*;
 
-class Address {
-  private String street;
-  private String number;
-
-  public Address(String street, String number) {
-    this.street = street;
-    this.number = number;
-  }
-
-  public String getStreet() { return street; }
-  public String getNumber() { return number; }
-
-  public boolean equals(Object o) {
-    if (o instanceof Address) {
-      Address peer = (Address) o;
-      return peer.getStreet().equals(street)
-	&& peer.getNumber().equals(number);
-    }
-    return false;
-  }
-
-  public int hashCode() {
-    return (street.hashCode() << 2) ^ number.hashCode();
-  }
-}
-
 public class AddressBookImpl extends UnicastRemoteObject implements AddressBook {
   private Map addressMap;
 
@@ -37,14 +11,14 @@ public class AddressBookImpl extends UnicastRemoteObject implements AddressBook 
     super();
     addressMap = new HashMap();
 
-    String shared = new String("Balistraat 130 II");
+    Address shared = new Address("Balistraat", "130 II");
     add("hayco", shared);
     add("loge", shared);
 
-    add("pieter", "Ergens anders 42");
+    add("pieter", new Address("Ergens anders", "42"));
   }
 
-  public void add(String name, String address) throws RemoteException {
+  public void add(String name, Address address) throws RemoteException {
     addressMap.put(name, address);
   }
 
@@ -52,7 +26,7 @@ public class AddressBookImpl extends UnicastRemoteObject implements AddressBook 
     addressMap.remove(name);
   }
 
-  public String getAddress(String name) throws RemoteException {
-    return (String) addressMap.get(name);
+  public Address getAddress(String name) throws RemoteException {
+    return (Address) addressMap.get(name);
   }
 }
