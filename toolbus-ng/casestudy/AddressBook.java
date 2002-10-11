e315525fc63afe946c82143eb206262f106d4ff8
Hayco de Jong <jong@metamud.org>
2002-10-11 11:36:12 +0000

    add: AddressBook implementation based on javax.rmi.PortableRemoteObject

--- toolbus-ng/casestudy/AddressBook.java
+++
@@ -1,8 +0,0 @@
-package casestudy;
-
-import java.rmi.*;
-
-public interface AddressBook extends Remote {
-  public void setAddress(String name, String address) throws RemoteException;
-  public String getAddress(String name) throws RemoteException;
-}
