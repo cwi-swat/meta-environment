2c3b50525bd3c8f8754a25e037f66ba817e409f5
Hayco de Jong <jong@metamud.org>
2002-10-11 11:29:59 +0000

    fix must throw RemoteException in interface

--- toolbus-ng/casestudy/AddressBook.java
+++ toolbus-ng/casestudy/AddressBook.java
@@ -1,8 +1,8 @@
 package casestudy;
 
-import java.rmi.Remote;
+import java.rmi.*;
 
 public interface AddressBook extends Remote {
-  public void setAddress(String name, String address);
-  public String getAddress(String name);
+  public void setAddress(String name, String address) throws RemoteException;
+  public String getAddress(String name) throws RemoteException;
 }
