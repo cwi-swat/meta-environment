191c04f0e1c2419270063b34bbd2a3bd7032aeba
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2002-10-15 17:53:58 +0000

    * fixed formatString bugs

--- meta/meta-studio/metastudio/MetaStudio.java
+++ meta/meta-studio/metastudio/MetaStudio.java
@@ -835,18 +835,21 @@ public class MetaStudio
 		int index;
 		String prefix = "";
 		String postfix = format;
-		while ((index = postfix.indexOf("%s")) != -1) {
+		while ((index = postfix.indexOf("%")) != -1) {
 			prefix += postfix.substring(0, index);
 			switch (postfix.charAt(index + 1)) {
+        case 't' :
 				case 'd' :
 					prefix += args.getFirst().toString();
 					break;
 				case 's' :
 					prefix += ((ATermAppl) args.getFirst()).getName();
-					break;
-			}
+				  break;
+        default:
+          prefix += "%" + postfix.charAt(index+1);
+      }
 			postfix = postfix.substring(index + 2);
-			args = args.getNext();
+      args = args.getNext();
 		}
 		return prefix + postfix;
 	}
