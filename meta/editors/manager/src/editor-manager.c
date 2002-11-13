4f79115543a569a0a4787ccda48a4567c774fa0a
Mark van den Brand <M.G.J.v.d.Brand@tue.nl>
2002-11-13 12:34:06 +0000

    * Added a number of essential components to the -V of generic-meta.  * Added a similar interface is used in "module-db" to "editor-manager".

--- meta/editors/manager/src/editor-manager.c
+++ meta/editors/manager/src/editor-manager.c
@@ -23,6 +23,8 @@ char editor_manager_id[] = "$Id$";
 /*}}}  */
 /*{{{  variables */
 
+static char myversion[] = "1.0";
+
 static ATermList editors = NULL;
 
 /*}}}  */
@@ -309,13 +311,42 @@ rec_terminate(int conn, ATerm reason)
 }
 
 /*}}}  */
+/*{{{  static void usage(char *prg, ATbool is_err) */
 
+static void usage(char *prg, ATbool is_err)
+{
+  ATwarning("usage: %s [aterm-options] [toolbus-options]\n", prg);
+  ATwarning("use '%s -at-help' to get more options.\n", prg);
+  ATwarning("This program can only be used as a ToolBus tool!\n");
+  exit(is_err ? 1 : 0);
+}
+
+/*}}}  */
+/*{{{  static void version(const char *msg) */
+
+static void
+version(const char *msg)
+{
+  ATwarning("%s v%s\n", msg, myversion);
+  exit(1);
+}
+
+/*}}}  */
 /*{{{  int main(int argc, char *argv[]) */
 
 int main(int argc, char *argv[])
 {
-  int cid;
+  int i, cid;
   ATerm bottomOfStack;
+
+  for (i=1; i<argc; i++) {
+    if (strcmp(argv[i], "-h") == 0) {
+      usage(argv[0], ATfalse);
+    } 
+    else if (strcmp(argv[i], "-V") == 0) {
+      version(argv[0]);
+    }
+  }
 
   ATBinit(argc, argv, &bottomOfStack);
 
