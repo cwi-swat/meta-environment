f3b8fa61b954f44011740bf6692e3ac1971c8c9b
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2003-02-12 13:19:24 +0000

    * changed interface of get-term-anno

--- asc-support/builtins/lib/annos.c
+++ asc-support/builtins/lib/annos.c
@@ -4,10 +4,13 @@
 #include <MEPT-utils.h>
 #include <PTMEPT-utils.h>
 #include <aterm2.h>
+#include <string.h>
 
 /* note that by using AsFix trees as keys the storing and retrieval
  * of ATerm annotations depends on whitespace!
  */
+
+/*{{{  PT_Tree set_anno(ATerm builtin, PT_Tree input) */
 
 PT_Tree set_anno(ATerm builtin, PT_Tree input)
 {
@@ -19,6 +22,9 @@ PT_Tree set_anno(ATerm builtin, PT_Tree input)
 			      PT_TreeToTerm(key),
 			      PT_TreeToTerm(value));
 }
+
+/*}}}  */
+/*{{{  PT_Tree get_anno(ATerm builtin, PT_Tree input) */
 
 PT_Tree get_anno(ATerm builtin, PT_Tree input)
 {
@@ -36,13 +42,34 @@ PT_Tree get_anno(ATerm builtin, PT_Tree input)
   return input;
 }
 
+/*}}}  */
+
+/*{{{  static ATermAppl unquoteAppl(ATermAppl appl)  */
+
+static ATermAppl unquoteAppl(ATermAppl appl) 
+{
+  AFun fun = ATgetAFun(appl);
+  int arity = ATgetArity(fun);
+  char *name = ATgetName(fun);
+  ATermList args = ATgetArguments(appl);
+  fun = ATmakeAFun(name, arity, ATfalse);
+
+  return ATmakeApplList(fun, args);
+}
+
+/*}}}  */
+
+/*{{{  PT_Tree get_term_anno(ATerm builtin, PT_Tree input)  */
+
 PT_Tree get_term_anno(ATerm builtin, PT_Tree input) 
 {
   PT_Tree term = PT_getArgsArgumentAt(PT_getTreeArgs(input),4);
   PT_Tree key = PT_getArgsArgumentAt(PT_getTreeArgs(input),8);
   PT_Tree value = NULL;
-  ATerm   label = ATparse(PT_yieldTree(key));
+  ATerm label;
   ATerm anno;
+
+  label = (ATerm) unquoteAppl((ATermAppl) ATparse(PT_yieldTree(key)));
 
   if (label == NULL) {
     return input;
@@ -60,3 +87,5 @@ PT_Tree get_term_anno(ATerm builtin, PT_Tree input)
 
   return input;
 }
+
+/*}}}  */
