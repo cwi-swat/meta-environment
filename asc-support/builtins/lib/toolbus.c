763b541d5fad1a00c37dbed73846e3379a57678d
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2003-02-13 17:56:29 +0000

    * made event builtin blocking until acknowledgement arives

--- asc-support/builtins/lib/toolbus.c
+++ asc-support/builtins/lib/toolbus.c
@@ -3,16 +3,35 @@
 
 /* From the interpreter */
 extern int toolbus_id;
+extern ATerm acknowledgement;
 
 /*{{{  static void post_event(ATerm label, ATerm event) */
 
 static void post_event(ATerm label, ATerm event, char *msg)
 {
+  assert(ATBisValidConnection(toolbus_id));
+
   ATBpostEvent(toolbus_id, ATmake("event(<term>,<term>,<str>)",
 				  label, event, msg));
 }
 
 /*}}}  */
+static ATerm receive_ack(void)
+{
+  ATerm ack;
+
+  assert(ATBisValidConnection(toolbus_id));
+
+  while (!ATBpeekOne(toolbus_id));
+  ATBhandleOne(toolbus_id);
+
+  /* get acknowledgement from global variable because 
+   * the rec_ack_event in the interpreter put it there
+   */
+  ack = acknowledgement;
+
+  return ack;
+}
 
 /*{{{  PT_Tree toolbus_event(ATerm builtin, PT_Tree input) */
 
@@ -36,6 +55,7 @@ PT_Tree toolbus_term_event(ATerm builtin, PT_Tree input)
   PT_Tree event = CO_getFunctionArgument(input,1);
   ATerm label = CO_unquoteAppl(ATparse(PT_yieldTree(name))); 
   ATerm term = ATparse(PT_yieldTree(event));
+  PT_Tree result = event;
 
   if (term == NULL) {
     ATwarning("toolbus_term_event: %s is not a proper ATerm\n",
@@ -45,8 +65,14 @@ PT_Tree toolbus_term_event(ATerm builtin, PT_Tree input)
   }
 
   post_event(label, term, "term-event"); 
+  result = (PT_Tree) receive_ack();
 
-  return event;
+  if (result == NULL) {
+    return input; 
+  }
+  else {
+    return result;
+  }
 }
 
 /*}}}  */
