c15ec08ef24b1bdd513fd6af3031c625ce3f195f
Hayco de Jong <jong@metamud.org>
2003-03-20 10:08:17 +0000

    * example extended

--- toolbusNG/casestudy/rmi/RemoteClient.java
+++ toolbusNG/casestudy/rmi/RemoteClient.java
@@ -8,11 +8,11 @@ public class RemoteClient {
       AddressBook ab = (AddressBook) Naming.lookup("rmi://localhost/AddressBookService");
       //System.out.println("lookup '"+args[0]+"': " + ab.getAddress(args[0]));
 
-      Address a1 = ab.getAddress("hayco");
-      System.out.println("hayco: " + a1.getStreet() + " " + a1.getNumber());
+      Address a1 = ab.getAddress("john");
+      System.out.println("john : " + a1.getStreet() + " " + a1.getNumber());
 
-      Address a2 = ab.getAddress("loge");
-      System.out.println("loge : " + a2.getStreet() + " " + a2.getNumber());
+      Address a2 = ab.getAddress("guest");
+      System.out.println("guest: " + a2.getStreet() + " " + a2.getNumber());
 
       System.out.println("equal: " + a1.equals(a2));
       System.out.println("a1 == a2: " + ((a1 == a2) ? "yes" : "no"));
