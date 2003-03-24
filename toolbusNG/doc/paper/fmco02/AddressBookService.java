1fb5d121d53716bad84e9df2c6bf198569875404
Hayco de Jong <jong@metamud.org>
2003-03-24 08:42:13 +0000

    * fixed toolbus example

--- toolbusNG/doc/paper/fmco02/AddressBookService.java
+++ toolbusNG/doc/paper/fmco02/AddressBookService.java
@@ -1,20 +1,56 @@
-import java.util.*
-public class AddressBookService implements AddressBookTif
+import java.util.*;
+
+import aterm.*;
+import aterm.pure.*;
+
+public class TBAddressBookService implements AddressBookTif
 {
+  private static int id = 0;
+  private static int createUniqueID() { return id++; }
+
+  private static ATermFactory factory = new PureFactory();
+
   private Map addressMap;
 
-  public AddressBookService() { addressMap = new HashMap(); }
-
-  private Address find(String id) { return (Address) addressMap.get(id); }
-
-  public void deleteAddress(String id) { addressMap.remove(id); }
-  public void setNumber(String id, int number) { find(id).setNumber(number); }
-  public void setStreet(String id, String street) { find(id).setStreet(street); }
-  public void setZip(String id, String zip) { find(id).setZip(number); }
-  public ATerm createAddress() {
-    Address address = new Address();
-    addressMap.put(address.getId(), address);
-    return address.getId();
+  public TBAddressBookService() {
+    addressMap = new HashMap();
   }
-  public void recTerminate(ATerm t0);
+
+  private AddressBookEntry getEntry(int id) {
+    return (AddressBookEntry) addressMap.get(new Integer(id));
+  }
+
+  public void deleteAddress(int id) {
+    addressMap.remove(new Integer(id));
+  }
+
+  public void setName(int id, String name) {
+    getEntry(id).setName(name);
+  }
+
+  public void setAddress(int id, String address) {
+    System.out.println("setAddress: id=" + id + ", address=" + address);
+    getEntry(id).setAddress(address);
+  }
+
+  public void setZip(int id, String zip) {
+    getEntry(id).setZip(zip);
+  }
+
+  public ATerm createAddress() {
+    Integer id = new Integer(createUniqueID());
+    AddressBookEntry entry = new AddressBookEntry();
+    addressMap.put(id, entry);
+    return factory.make("snd-value(new-address(<int>))", id);
+  }
+
+  public void recTerminate(ATerm t0) {}
+
+  public static final void main(String[] args) throws Exception {
+    IAddressBookService service = new TBAddressBookService();
+    AddressBookBridge bridge = new AddressBookBridge(factory, service);
+    bridge.init(args);
+    bridge.connect();
+    bridge.run();
+  }
 }
