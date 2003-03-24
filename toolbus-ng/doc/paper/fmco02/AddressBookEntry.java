02e641785770cea95085e34c40ec29808bdb6a96
Hayco de Jong <jong@metamud.org>
2003-03-24 08:42:13 +0000

    * fixed toolbus example

---
+++ toolbus-ng/doc/paper/fmco02/AddressBookEntry.java
@@ -0,0 +1,12 @@
+public class AddressBookEntry {
+  private String name;
+  private String address;
+  private String zip;
+
+  public void setName(String name) { this.name = name; }
+  public String getName() { return name; }
+  public void setAddress(String address) { this.address = address; }
+  public String getAddress() { return address; }
+  public void setZip(String zip) { this.zip = zip;}
+  public String getZip() { return zip; }
+}
