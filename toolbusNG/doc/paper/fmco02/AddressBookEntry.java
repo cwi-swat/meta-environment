8c24feaf5888124fbbd6b8f59b281615b325d8d3
Hayco de Jong <jong@metamud.org>
2003-03-24 09:10:52 +0000

    * add description of the AddressBookEntry

--- toolbusNG/doc/paper/fmco02/AddressBookEntry.java
+++ toolbusNG/doc/paper/fmco02/AddressBookEntry.java
@@ -1,12 +1,9 @@
 public class AddressBookEntry {
   private String name;
-  private String address;
-  private String zip;
-
   public void setName(String name) { this.name = name; }
   public String getName() { return name; }
+
+  private String address;
   public void setAddress(String address) { this.address = address; }
   public String getAddress() { return address; }
-  public void setZip(String zip) { this.zip = zip;}
-  public String getZip() { return zip; }
 }
