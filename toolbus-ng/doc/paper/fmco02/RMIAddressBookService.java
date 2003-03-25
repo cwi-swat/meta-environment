ec4d5faf4d3198c632a15a0b3965482d3167b0e9
Hayco de Jong <jong@metamud.org>
2003-03-25 10:11:59 +0000

    * start explanation of differences TB vs RMI

--- toolbus-ng/doc/paper/fmco02/RMIAddressBookService.java
+++ toolbus-ng/doc/paper/fmco02/RMIAddressBookService.java
@@ -1,37 +1,63 @@
-import java.rmi.RemoteException;
-import java.rmi.server.unicastRemoteObject;
+import java.rmi.*;
+import java.rmi.server.*;
 import java.util.*;
 
 public class RMIAddressBookService
   extends UnicastRemoteObject
   implements IAddressBookService
 {
-  private Map addressMap;
+  private static int id = 0;
+  private static int createUniqueID() { return id++; }
+
+  private Map addressById;
+  private Map addressByName;
 
   public RMIAddressBookService() throws RemoteException {
     super();
-    addressMap = new HashMap();
+    addressById = new HashMap();
+    addressByName = new HashMap();
   }
 
   private AddressBookEntry getEntry(int id) {
-    return (AddressBookEntry) addressMap.get(new Integer(id));
+    return (AddressBookEntry) addressById.get(new Integer(id));
   }
 
-  public int createAddress() throws RemoteException {
-    return 0;
+  public int createEntry() {
+    Integer id = new Integer(createUniqueID());
+    AddressBookEntry entry = new AddressBookEntry();
+    addressById.put(id, entry);
+    return id.intValue();
   }
 
-  public void deleteAddress(int id) throws RemoteException {
-    addressMap.remove(new Integer(id));
+  public void deleteEntry(int id) {
+    addressById.remove(new Integer(id));
   }
 
-  public void setName(int id, String name) throws RemoteException {
-    getEntry(id).set
+  public void setName(int id, String name) {
+    AddressBookEntry entry = getEntry(id);
+    addressByName.remove(name);
+    addressByName.put(name, new Integer(id));
+    entry.setName(name);
   }
 
-  public void setAddress(int id, String address) throws RemoteException {
+  public void setAddress(int id, String address) {
+    getEntry(id).setAddress(address);
   }
 
-  public void setZip(int id, String zip) throws RemoteException {
+  public int findByName(String name) {
+    Integer id = (Integer) addressByName.get(name);
+    return id == null ? -1 : id.intValue();
+  }
+
+  public static final void main(String args[]) {
+    String name = "//localhost/IAddressBookService";
+    try {
+      IAddressBookService service = new RMIAddressBookService();
+      Naming.rebind(name, service);
+      System.out.println("AddressBookService bound");
+    } catch (Exception e) {
+      System.err.println("Exception: " + e.getMessage());
+      e.printStackTrace();
+    }
   }
 }
