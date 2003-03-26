ae3e72082bfc86dab93bd922d09b6729b6760efb
Hayco de Jong <jong@metamud.org>
2003-03-26 13:00:25 +0000

    * RMI version of compound data exchange using java.io.Serializable

--- toolbusNG/doc/paper/fmco02/AddressBookEntry.java
+++ toolbusNG/doc/paper/fmco02/AddressBookEntry.java
@@ -1,4 +1,6 @@
-public class AddressBookEntry implements IAddressBookEntry {
+import java.io.Serializable;
+
+public class AddressBookEntry implements IAddressBookEntry, Serializable {
   private String name;
   public void setName(String name) { this.name = name; }
   public String getName() { return name; }
