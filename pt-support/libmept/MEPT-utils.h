dbd10b2431a6c4e272b1062ca8ba76e78c3deb1d
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2003-03-28 15:54:25 +0000

    * moved tree2graph and tree2dot to graph-support package

--- pt-support/libmept/MEPT-utils.h
+++ pt-support/libmept/MEPT-utils.h
@@ -77,16 +77,6 @@ char *PT_yieldSymbolVisualVariables(PT_Symbol symbol);
 ATerm PT_reportTreeAmbiguities(PT_Tree tree);
 ATerm PT_reportParseTreeAmbiguities(PT_ParseTree parsetree);
 
-char* PT_printTreeToDot(PT_Tree tree, ATbool sharing, ATbool characters, 
-			ATbool characters_sharing, ATbool productions, 
-			ATbool layout, ATbool literals);
-char* PT_printParseTreeToDot(PT_ParseTree parsetree, ATbool sharing, 
-			     ATbool characters_sharing, ATbool characters, 
-			     ATbool productions, ATbool layout, 
-			     ATbool literals);
-char* PT_printAnyToDot(ATerm term, ATbool sharing, ATbool characters, 
-		       ATbool characters_sharing, ATbool productions, 
-		       ATbool layout, ATbool literals);
 
 ATerm PT_implodeParseTree(PT_ParseTree tree,
 			  ATbool _interpret_cons ,
