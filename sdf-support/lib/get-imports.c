aa93fac66f408dd3cbdf5da6be359a3495fffbca
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2003-05-19 15:48:33 +0000

    * revived some code

--- sdf-support/lib/get-imports.c
+++ sdf-support/lib/get-imports.c
@@ -50,6 +50,59 @@ collect_imports(SDF_Grammar grammar, ATermList *imports)
 }
 
 /*}}}  */
+
+/*{{{  KLOON: getImpsectionImportsList(SDF_ImpSection impsection) */
+
+static  SDF_ImportList
+getImpsectionImportsList(SDF_ImpSection impsection)
+{
+    return SDF_getImportsList(SDF_getImpSectionList(impsection));
+}
+
+/*}}}  */
+/*{{{ KLOON:  collect_imports_list(SDF_Grammar grammar, SDF_ImportList *imports) */
+
+static void
+collect_imports_list(SDF_Grammar grammar, SDF_ImportList *imports)
+{
+    if (SDF_hasGrammarImpSection(grammar)) {
+          SDF_ImpSection impsection = SDF_getGrammarImpSection(grammar);
+	      *imports = SDF_concatImportList(*imports, 
+					                                          getImpsectionImportsList(impsection));
+	        }
+}
+
+/*}}}  */
+/*{{{  KLOON: SDF_ImportList SDF_getModuleImportsList(SDF_Module module) */
+
+SDF_ImportList SDF_getModuleImportsList(SDF_Module module)
+{
+  SDF_ImpSectionList imps;
+  SDF_Sections       sections;
+  SDF_ImportList     imports = SDF_makeImportListEmpty();
+
+  imps = SDF_getModuleList(module);
+  while (!SDF_isImpSectionListEmpty(imps)) {
+    SDF_ImpSection impsection = SDF_getImpSectionListHead(imps);
+    imports = SDF_concatImportList(getImpsectionImportsList(impsection),
+                                   imports);
+
+    if (SDF_isImpSectionListSingle(imps)) {
+      break;
+    }
+    imps = SDF_getImpSectionListTail(imps);
+  }
+
+  sections = SDF_getModuleSections(module);
+  SDFforeachGrammarInSections(sections,
+                              (SDFGrammarFunc)collect_imports_list,
+                              &imports);
+
+  return imports;
+}
+
+/*}}}  */
+
 /*{{{  ATermList SDF_getImports(SDF_Module module) */
 
 ATermList SDF_getImports(SDF_Module module)
