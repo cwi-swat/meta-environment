6fccf911226c7329d8a22cd662dab62754292187
Mark van den Brand <M.G.J.v.d.Brand@tue.nl>
2003-06-30 08:48:15 +0000

    - Removed the explicit knowledge of how to show the source of a node.

--- meta/meta-studio/metastudio/ParseTreePanel.java
+++ meta/meta-studio/metastudio/ParseTreePanel.java
@@ -2,6 +2,7 @@ package metastudio;
 
 import metastudio.graph.*;  
 import java.awt.event.*;   
+import aterm.*;   
 
 
 public class ParseTreePanel extends GraphPanel
@@ -22,9 +23,10 @@ public class ParseTreePanel extends GraphPanel
           Node node = getNodeAt(event.getX(), event.getY());
           if (node != getSelectedNode()) {
             if (node != null) {
-              if (node.hasInfo("pos-info")) {
-                 //System.out.println("Node has info " + node.getInfo("pos-info"));
-                 bridge.postEvent(factory.make("element-selected([\"ShowSource\"], <term>)", node.getInfo("pos-info")));
+              if (node.hasInfo("message")) {
+                 //ATerm message = node.getInfo("message");
+                 //System.out.println("Node has info " + message);
+                 bridge.postEvent(node.getInfo("message"));
               }
             }
           }
