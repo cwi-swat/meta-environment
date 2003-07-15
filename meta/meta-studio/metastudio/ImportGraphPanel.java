966d1798028d79730ba65fe680d056fe68cdd6e6
Hayco de Jong <jong@metamud.org>
2003-07-15 08:07:04 +0000

    * changed obfuscated construct:
      boolean b = false;
      b |= e.isPopupTrigger();
      if (b) ...
    
    into
    
      if (e.isPopupTrigger()) ...

--- meta/meta-studio/metastudio/ImportGraphPanel.java
+++ meta/meta-studio/metastudio/ImportGraphPanel.java
@@ -7,35 +7,35 @@ import java.awt.event.MouseListener;
 import metastudio.graph.Node;
 
 public class ImportGraphPanel extends GraphPanel implements ModuleSelectionListener {
-	private ModuleTreeModel moduleManager;
+  private ModuleTreeModel moduleManager;
 
-	public ImportGraphPanel(final ModuleTreeModel moduleManager) {
-		super("import");
-		this.moduleManager = moduleManager;
-		moduleManager.addModuleSelectionListener(this);
+  public ImportGraphPanel(final ModuleTreeModel moduleManager) {
+    super("import");
+    this.moduleManager = moduleManager;
+    moduleManager.addModuleSelectionListener(this);
 
-		MouseListener listener = new MouseAdapter() {
-			public void mouseClicked(MouseEvent event) {
-				Node node = getNodeAt(event.getX(), event.getY());
-				if (node != getSelectedNode()) {
-					Module module;
-					if (node == null) {
-						module = null;
-					} else {
-						module = moduleManager.getModule(node.getName());
-					}
-					moduleManager.selectModule(module);
-				}
-			}
-		};
-		addMouseListener(listener);
+    MouseListener listener = new MouseAdapter() {
+      public void mouseClicked(MouseEvent event) {
+	Node node = getNodeAt(event.getX(), event.getY());
+	if (node != getSelectedNode()) {
+	  Module module;
+	  if (node == null) {
+	    module = null;
+	  } else {
+	    module = moduleManager.getModule(node.getName());
+	  }
+	  moduleManager.selectModule(module);
 	}
+      }
+    };
+    addMouseListener(listener);
+  }
 
-	public void moduleSelected(Module module) {
-		if (module == null) {
-			setSelectedNode(null);
-		} else {
-			setSelectedNode(getNode(module.getName()));
-		}
-	}
+  public void moduleSelected(Module module) {
+    if (module == null) {
+      setSelectedNode(null);
+    } else {
+      setSelectedNode(getNode(module.getName()));
+    }
+  }
 }
--- meta/meta-studio/metastudio/ParseTreePanel.java
+++ meta/meta-studio/metastudio/ParseTreePanel.java
@@ -8,27 +8,27 @@ import metastudio.graph.MetaGraphFactory;
 import metastudio.graph.Node;
 
 public class ParseTreePanel extends GraphPanel {
-	private UserInterfaceBridge bridge;
-	private static MetaGraphFactory factory;
+  private UserInterfaceBridge bridge;
+  private static MetaGraphFactory factory;
 
-	public ParseTreePanel(UserInterfaceBridge br, MetaGraphFactory fac) {
+  public ParseTreePanel(UserInterfaceBridge br, MetaGraphFactory fac) {
 
-		super("parsetree");
-		bridge = br;
-		factory = fac;
+    super("parsetree");
+    bridge = br;
+    factory = fac;
 
-		MouseListener listener = new MouseAdapter() {
-			public void mouseClicked(MouseEvent event) {
-				Node node = getNodeAt(event.getX(), event.getY());
-				if (node != getSelectedNode()) {
-					if (node != null) {
-						if (node.hasInfo("message")) {
-							bridge.postEvent(node.getInfo("message"));
-						}
-					}
-				}
-			}
-		};
-		addMouseListener(listener);
+    MouseListener listener = new MouseAdapter() {
+      public void mouseClicked(MouseEvent event) {
+	Node node = getNodeAt(event.getX(), event.getY());
+	if (node != getSelectedNode()) {
+	  if (node != null) {
+	    if (node.hasInfo("message")) {
+	      bridge.postEvent(node.getInfo("message"));
+	    }
+	  }
 	}
+      }
+    };
+    addMouseListener(listener);
+  }
 }
