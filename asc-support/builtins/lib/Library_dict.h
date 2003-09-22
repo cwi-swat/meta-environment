bc669a0b2237a4c28c37ff087d19b28eab2e422c
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2003-09-22 08:08:26 +0000

    * Fixed some bugs in makefiles

--- asc-support/builtins/lib/Library_dict.h
+++ asc-support/builtins/lib/Library_dict.h
@@ -1,5 +1,5 @@
 /*
- * Generated at Mon Sep 22 10:03:17 2003
+ * Generated at Mon Sep 22 10:05:25 2003
  */
 
 #ifndef __Library_dict_H
@@ -7,7 +7,48 @@
 
 #include <aterm2.h>
 
+extern AFun CO_afun0;
+extern AFun CO_afun1;
+extern AFun CO_afun2;
+extern AFun CO_afun3;
+extern AFun CO_afun4;
+extern AFun CO_afun5;
+extern AFun CO_afun6;
+extern AFun CO_afun7;
+extern AFun CO_afun8;
+extern AFun CO_afun9;
+extern AFun CO_afun10;
+extern AFun CO_afun11;
+extern AFun CO_afun12;
+extern AFun CO_afun13;
+extern AFun CO_afun14;
+extern AFun CO_afun15;
+extern AFun CO_afun16;
+extern AFun CO_afun17;
+extern AFun CO_afun18;
+extern AFun CO_afun19;
+extern AFun CO_afun20;
+extern AFun CO_afun21;
+extern AFun CO_afun22;
+extern AFun CO_afun23;
+extern AFun CO_afun24;
+extern AFun CO_afun25;
+extern AFun CO_afun26;
+extern AFun CO_afun27;
+extern AFun CO_afun28;
+extern AFun CO_afun29;
 
+extern ATerm CO_patternOptLayoutPresent;
+extern ATerm CO_patternOptLayoutAbsent;
+extern ATerm CO_patternStartBoolean;
+extern ATerm CO_patternStartBoolCon;
+extern ATerm CO_patternBooleanBracket;
+extern ATerm CO_patternBooleanNot;
+extern ATerm CO_patternBooleanAnd;
+extern ATerm CO_patternBooleanOr;
+extern ATerm CO_patternBooleanConstant;
+extern ATerm CO_patternBoolConFalse;
+extern ATerm CO_patternBoolConTrue;
 
 extern void init_Library_dict();
 
