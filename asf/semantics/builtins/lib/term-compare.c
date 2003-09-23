5bb5d672b3a106c8b86c139cbc41a6bb15a18187
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2003-09-23 10:04:41 +0000

    * made CompareTerm module whitespace and annotations

--- asf/semantics/builtins/lib/term-compare.c
+++ asf/semantics/builtins/lib/term-compare.c
@@ -1,12 +1,15 @@
 #include <Library.h>
 #include "common.h"
-
+#include <aterm2.h>
 
 
 PT_Tree term_compare_less(PT_Tree input)
 {
   PT_Tree left = CO_getFunctionArgument(input,0);
   PT_Tree right = CO_getFunctionArgument(input,1);
+  
+  left = PT_removeTreeAllLayoutAndAnnotations(left);
+  right = PT_removeTreeAllLayoutAndAnnotations(right);
 
   if (((int) left) < ((int) right)) {
    return (PT_Tree) CO_makeBooleanConstant(CO_makeBoolConTrue());
