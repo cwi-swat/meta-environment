307201f9d3ca5731c7e98015cf172c3df648a4b7
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2003-09-29 14:59:30 +0000

    * added test terms

---
+++ asf-library/library/languages/c/examples/small.c
@@ -0,0 +1,46 @@
+
+static int a = 0;
+static int f(int a, int b);
+static Aap g(void a, void b);
+char * aap(noot t) ;
+char * aap(noot t) { int i = 1; }
+aap (*noot) ();
+
+f()
+{
+  char a[] = "Hello, world!";
+}
+
+int main(void) 
+{
+  i - 1;
+  sjaak[i];
+  sjaak[i - 1] = 0; 
+
+  /* a ++ b;  is illegal */
+  aap & & aap;
+  aap && aap;
+
+  !aap->mies++;
+  aap[noot]->mies++;
+  !aap[noot];
+  !aap[noot]->mies++;
+
+  { a b; b *b; }
+  { a b; b *b; a;}
+  { b *b = c; a; } /* works because of injection counting */
+  /* { a b; b *b = c; a; }  ambiguous assignment expression or initializer */
+  { a b; b *b = c; int b; a; } /* no problem because not latest decl... */
+
+  (a) ( (a) b );
+
+  /* { aap (noot(t)); } ambiguous declaration or statement */
+
+  a = *--b;
+
+
+}
+
+
+
+
