14b1fd395b7780ff864dea1e489a4f8d563a4635
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2003-11-03 10:49:32 +0000

    * liftPT tool changes to lowering tool by providing the -l option

--- pt-support/utils/liftPT-main.c
+++ pt-support/utils/liftPT-main.c
@@ -20,8 +20,8 @@
 /*{{{  globals */
 
 static char myname[]    = "parsePT";
-static char myversion[] = "1.0";
-static char myarguments[] = "ahi:o:tV";
+static char myversion[] = "2.0";
+static char myarguments[] = "ahi:lo:tV";
 
 /*}}}  */
 
@@ -30,11 +30,19 @@ static char myarguments[] = "ahi:o:tV";
 void usage(void)
 {
     fprintf(stderr,
+	"This tool can do several operations on AsFix and ATerms:\n"
+	"\t* lift an ATerm to a parse tree of an ATerm\n"
+	"\t* lower a parse tree of an ATerm to a ATerm\n"
+	"\t* lift a parse tree to a parse tree of a parse tree\n"
+	"\t* lower a parse tree of a parse tree to a parse tree\n\n"
+	"The tool will always lift a parse tree to a parse tree unless the\n"
+	"-a option is provided. Other ATerms are lifted to parsed ATerms.\n\n"
         "Usage: %s -h -i arg -o arg -V . . .\n"
         "Options:\n"
 	"\t-a              lift to parsed ATerm always, not AsFix (default off)\n"
         "\t-h              display help information (usage)\n"
         "\t-i filename     input from file (default stdin)\n"
+	"\t-l              lower instead of lift\n"
         "\t-o filename     output to file (default stdout)\n"
 	"\t-t              output textual instead of binary (default off)\n"
         "\t-V              reveal program version (i.e. %s)\n",
@@ -53,6 +61,7 @@ int main (int argc, char **argv)
   ATerm output = NULL;
   ATbool aterms = ATfalse;
   ATbool textual = ATfalse;
+  ATbool lower = ATfalse;
   char   *input_file_name  = "-";
   char   *output_file_name = "-";
   
@@ -61,6 +70,7 @@ int main (int argc, char **argv)
       case 'a':  aterms = ATtrue;         break;
       case 'h':  usage();                      exit(0);
       case 'i':  input_file_name  = optarg;    break;
+      case 'l':  lower = ATtrue; break;
       case 'o':  output_file_name = optarg;    break;
       case 't':  textual = ATtrue; break;
       case 'V':  fprintf(stderr, "%s %s\n", myname, myversion);
@@ -80,22 +90,50 @@ int main (int argc, char **argv)
     return 1;
   }
 
-  if (!aterms && ATmatch(input, "parsetree(<term>,<term>)",NULL,NULL)) {
-    output = (ATerm) PTPT_liftParseTree((PT_ParseTree) input);
-  }
-  else if (!aterms && ATmatch(input, "appl(<term>,<term>)", NULL, NULL)) {
-    output = (ATerm) PTPT_liftTree((PT_Tree) input);
-  }
-  else if (!aterms && ATmatch(input, "amb(<term>)", NULL)) {
-    output = (ATerm) PTPT_liftTree((PT_Tree) input);
+  if (!lower) {
+    if (!aterms && ATmatch(input, "parsetree(<term>,<term>)",NULL,NULL)) {
+      output = (ATerm) PTPT_liftParseTree((PT_ParseTree) input);
+    }
+    else if (!aterms && ATmatch(input, "appl(<term>,<term>)", NULL, NULL)) {
+      output = (ATerm) PTPT_liftTree((PT_Tree) input);
+    }
+    else if (!aterms && ATmatch(input, "amb(<term>)", NULL)) {
+      output = (ATerm) PTPT_liftTree((PT_Tree) input);
+    }
+    else {
+      output = (ATerm) PTPT_liftATerm(input);
+    }
+  
+    if(output != NULL) {
+      output = (ATerm) PT_makeValidParseTreeFromTree((PT_Tree) output);
+    }
   }
   else {
-    output = (ATerm) PTPT_liftATerm(input);
+    PT_ParseTree pt = PT_ParseTreeFromTerm(input);
+    PT_Tree tree = PT_getParseTreeTree(pt);
+    
+    if (PT_hasTreeProd(tree)) {
+      PT_Symbol sym = PT_getProductionRhs(PT_getTreeProd(tree));
+
+      if (PT_isEqualSymbol(sym, PT_makeSymbolCf(PT_makeSymbolSort("ATerm")))) {
+	output = PTPT_lowerATerm((PTPT_ATerm) tree);
+      }
+      else if (PT_isEqualSymbol(sym,
+				PT_makeSymbolCf(PT_makeSymbolSort("Tree")))) {
+	output = (ATerm) PTPT_lowerTree((PTPT_Tree) tree);
+      }
+      else if (PT_isEqualSymbol(sym,
+				PT_makeSymbolCf(PT_makeSymbolSort("ParseTree")))) {
+	output = (ATerm) PTPT_lowerParseTree((PTPT_ParseTree) tree);
+      }
+      else {
+	ATwarning("File is not in a format that can be lowered\n");
+      }
+    }
   }
+    
 
   if(output != NULL) {
-    output = (ATerm) PT_makeValidParseTreeFromTree((PT_Tree) output);
-
     if (textual) {
       ATwriteToNamedTextFile(output, output_file_name);
     }
