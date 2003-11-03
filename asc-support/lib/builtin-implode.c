ea776702a0b9f8741b0de909598ab4d8ebf27c0e
Tijs van der Storm <T.van.der.Storm@cwi.nl>
2003-11-03 12:34:36 +0000

    Added implode builtin.

---
+++ asc-support/lib/builtin-implode.c
@@ -0,0 +1,41 @@
+#include <PTMEPT-utils.h>
+#include <MEPT-utils.h>
+#include "builtin-common.h"
+
+static PT_Tree implode_tree(PT_Tree tree);
+
+/*{{{  PT_Tree ASFE_implode(PT_Tree input) */
+
+PT_Tree ASFE_implode(PT_Tree input)
+{
+  PT_Tree tree = CO_getFunctionArgument(input,0);
+  return implode_tree(tree);
+}
+
+/*}}}  */
+/*{{{  PT_Tree ASC_implode(ATerm input) */
+
+PT_Tree ASC_implode(ATerm input)
+{
+  PT_Tree tree = muASFToTree(input);
+
+  return implode_tree(tree);
+}
+
+static PT_Tree implode_tree(PT_Tree tree) {
+  PT_ParseTree pt = PT_makeValidParseTreeFromTree(tree);
+  return (PT_Tree)PTPT_liftATerm(PT_implodeParseTree(pt,
+						     ATtrue,
+						     ATtrue,
+						     ATtrue,
+						     ATtrue,
+						     ATtrue,
+						     ATtrue,
+						     ATfalse,
+						     ATtrue,
+						     ATtrue,
+						     ATtrue,
+						     ATtrue));
+}
+
+/*}}}  */
