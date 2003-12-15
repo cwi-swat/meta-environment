31e77c0796784045365ec28d32acf3e8aaeb9eea
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2003-12-15 09:15:50 +0000

    * CHARLIST sort is not used anymore in generated APIs! Instead,    for each lexical and the OptLayout sort, field are made of type <chars>.    This leads to an interface that uses directly 'char*' or String in C    and Java, but builds an ATermList of the ASCII characters.    For example: makeOptLayoutPresent(" "), will make a layout parse tree    containing somewhere a singleton [32].  * Generation of bottom-sorts removed accordingly  * Shell scripts simplified

--- sdf-apigen/test/PeanoTest.java
+++ sdf-apigen/test/PeanoTest.java
@@ -13,7 +13,7 @@ public class PeanoTest {
 
   public PeanoTest(Factory factory) {
     this.factory = factory;
-    this.e = factory.makeOptLayout_Present(factory.makeCHARLIST(' '));
+    this.e = factory.makeOptLayout_Present(" ");
   }
 
   public Nat make(int n) {
