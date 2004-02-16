14e7107f4fe5e6e48ec3947b6c92af51bac7a9c6
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2004-02-16 09:27:22 +0000

    * term-compare builtin changed to term-less

--- asc-support/lib/builtin-term-compare.c
+++ asc-support/lib/builtin-term-compare.c
@@ -4,16 +4,13 @@
 
 /*{{{  static PT_Tree makeMeasureFromInt(int result) */
 
-static PT_Tree makeMeasureFromInt(int result)
+static PT_Tree makeBooleanFromInt(int result)
 {
-  if (result == 0) {
-    return PT_TreeFromTerm(CO_MeasureToTerm(CO_makeMeasureEqual()));
-  }
-  else if (result < 0) {
-    return PT_TreeFromTerm(CO_MeasureToTerm(CO_makeMeasureLess()));
+  if (result < 0 ) {
+    return (PT_Tree) CO_makeBooleanConstant(CO_makeBoolConTrue());
   }
   else {
-    return PT_TreeFromTerm(CO_MeasureToTerm(CO_makeMeasureGreater()));
+    return (PT_Tree) CO_makeBooleanConstant(CO_makeBoolConFalse());
   }
 }
 
@@ -21,19 +18,19 @@ static PT_Tree makeMeasureFromInt(int result)
 
 /*{{{  PT_Tree ASFE_term_compare(PT_Tree input) */
 
-PT_Tree ASFE_term_compare(PT_Symbol type, PT_Tree left, PT_Tree right)
+PT_Tree ASFE_term_less(PT_Symbol type, PT_Tree left, PT_Tree right)
 {
   int result = PT_compareTree(left, right);
 
-  return makeMeasureFromInt(result);
+  return makeBooleanFromInt(result);
 }
 
 /*}}}  */
 /*{{{  PT_Tree ASC_term_compare(ATerm left, ATerm right) */
 
-PT_Tree ASC_term_compare(ATerm type, ATerm left, ATerm right)
+PT_Tree ASC_term_less(ATerm type, ATerm left, ATerm right)
 {
-  return makeMeasureFromInt(ATcompare(left, right));
+  return makeBooleanFromInt(ATcompare(left, right));
 }
 
 /*}}}  */
