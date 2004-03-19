c1c628ff9e2265c67b29d43fc2318018978ac0a5
Hayco de Jong <jong@metamud.org>
2004-03-19 07:34:52 +0000

    * libptmept/PTMEPT_bottom.c:    - add missing #include <stdlib.h> for malloc()

--- pt-support/libptmept/PTMEPT_bottom.c
+++ pt-support/libptmept/PTMEPT_bottom.c
@@ -1,8 +1,20 @@
+/*{{{  includes */
+
+#include <stdlib.h>
 #include <string.h>
 #include <aterm2.h>
+
 #include "PTMEPT.h"
 
+/*}}}  */
+
+/*{{{  typedefs */
+
 typedef struct ATerm _PTPT_CHARLIST;
+
+/*}}}  */
+
+/*{{{  static ATermList PTPT_convertStringToChars(char *str) */
 
 static ATermList PTPT_convertStringToChars(char *str)
 {
@@ -16,10 +28,13 @@ static ATermList PTPT_convertStringToChars(char *str)
   return list;
 }
 
-static char * PTPT_convertCharsToString(ATermList list)
+/*}}}  */
+/*{{{  static char *PTPT_convertCharsToString(ATermList list) */
+
+static char *PTPT_convertCharsToString(ATermList list)
 {
   int i;
-  char *tmp = (char*) malloc((1 + ATgetLength(list)) * sizeof(char));
+  char *tmp = (char *) malloc((1 + ATgetLength(list)) * sizeof(char));
 
   for(i = 0; !ATisEmpty(list); list = ATgetNext(list), i++) {
     tmp[i] = ATgetInt((ATermInt) ATgetFirst(list));
@@ -29,37 +44,61 @@ static char * PTPT_convertCharsToString(ATermList list)
   return tmp;
 }
 
+/*}}}  */
+
+/*{{{  ATbool PTPT_isValidCHARLIST(PTPT_CHARLIST arg) */
+
 ATbool PTPT_isValidCHARLIST(PTPT_CHARLIST arg)
 {
   return ATtrue;
 }
+
+/*}}}  */
+/*{{{  ATbool PTPT_isCHARLISTString(PTPT_CHARLIST arg) */
 
 ATbool PTPT_isCHARLISTString(PTPT_CHARLIST arg)
 {
   return ATtrue;
 }
 
-char*  PTPT_getCHARLISTString(PTPT_CHARLIST arg)
+/*}}}  */
+/*{{{  char *PTPT_getCHARLISTString(PTPT_CHARLIST arg) */
+
+char *PTPT_getCHARLISTString(PTPT_CHARLIST arg)
 {
   return PTPT_convertCharsToString((ATermList) arg);
 }
+
+/*}}}  */
+/*{{{  PTPT_CHARLIST  PTPT_setCHARLISTString(PTPT_CHARLIST arg, char *str) */
 
 PTPT_CHARLIST  PTPT_setCHARLISTString(PTPT_CHARLIST arg, char *str)
 {
   return (PTPT_CHARLIST) PTPT_convertStringToChars(str);
 }
 
-ATerm  PTPT_CHARLISTToTerm(PTPT_CHARLIST arg)
+/*}}}  */
+/*{{{  ATerm PTPT_CHARLISTToTerm(PTPT_CHARLIST arg) */
+
+ATerm PTPT_CHARLISTToTerm(PTPT_CHARLIST arg)
 {
   return (ATerm) arg;
 }
+
+/*}}}  */
+/*{{{  PTPT_CHARLIST PTPT_CHARLISTFromTerm(ATerm trm) */
 
 PTPT_CHARLIST PTPT_CHARLISTFromTerm(ATerm trm)
 {
   return (PTPT_CHARLIST) trm;
 }
 
+/*}}}  */
+/*{{{  PTPT_CHARLIST PTPT_makeCHARLISTString(char *str) */
+
 PTPT_CHARLIST PTPT_makeCHARLISTString(char *str)
 {
   return (PTPT_CHARLIST) PTPT_convertStringToChars(str);
 }
+
+/*}}}  */
