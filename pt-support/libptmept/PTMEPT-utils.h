530a02e87882d66bedfb0c47d1086065c68e5d54
Hayco de Jong <jong@metamud.org>
2004-03-23 08:39:22 +0000

    * libptmept: remove list reverse functions that are now generated

--- pt-support/libptmept/PTMEPT-utils.h
+++ pt-support/libptmept/PTMEPT-utils.h
@@ -1,22 +1,15 @@
-
 #ifndef PTMEPT_UTILS_H
 #define PTMEPT_UTILS_H
 
 #include <PTMEPT.h>
 #include <MEPT.h>
 
-
-PTPT_TreeList PTPT_reverseTreeList(PTPT_TreeList args);
-PTPT_SymbolList PTPT_reverseSymbolList(PTPT_SymbolList args);
-PTPT_AttrList PTPT_reverseAttrList(PTPT_AttrList args);
-PTPT_CharRangeList PTPT_reverseCharRangeList(PTPT_CharRangeList args);
-PTPT_ATermElems PTPT_reverseATermElems(PTPT_ATermElems args);
-
 PTPT_ParseTree PTPT_liftParseTree(PT_ParseTree pt);
-PTPT_Tree      PTPT_liftTree(PT_Tree pt);
-PTPT_ATerm     PTPT_liftATerm(ATerm trm);
+PTPT_Tree PTPT_liftTree(PT_Tree pt);
+PTPT_ATerm PTPT_liftATerm(ATerm trm);
 
 PT_ParseTree PTPT_lowerParseTree(PTPT_ParseTree pt);
-PT_Tree      PTPT_lowerTree(PTPT_Tree pt);
-ATerm     PTPT_lowerATerm(PTPT_ATerm trm);
+PT_Tree PTPT_lowerTree(PTPT_Tree pt);
+ATerm PTPT_lowerATerm(PTPT_ATerm trm);
+
 #endif
