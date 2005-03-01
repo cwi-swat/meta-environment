b5a8e0849339fb365b657faeee73fac7577aac67
Hayco de Jong <jong@metamud.org>
2005-03-01 12:39:47 +0000

    *** empty log message ***

--- meta-query/extraction/toolbus/extract.sh
+++ meta-query/extraction/toolbus/extract.sh
@@ -1,1 +1,1 @@
-sglr -p Tscript.trm.tbl -i meta-1.6.tb | addPosInfo -p meta-1.6.tb | ./ExtractAll -f extractAll -r RSTORE -i - | unparsePT > meta-1.6.rstore 
+sglr -p Tscript.trm.tbl -i refactor.trm | addPosInfo -p refactor.trm | ./ExtractAll -f extractAll -r RSTORE -i - | unparsePT > refactor.rstore 
