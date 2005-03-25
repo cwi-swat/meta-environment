e3f0da1c94b030eaf41ba76bb8c3fabbc252fc31
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2005-03-25 10:38:30 +0000

    *** empty log message ***

---
+++ asf-library/library/languages/c/examples/smallcpp.c
@@ -0,0 +1,73 @@
+# 1 "small.c"
+# 1 "<built-in>"
+# 1 "<command line>"
+# 1 "small.c"
+# 10 "small.c"
+typedef int Aap;
+
+
+static Aap g(void a, void b);
+char * aap(noot t) ;
+char * aap(noot t) { int i = 1; }
+aap (*noot) ();
+static int fiets;
+
+
+f()
+{
+  char a[] = "Hello, world!";
+}
+
+static int expressions(char static typedef **argv[2+9][])
+{
+  int a[] = {1,2,3,4};
+
+  1 + 2 * 3;
+  1 + 2 [3];
+  *1 - --2 + ++2;
+  *(1 << 2 << 3)[3] += 5 += 9;
+  1 & 2 ^ 7 ++ +12.34, 67;
+
+  while (++*c = *d++);
+}
+
+int main(int **[][], char *argv[])
+{
+  int a = 1, c[3];
+  f(a, b?a:v, c);
+  i - 1;
+  sjaak[i] = (int**) i;
+  sjaak[i - 1] = 0.E-1;
+# 53 "small.c"
+  aap & & aap;
+  aap && aap;
+  a+++b;
+  a+(++b++);
+
+  !aap->mies++;
+  aap[noot]->mies++;
+  !aap[noot];
+  !aap[noot]->mies++;
+
+  { a b; b const *const *const b; }
+  { a b; b *b; a;}
+  { b *b = c; a; }
+
+  { a b; b *b = c; int b; a; }
+
+  h, a + b ? a : b ? a + b + c , v : z + d, d;
+
+  {
+    typedef int a;
+    (a) (a);
+    (a) ( (a) b );
+  }
+
+  if (a = b = c);
+
+
+
+  a = *--b;
+
+
+}
