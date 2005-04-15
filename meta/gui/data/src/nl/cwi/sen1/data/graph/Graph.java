e8593655e4651aa8356949a6a02aa78aab39148f
Hayco de Jong <jong@metamud.org>
2005-04-15 12:54:31 +0000

    * restructured and now uses ant to build

--- meta/gui/data/src/nl/cwi/sen1/data/graph/Graph.java
+++ meta/gui/data/src/nl/cwi/sen1/data/graph/Graph.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.Vector;
 
-import nl.cwi.sen1.gui.component.NodeSizer;
+import nl.cwi.sen1.data.NodeSizer;
 import aterm.ATerm;
 import aterm.ATermAppl;
 import aterm.ATermList;
