b0baabd60b06ebbb4980f886ef82ee8572847334
Taeke Kooiker <taeke@kooiker.info>
2005-06-04 13:01:17 +0000

    * utils/error-support.c:    - Remove declaration after code

--- error-support/utils/error-support.c
+++ error-support/utils/error-support.c
@@ -245,11 +245,12 @@ ATerm get_summary_errors(int cid, ATerm t)
 ATerm lower_summary(int cid, ATerm t)
 {
   PERR_Start startSummary = PERR_StartFromTerm(ATBunpack(t));
+  PERR_Summary pSummary;
 
   ATwarning("error-support: FIXME, ApiGen generated start injections are wrong\n");
   /* FIX ME in APIGEN! */
   /* if (PERR_isStartSummary(startSummary)) { */
-    PERR_Summary pSummary = PERR_getStartTopSummary(startSummary);
+    pSummary = PERR_getStartTopSummary(startSummary);
     t = ERR_SummaryToTerm(PERR_lowerSummary(pSummary));
   /*
   }
