c79c01ecb4b3c43ca59b1b5cc2ccd62e579bb12d
Taeke Kooiker <taeke@kooiker.info>
2005-06-16 12:33:57 +0000

    * src/nl/cwi/sen1/gui/plugin/EditorTextArea.java:    - Turn off EOL markers    - Show selection in focus  * src/nl/cwi/sen1/gui/plugin/EditorPanel.java:    - Fix modified notification

--- editor-plugin/src/nl/cwi/sen1/gui/plugin/EditorPanel.java
+++ editor-plugin/src/nl/cwi/sen1/gui/plugin/EditorPanel.java
@@ -7,44 +7,47 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 import javax.swing.JPanel;
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
 
 import errorapi.types.Area;
 
 public class EditorPanel extends JPanel {
-	private String id;
+    private String id;
 
     private String filename;
-    
+
     private EditorTextArea textArea;
-    
+
     private boolean modified;
 
     public EditorPanel(String id, String filename) throws IOException {
         this.id = id;
         this.filename = filename;
-        
+
         setLayout(new BorderLayout());
 
-//        SyntaxDocument document = new SyntaxDocument();
-//        document.addUndoableEditListener(new UndoableEditListener() {
-//            public void undoableEditHappened(UndoableEditEvent e) {
-//                setModified(true);
-//            }
-//        });
-
+        // SyntaxDocument document = new SyntaxDocument();
         textArea = new EditorTextArea();
-//        textArea.setDocument(document);
+        // textArea.setDocument(document);
+
+        textArea.getDocument().addUndoableEditListener(
+                new UndoableEditListener() {
+                    public void undoableEditHappened(UndoableEditEvent e) {
+                        setModified(true);
+                    }
+                });
 
         textArea.setText(readContents(filename));
         modified = false;
         textArea.setCaretPosition(0);
         add(textArea, BorderLayout.CENTER);
     }
-    
+
     public EditorTextArea getTextArea() {
         return textArea;
     }
-    
+
     private String readContents(String filename) throws IOException {
         try {
             FileInputStream fis = new FileInputStream(filename);
@@ -60,11 +63,11 @@ public class EditorPanel extends JPanel {
 
     public void writeContents() throws IOException {
         String text = textArea.getText();
-        
+
         FileOutputStream fos = new FileOutputStream(filename);
         fos.write(text.getBytes());
     }
-    
+
     public void setFocus(Area focus) {
         textArea.setFocus(focus);
     }
--- editor-plugin/src/nl/cwi/sen1/gui/plugin/EditorTextArea.java
+++ editor-plugin/src/nl/cwi/sen1/gui/plugin/EditorTextArea.java
@@ -8,16 +8,25 @@ import org.ujac.ui.editor.TextArea;
 import errorapi.types.Area;
 
 public class EditorTextArea extends TextArea {
-    public EditorTextArea() {
-        super();
+    private static final Color focusColor = new Color(255, 165, 0);
 
-    }
-    private static final Color focusColor = new Color(255,165,0);
     private Area focus;
 
+    public EditorTextArea() {
+        super();
+        setEOLMarkersPainted(false);
+    }
+
+    /* 
+     * 1. Highlight current line
+     * 2. Paint focus
+     * 3. Paint selection
+     * 4. Paint highlight
+     * 5. Paint brackethighlight
+     * 6. Paint caret
+     */
     protected void paintHighlight(Graphics gfx, int line, int y) {
-        if (line >= getSelectionStartLine()
-                && line <= getSelectionEndLine()) {
+        if (line >= getSelectionStartLine() && line <= getSelectionEndLine()) {
             paintLineHighlight(gfx, line, y);
         }
 
@@ -32,6 +41,15 @@ public class EditorTextArea extends TextArea {
                 gfx.setColor(focusColor);
                 paintArea(gfx, line, y, startLine, endLine, startCol, endCol);
             }
+        }
+
+        if (line >= getSelectionStartLine() && line <= getSelectionEndLine()
+                && !(getSelectionStart() == getSelectionEnd())) {
+            paintLineHighlight(gfx, line, y);
+        }
+
+        if (highlight != null) {
+            highlight.paintHighlight(gfx, line, y);
         }
 
         if (bracketHighlight && line == getBracketLine())
