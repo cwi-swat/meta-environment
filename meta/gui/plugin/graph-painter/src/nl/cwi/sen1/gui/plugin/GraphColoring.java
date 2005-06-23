1af2dfbc8389618f9c9e66373ba8baaffa4d40db
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2005-06-23 08:16:45 +0000

    * fixed highlighting bug

--- meta/gui/plugin/graph-painter/src/nl/cwi/sen1/gui/plugin/GraphColoring.java
+++ meta/gui/plugin/graph-painter/src/nl/cwi/sen1/gui/plugin/GraphColoring.java
@@ -5,48 +5,62 @@ import java.awt.Paint;
 
 import nl.cwi.sen1.util.Preferences;
 import edu.berkeley.guir.prefuse.EdgeItem;
+import edu.berkeley.guir.prefuse.NodeItem;
 import edu.berkeley.guir.prefuse.VisualItem;
 import edu.berkeley.guir.prefuse.action.assignment.ColorFunction;
 
 public class GraphColoring extends ColorFunction {
 	private Color edgeFillColor;
-	
+
 	public GraphColoring(Preferences prefs) {
 		super();
 		setDefaultFillColor(prefs.getColor("graph.node.background"));
 		setDefaultColor(prefs.getColor("graph.node.foreground"));
-		setFocusColor(prefs.getColor("graph.node.selected.background"));
-		setFocusFillColor(prefs.getColor("graph.node.selected.foreground"));
+		setFocusColor(prefs.getColor("graph.node.selected.foreground"));
+		setFocusFillColor(prefs.getColor("graph.node.selected.background"));
 		setHighlightFillColor(prefs.getColor("graph.node.hovered.background"));
 		setHighlightColor(prefs.getColor("graph.node.hovered.foreground"));
 		setEdgeFillColor(prefs.getColor("graph.node.foreground"));
 	}
-	
+
 	public void setEdgeFillColor(Color color) {
 		edgeFillColor = color;
 	}
-	
+
 	public Paint getColor(VisualItem item) {
-		Color color = (Color) item.getVizAttribute("color");
-		if (color != null) {
-			setDefaultColor(color);
+		if (item instanceof NodeItem) {
+			if (item.isFocus()) {
+				return focusColor;
+			}
+			else if (item.isHighlighted()) {
+				return highlightColor;
+			}
 		}
-		return super.getColor(item);
+		
+		return defaultColor;
 	}
-	
+
 	public Paint getFillColor(VisualItem item) {
 		Color color;
-		
-		if (item instanceof EdgeItem) {
+		if (item instanceof NodeItem) {
+			if (item.isFocus()) {
+				return focusFillColor;
+			}
+			else if (item.isHighlighted()) {
+				return highlightFillColor;
+			}
+			else {
+				GraphNode node = (GraphNode) item.getEntity();
+				color = node.getFillColor();
+				if (color != null) {
+					return color;
+				}
+			}
+		} 
+		else if (item instanceof EdgeItem) {
 			return edgeFillColor;
 		}
-	    
-		color = (Color) item.getFillColor();
-		
-		if (color != null) {
-			setDefaultFillColor(color);
-		}
-		
-		return super.getFillColor(item);
+
+		return defaultFillColor;
 	}
 }
