2a4878298453f54c48f8a68fd30be397e73ded1b
Taeke Kooiker <taeke@kooiker.info>
2005-08-03 11:16:34 +0000

    *** empty log message ***

--- error-gui/src/nl/cwi/sen1/error/viewer/ErrorViewer.java
+++ error-gui/src/nl/cwi/sen1/error/viewer/ErrorViewer.java
@@ -7,8 +7,10 @@ import javax.swing.tree.TreePath;
 
 import nl.cwi.sen1.error.model.ErrorNode;
 import nl.cwi.sen1.gui.Studio;
-import nl.cwi.sen1.gui.StudioComponentAdapter;
+import nl.cwi.sen1.gui.StudioComponentImpl;
+import nl.cwi.sen1.gui.StudioImplWithPredefinedLayout;
 import nl.cwi.sen1.gui.StudioPlugin;
+import nl.cwi.sen1.gui.StudioWithPredefinedLayout;
 import aterm.ATerm;
 import aterm.ATermFactory;
 import aterm.pure.PureFactory;
@@ -16,66 +18,68 @@ import errorapi.Factory;
 import errorapi.types.Summary;
 
 public class ErrorViewer implements ErrorViewerTif, StudioPlugin {
-	private static final String TOOL_NAME = "error-viewer";
+    private static final String TOOL_NAME = "error-viewer";
 
-	private Studio studio;
+    private Studio studio;
 
-	ErrorViewerBridge bridge;
+    ErrorViewerBridge bridge;
 
-	errorapi.Factory errorFactory;
+    errorapi.Factory errorFactory;
 
-	private ErrorPanel panel;
+    private ErrorPanel panel;
 
-	public String getName() {
-		return TOOL_NAME;
-	}
+    public String getName() {
+        return TOOL_NAME;
+    }
 
-	public void initStudioPlugin(Studio studio) {
-		this.studio = studio;
+    public void initStudioPlugin(Studio studio) {
+        this.studio = studio;
 
-		ATermFactory factory = studio.getATermFactory();
-		errorFactory = new Factory((PureFactory) factory);
+        ATermFactory factory = studio.getATermFactory();
+        errorFactory = Factory.getInstance((PureFactory) factory);
 
-		panel = new ErrorPanel();
-		addListener();
+        panel = new ErrorPanel();
+        addListener();
 
-		bridge = new ErrorViewerBridge(factory, this);
-		bridge.setLockObject(this);
-		studio.connect(getName(), bridge);
-		studio.addComponent(new StudioComponentAdapter("Error Viewer", panel));
-	}
+        bridge = new ErrorViewerBridge(factory, this);
+        bridge.setLockObject(this);
+        studio.connect(getName(), bridge);
+        ((StudioWithPredefinedLayout) studio).addComponent(
+                new StudioComponentImpl("Error Viewer", panel),
+                StudioImplWithPredefinedLayout.BOTTOM_RIGHT);
+    }
 
-	public ErrorViewer() {
-	}
+    public ErrorViewer() {
+    }
 
-	private void addListener() {
-		final JTree tree = panel.getTree();
-		tree.addTreeSelectionListener(new TreeSelectionListener() {
-			public void valueChanged(TreeSelectionEvent e) {
-				TreePath path = tree.getSelectionPath();
-				if (path != null) {
-					ErrorNode node = (ErrorNode) path.getLastPathComponent();
-					if (node != null) {
-						node.selected(studio, bridge);
-					}
-				}
-			}
+    private void addListener() {
+        final JTree tree = panel.getTree();
+        tree.addTreeSelectionListener(new TreeSelectionListener() {
+            public void valueChanged(TreeSelectionEvent e) {
+                TreePath path = tree.getSelectionPath();
+                if (path != null) {
+                    ErrorNode node = (ErrorNode) path.getLastPathComponent();
+                    if (node != null) {
+                        node.selected(studio, bridge);
+                    }
+                }
+            }
 
-		});
-	}
+        });
+    }
 
-	public void showFeedbackSummary(ATerm summaryTerm) {
-		Summary summary = errorFactory.SummaryFromTerm(summaryTerm);
-		panel.addError(summary);
-	}
+    public void showFeedbackSummary(ATerm summaryTerm) {
+        Summary summary = errorFactory.SummaryFromTerm(summaryTerm);
+        panel.addError(summary);
+    }
 
-	public void removeFeedbackSummary(String s0, String s1) {
+    public void removeFeedbackSummary(String s0, String s1) {
 
-	}
+    }
 
-	public void recAckEvent(ATerm t0) {
-	}
+    public void recAckEvent(ATerm t0) {
+    }
 
-	public void recTerminate(ATerm t0) {
-	}
+    public void recTerminate(ATerm t0) {
+    }
 }
