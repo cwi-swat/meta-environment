6f4eb4c85493752268ddccf6d66d17c05a772edb
Taeke Kooiker <taeke@kooiker.info>
2005-09-22 14:36:12 +0000

    * src/nl/cwi/sen1/modulemanager:    - Add ModuleGraph for conversion from internal database to Graph adt format    - Add support for creating graph labels

--- module-manager/src/nl/cwi/sen1/modulemanager/model/ModuleGraph.java
+++ module-manager/src/nl/cwi/sen1/modulemanager/model/ModuleGraph.java
@@ -19,34 +19,39 @@ import aterm.pure.PureFactory;
 
 public class ModuleGraph extends ModuleDatabase {
     private NodeList nodeList;
-    
+
     private EdgeList edgeList;
-    
+
     private Factory factory;
 
     public ModuleGraph(PureFactory pureFactory) {
         factory = Factory.getInstance(pureFactory);
-        nodeList = factory.makeNodeList();
-        edgeList = factory.makeEdgeList();
     }
 
     private void setNodes(ATerm namespace) {
         for (Iterator iter = modules.keySet().iterator(); iter.hasNext();) {
             ModuleId moduleId = (ModuleId) iter.next();
             NodeId nodeId = factory.makeNodeId_Default(moduleId.toTerm());
-            
+
             Module module = (Module) modules.get(moduleId);
-            AttributeTable table = module.getAttributes(namespace);
-            Map entries = table.getEntries();
-            
             AttributeList attrList = factory.makeAttributeList();
-            for (Iterator iterEntries = entries.keySet().iterator(); iterEntries.hasNext();) {
-                ATerm key = (ATerm) iterEntries.next();
-                ATerm value = (ATerm) entries.get(key);
-                
-                if (key.toString().equals("label")) {
-                    Attribute attr = factory.makeAttribute_Label(value.toString());
-                    attrList = attrList.insert(attr);
+
+            AttributeTable table = module.getAttributes(namespace);
+            if (table != null) {
+                Map entries = table.getEntries();
+
+                for (Iterator iterEntries = entries.keySet().iterator(); iterEntries
+                        .hasNext();) {
+                    ATerm key = (ATerm) iterEntries.next();
+                    ATerm value = (ATerm) entries.get(key);
+
+                    System.err.println("+++ Key: " + key + ", Value: " + value);
+                    
+                    if (key.equals(factory.getPureFactory().parse("\"label\""))) {
+                        Attribute attr = factory.makeAttribute_Label(value
+                                .toString());
+                        attrList = attrList.insert(attr);
+                    }
                 }
             }
             
@@ -54,7 +59,7 @@ public class ModuleGraph extends ModuleDatabase {
             nodeList = nodeList.insert(node);
         }
     }
-    
+
     private void setEdges() {
         for (Iterator iter = dependencies.keySet().iterator(); iter.hasNext();) {
             ModuleId moduleId = (ModuleId) iter.next();
@@ -65,7 +70,8 @@ public class ModuleGraph extends ModuleDatabase {
             for (Iterator depsIter = dependingModules.iterator(); depsIter
                     .hasNext();) {
                 ModuleId dependencyId = (ModuleId) depsIter.next();
-                NodeId nodeToId = factory.makeNodeId_Default(dependencyId.toTerm());
+                NodeId nodeToId = factory.makeNodeId_Default(dependencyId
+                        .toTerm());
 
                 Edge edge = factory.makeEdge_Default(nodeFromId, nodeToId,
                         factory.makeAttributeList());
@@ -76,10 +82,13 @@ public class ModuleGraph extends ModuleDatabase {
     }
 
     public Graph getModuleGraph(ATerm namespace) {
+        nodeList = factory.makeNodeList();
+        edgeList = factory.makeEdgeList();
+
         setNodes(namespace);
         setEdges();
         AttributeList attrList = factory.makeAttributeList();
-        
+
         return factory.makeGraph_Default(nodeList, edgeList, attrList);
     }
 }
