dd05f11a9d045c98c77ab95031cbbaf8826afdcb
Taeke Kooiker <taeke@kooiker.info>
2005-09-27 09:14:26 +0000

    * Removed import-graph from trunk in stead of module-manager branch:    - readded

---
+++ sdf-support/utils/import-graph/sdf/SdfImportGraphTif.java
@@ -0,0 +1,13 @@
+// Java tool interface SdfImportGraphTif
+// This file is generated automatically, please do not edit!
+// generation time: May 25, 2004 4:21:34 PM
+
+package sdf;
+
+import aterm.*;
+
+public interface SdfImportGraphTif
+{
+  public ATerm importRelationToGraph(ATerm t0);
+  public void recTerminate(ATerm t0);
+}
