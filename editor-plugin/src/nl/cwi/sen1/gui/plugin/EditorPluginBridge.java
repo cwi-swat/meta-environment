016d17a4696c5fa2e2d07af1e8526ae2722fff9d
Taeke Kooiker <taeke@kooiker.info>
2005-12-12 08:33:25 +0000

    *** empty log message ***

--- editor-plugin/src/nl/cwi/sen1/gui/plugin/EditorPluginBridge.java
+++ editor-plugin/src/nl/cwi/sen1/gui/plugin/EditorPluginBridge.java
@@ -1,6 +1,6 @@
 // Java tool bridge EditorPluginBridge
 // This file is generated automatically, please do not edit!
-// generation time: Dec 12, 2005 9:28:57 AM
+// generation time: Dec 12, 2005 9:33:01 AM
 
 package nl.cwi.sen1.gui.plugin;
 
@@ -138,12 +138,12 @@ public class EditorPluginBridge
   }
 
   //}}}
-  //{{{  public void editFile(ATerm t0, String s1)
+  //{{{  public void editFile(ATerm t0, String s1, String s2)
 
-  public void editFile(ATerm t0, String s1)
+  public void editFile(ATerm t0, String s1, String s2)
   {
     if (tool != null) {
-      tool.editFile(t0, s1);
+      tool.editFile(t0, s1, s2);
     }
     else {
       throw new UnsupportedOperationException("method `editFile' not supported.");
