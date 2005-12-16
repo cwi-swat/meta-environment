7dc0859c45c95e5f47e91acc1d15e703d7271c7d
Rob Economopoulos <rob.economopoulos@gmail.com>
2005-12-16 16:42:32 +0000

    * error-diff changed to accept summaries

--- error-support/utils/error-diff/error-diff.c
+++ error-support/utils/error-diff/error-diff.c
@@ -1,6 +1,5 @@
 /* $Id$ */
 
-/*{{{  includes */
 
 #include <stdio.h>
 #include <unistd.h>
@@ -10,34 +9,23 @@
 
 #include <Error-utils.h>
 
-/*}}}  */
-
-/*{{{  globals */
-
 static char myname[]    = "error-diff";
 static char myversion[] = "1.0";
-static char myarguments[] = "h1:2:V";
+static char myarguments[] = "hV";
 
-/*}}}  */
-
-/*{{{  void usage(void) */
-
-void usage(void)
-{
+void usage(void) {
     fprintf(stderr,
-        "Usage: %s -1 <file1> -2 <file2>\n"
+	"Info:  Compares two error summaries, ignoring specific path names\n"
+        "Usage: %s [<options>] <file1> <file2>\n"
         "Options:\n"
         "\t-h              display help information (usage)\n"
         "\t-V              reveal program version (i.e. %s)\n",
         myname, myversion);
 }
 
-/*}}}  */
 
-/*{{{  static ATbool ERR_subjectEqual(ERR_Subject subject1, ERR_Subject subject2) */
 
-static ATbool ERR_subjectEqual(ERR_Subject subject1, ERR_Subject subject2)
-{
+static ATbool ERR_subjectEqual(ERR_Subject subject1, ERR_Subject subject2) {
   char *description1 = ERR_getSubjectDescription(subject1);
   char *description2 = ERR_getSubjectDescription(subject2);
 
@@ -67,12 +55,8 @@ static ATbool ERR_subjectEqual(ERR_Subject subject1, ERR_Subject subject2)
   return ATfalse;
 }
 
-/*}}}  */
-/*{{{  static ATbool ERR_subjectListEqual(ERR_SubjectList subjectlist1,  */
 
-static ATbool ERR_subjectListEqual(ERR_SubjectList subjectlist1, 
-                                   ERR_SubjectList subjectlist2)
-{
+static ATbool ERR_subjectListEqual(ERR_SubjectList subjectlist1, ERR_SubjectList subjectlist2) {
   if (ERR_isSubjectListEmpty(subjectlist1)
       && ERR_isSubjectListEmpty(subjectlist2)) {
     return ATtrue;
@@ -91,9 +75,6 @@ static ATbool ERR_subjectListEqual(ERR_SubjectList subjectlist1,
   }
   return ATfalse;
 }
-
-/*}}}  */
-/*{{{  static ATbool ERR_errorEqual(ERR_Error error1, ERR_Error error2) */
 
 static ATbool ERR_errorEqual(ERR_Error error1, ERR_Error error2)
 {
@@ -130,48 +111,107 @@ static ATbool ERR_errorEqual(ERR_Error error1, ERR_Error error2)
   return ATfalse;
 }
 
-/*}}}  */
+static ATbool ERR_ErrorListEqual(ERR_ErrorList list1, ERR_ErrorList list2) {
 
-/*{{{  int main (int argc, char *argv[]) */
+	while (!ERR_isErrorListEmpty(list1) && !ERR_isErrorListEmpty(list2)) {
+		ERR_Error head1 = ERR_getErrorListHead(list1);
+		ERR_Error head2 = ERR_getErrorListHead(list2);
 
-int main (int argc, char *argv[])
-{
+		if (!ERR_errorEqual(head1, head2)) {
+			return ATfalse;
+		}
+
+		list1 = ERR_getErrorListTail(list1);
+		list2 = ERR_getErrorListTail(list2);
+	}
+
+	return ERR_isErrorListEmpty(list1) && ERR_isErrorListEmpty(list2);
+}
+
+static ATbool ERR_summaryEqual(ERR_Summary summary1, ERR_Summary summary2) {
+	char *id1 = ERR_getSummaryId(summary1);
+	char *id2 = ERR_getSummaryId(summary2);
+	char *producer1 = ERR_getSummaryProducer(summary1);
+	char *producer2 = ERR_getSummaryProducer(summary2);
+	ERR_ErrorList list1 = ERR_getSummaryList(summary1);
+	ERR_ErrorList list2 = ERR_getSummaryList(summary2);
+
+	if (strcmp(id1, id2)) {
+		return ATfalse;
+	}
+	else if (strcmp(producer1, producer2)) {
+		return ATfalse;
+	}
+	else if (ERR_ErrorListEqual(list1, list2)) {
+		return ATfalse;
+	}
+
+	return ATtrue;
+}
+
+
+
+static ATbool isTermSummary(ATerm term) {
+	AFun fun;
+
+	if (term == NULL) {
+					return ATfalse;
+	}
+
+	if (ATgetType(term) != AT_APPL) {
+					return ATfalse;
+	}
+
+	fun = ATgetAFun((ATermAppl) term);
+
+	if (strcmp(ATgetName(fun), "summary")) {
+					return ATfalse;
+	}
+
+	return ATtrue;
+}
+
+int main (int argc, char *argv[]) {
   int c;
   ATerm bottomOfStack;
   ATerm input1 = NULL;
   ATerm input2 = NULL;
-  char   *input_file_name1 = "-";
-  char   *input_file_name2 = "-";
+  char   *input_file_names[2] = { "-", "-" };
 
-  while ((c = getopt(argc, argv, myarguments)) != EOF)
+  while ((c = getopt(argc, argv, myarguments)) != EOF) {
     switch (c) {
       case 'h':  usage();                      exit(0);
-      case '1':  input_file_name1 = optarg;    break;
-      case '2':  input_file_name2 = optarg;    break;
       case 'V':  fprintf(stderr, "%s %s\n", myname, myversion);
                                                exit(0);
-      default :  usage();                      exit(1);
+      default :  break;
+    }
+  }
+
+  if (optind < argc) {
+	  input_file_names[0] = argv[optind];
+	  input_file_names[1] = argv[optind+1];
+  }
+  else {
+	  ATwarning("%s: need exactly two input files\n", myname);
+	  usage();
+	  exit(1);
   }
 
   ATinit(argc, argv, &bottomOfStack);
   initErrorApi();
 
-  
-  input1 = ATreadFromNamedFile(input_file_name1);
-  if (input1 == NULL) {
-	ATwarning("%s: no input files given\n", myname);
-        usage();
-	exit(1);
+  input1 = ATreadFromNamedFile(input_file_names[0]);
+  input2 = ATreadFromNamedFile(input_file_names[1]);
+
+  if (!isTermSummary(input1) || !isTermSummary(input2)) {
+	  ATwarning("%s: please provide two Summaries, and nothing else\n",
+			  myname);
+	  usage();
+	  exit(1);
   }
 
-  input2 = ATreadFromNamedFile(input_file_name2);
-  if (input2 == NULL) {
-	ATwarning("%s: no second input file given\n", myname);
-        usage();
-	exit(1);
-  }
-
-  if (ERR_errorEqual(ERR_ErrorFromTerm(input1), ERR_ErrorFromTerm(input2))) {
+  if (ERR_summaryEqual(ERR_SummaryFromTerm(input1), 
+			  ERR_SummaryFromTerm(input2))) {
     return 0;
   }
   else {
@@ -179,4 +219,3 @@ int main (int argc, char *argv[])
   }
 }
 
-/*}}}  */
