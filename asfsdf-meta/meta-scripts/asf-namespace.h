9ddec142e4a11ab020567c5d19f7adacc6559d9a
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2006-01-26 14:55:55 +0000

    * moved all asf specific stuff to asfsdf-meta

---
+++ asfsdf-meta/meta-scripts/asf-namespace.h
@@ -0,0 +1,8 @@
+#ifndef __ASF_NAMESPACE_H__
+#define __ASF_NAMESPACE_H__
+
+#define ASF_NAMESPACE "asf"
+#define ASF_EXTENSION ".asf"
+#define ASF_TOPSORT "ASF-Module"
+
+#endif /* __ASF_NAMESPACE_H__ */
