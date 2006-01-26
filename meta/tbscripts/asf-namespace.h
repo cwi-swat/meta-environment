f3835c8a6041705c03a558c8afbfc423e221e892
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2006-01-26 15:38:56 +0000

    *** empty log message ***

--- meta/tbscripts/asf-namespace.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ASF_NAMESPACE_H__
-#define __ASF_NAMESPACE_H__
-
-#define ASF_NAMESPACE "asf"
-#define ASF_EXTENSION ".asf"
-#define ASF_TOPSORT "ASF-Module"
-
-#endif /* __ASF_NAMESPACE_H__ */
