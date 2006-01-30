f5d93f4980915ed5b306fd2966a9090b69991ebd
Taeke Kooiker <taeke@kooiker.info>
2006-01-30 13:30:25 +0000

    * editors:    - Moved structure editor into its own package

--- meta/editors/structure/StructureEditor.pro
+++
@@ -1,4 +0,0 @@
-#include <MEPT.h>
-
-typedef PT_Tree SE_Tree;
-typedef PT_ParseTree SE_ParseTree;
--- meta/editors/structure/slice-tree.h
+++
@@ -1,8 +0,0 @@
-#ifndef SLICING_H
-#define SLICING_H
-
-#include <MEPT.h>
-
-ATermList TreeToSyntaxSlices(PT_Tree tree);
-
-#endif
