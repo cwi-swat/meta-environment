6c1c7c5e5e46db09f4c663daafffde2aadf798b1
Taeke Kooiker <taeke@kooiker.info>
2006-01-30 10:50:44 +0000

    *** empty log message ***

---
+++ module-details-gui/src/nl/cwi/sen1/gui/plugin/data/ModuledetailsTableModel.java
@@ -0,0 +1,56 @@
+package nl.cwi.sen1.gui.plugin.data;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+import javax.swing.table.AbstractTableModel;
+
+public class ModuledetailsTableModel extends AbstractTableModel {
+    private String[] columnNames;
+    private LinkedList data;
+    
+    
+    public ModuledetailsTableModel(String[] columnNames) {
+        this.columnNames = columnNames;
+        data = new LinkedList();
+    }
+    
+    public int getRowCount() {
+        return data.size();
+    }
+
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+
+    public String getColumnName(int col) {
+        return columnNames[col];
+    }
+    
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        String[] row = (String[]) data.get(rowIndex);
+        return row[columnIndex];
+    }
+    
+    public void setDetails(String namespace, String key, String value) {
+        String[] found = null;
+        
+        for (Iterator iter = data.iterator(); iter.hasNext();) {
+            String[] row = (String[]) iter.next();
+            if (row[0].equals(namespace) && row[1].equals(key)) {
+                found = row;
+            }
+        }
+        
+        if (found != null) {
+            found[2] = value;
+        } else {
+            found = new String[3];
+            found[0] = namespace;
+            found[1] = key;
+            found[2] = value;
+            data.add(found);
+        }
+        fireTableDataChanged();
+    }
+}
