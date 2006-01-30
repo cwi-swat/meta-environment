be6dc7f8f3e18d68dbee368d4aa5165d5751adca
Taeke Kooiker <taeke@kooiker.info>
2006-01-30 14:28:40 +0000

    * Initial version

---
+++ sdf-meta/tbscripts/sdf-namespace.h
@@ -0,0 +1,8 @@
+#ifndef __SDF_NAMESPACE_H__
+#define __SDF_NAMESPACE_H__
+
+#define SDF_NAMESPACE "sdf"
+#define SDF_EXTENSION ".sdf"
+#define SDF_TOPSORT "Module"
+
+#endif /* __SDF_NAMESPACE_H__ */
