23a1803f2a318ec8a064d57a9eeb5715216ae45a
Taeke Kooiker <taeke@kooiker.info>
2006-01-31 09:50:52 +0000

    * tbscripts/*.tb:    - Move SDF specific scripts to sdf-meta

---
+++ meta/tbscripts/scenarios/Cycle/gen.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+LIMIT=10
+
+for ((a=1; a <= LIMIT ; a++))
+do
+  echo "module a$a" > a$a.sdf
+  for ((b=1; b< LIMIT ; b++))
+  do
+    echo -n "imports " >> a$a.sdf
+    echo "a$b" >> a$a.sdf
+  done
+done
--- meta/tbscripts/sdf-namespace.h
+++
@@ -1,8 +0,0 @@
-#ifndef __SDF_NAMESPACE_H__
-#define __SDF_NAMESPACE_H__
-
-#define SDF_NAMESPACE "sdf"
-#define SDF_EXTENSION ".sdf"
-#define SDF_TOPSORT "Module"
-
-#endif /* __SDF_NAMESPACE_H__ */
