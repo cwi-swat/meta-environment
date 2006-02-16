f77b9b34e1f9321f1872f6a964ae7c7c69e6149b
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2006-02-16 17:15:22 +0000

    * parse-utils can toggle heuristics filters

--- meta/tbscripts/scenarios/Cycle/gen.sh
+++ meta/tbscripts/scenarios/Cycle/gen.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 
-LIMIT=10
+LIMIT=5
 
 for ((a=1; a <= LIMIT ; a++))
 do
   echo "module a$a" > a$a.sdf
-  for ((b=1; b< LIMIT ; b++))
+  for ((b=1; b<= LIMIT ; b++))
   do
     echo -n "imports " >> a$a.sdf
     echo "a$b" >> a$a.sdf
