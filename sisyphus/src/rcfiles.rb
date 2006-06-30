3efb0eff8b956f01e49040183c452137e63cae84
Tijs van der Storm <T.van.der.Storm@cwi.nl>
2006-06-30 12:33:52 +0000

    Moved rcfiles to Utils module.

--- sisyphus/src/rcfiles.rb
+++ sisyphus/src/rcfiles.rb
@@ -1,17 +1,21 @@
 
-module Roll
+module Utils
 
-  def Roll.rcfile(name)
-    home = ENV['HOME']
-    return File.join(home, name)
-  end
+  module Roll
 
-  def Roll.boot_roll_rcfile
-    return rcfile('.bootrollrc')
-  end
+    def Roll.rcfile(name)
+      home = ENV['HOME']
+      return File.join(home, name)
+    end
 
-  def Roll.roll_rcfile
-    return rcfile('.rollrc')
+    def Roll.boot_roll_rcfile
+      return rcfile('.bootrollrc')
+    end
+
+    def Roll.roll_rcfile
+      return rcfile('.rollrc')
+    end
+
   end
 
 end
