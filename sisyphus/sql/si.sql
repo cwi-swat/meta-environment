aa782c5035a1b32f526d81b667598b4fcc9a9ede
Tijs van der Storm <T.van.der.Storm@cwi.nl>
2006-07-25 13:02:46 +0000

    Added a view on revisions/components/builds combined.

--- sisyphus/sql/si.sql
+++ sisyphus/sql/si.sql
@@ -132,3 +132,23 @@ create table si_releases (
 	si_item_id integer references si_items,
 	version char(16)
 );
+
+
+create view si_builds (id, name, version, informative_version, si_item_id) as
+select 
+	si_items.id, 
+	si_components.name, 
+	si_revisions.version,
+	si_revisions.informative_version,
+	si_items.id
+from
+	si_items, 
+	si_components,
+	si_revisions
+where
+	si_revisions.si_component_id = si_components.id and
+	si_items.si_revision_id = si_revisions.id and
+	si_items.success = true
+;
+
+	
