e2afa33d6c505d14a7601a93686584d5cd595636
Tijs van der Storm <T.van.der.Storm@cwi.nl>
2006-07-28 09:57:20 +0000

    Renamed model to models.

---
+++ sisyphus-support/src/sisyphus-support.rb
@@ -0,0 +1,11 @@
+
+require 'models/component.rb'
+require 'models/build.rb'
+require 'models/designator.rb'
+require 'models/host.rb'
+require 'models/action.rb'
+require 'models/result.rb'
+require 'models/profile.rb'
+require 'models/repository.rb'
+require 'models/tree.rb'
+
