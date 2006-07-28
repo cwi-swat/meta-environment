d9377e6f17a6056862564df982bdf7c80251d410
Tijs van der Storm <T.van.der.Storm@cwi.nl>
2006-07-28 09:43:08 +0000

    Added roots to profile.

--- sisyphus-support/src/profile.rb
+++ sisyphus-support/src/profile.rb
@@ -2,6 +2,7 @@ require 'active_record'
 
 class Profile < ActiveRecord::Base
   has_and_belongs_to_many :repository
+  has_and_belongs_to_many :component # roots
   belongs_to :environment
   has_many :action
 end
