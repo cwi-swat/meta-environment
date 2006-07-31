7d42a3c00e5fb5a901161058fde9a78a77b2f657
Tijs van der Storm <T.van.der.Storm@cwi.nl>
2006-07-31 15:20:01 +0000

    Added some docs and overrode the doxygen make target.

--- sisyphus-support/src/sisyphus/support/action.rb
+++ sisyphus-support/src/sisyphus/support/action.rb
@@ -1,5 +1,10 @@
 require 'active_record'
 
+# = The +Action+ class
+# This class models build actions; they can be configured as part of a profile.
+# The name is used to identify a build action; the template should be a shell script
+# containing embedded ruby (erb). It is instantiated using +Contexts+
+
 class Action < ActiveRecord::Base
   belongs_to :profile
   acts_as_list :scope => :profile
