9d267fb7b59bd23785185c2c09977904f71fe06a
Tijs van der Storm <T.van.der.Storm@cwi.nl>
2006-07-31 11:36:55 +0000

    Functions for platforms.

--- sisyphus-support/src/sisyphus-model/host.rb
+++ sisyphus-support/src/sisyphus-model/host.rb
@@ -3,12 +3,11 @@ require 'active_record'
 class Host < ActiveRecord::Base
   belongs_to :platform
   validates_presence_of :name
-  validates_uniqueness_of :name
 
-  def self.find_by_name(name)
-    h = Host.find(:first, :conditions => ['name = ?', name])
+  def self.find_by_name_and_platform(name, platform)
+    h = Host.find(:first, :conditions => ['name = ? and platform_id = ?', name, platform])
     if not h then
-      h = Host.new(:name => name)
+      h = Host.new(:name => name, :platform => platform)
       h.save
     end
     return h
--- sisyphus-support/src/sisyphus-model/platform.rb
+++ sisyphus-support/src/sisyphus-model/platform.rb
@@ -8,5 +8,19 @@ class Platform < ActiveRecord::Base
   :hardware_platform, 
   :hardware_class, 
   :processor_type
+
+  def self.find_by_uname_elements(kernel_name, kernel_release, hardware_platform, 
+                             hardware_class, processor_type)
+    p = Platform.find(:first, :conditions => ['kernel_name = ? and kernel_release = ? and hardware_platform = ? and hardware_class = ? and processor_type = ?', 
+                                              kernel_name, kernel_release, hardware_platform, 
+                                              hardware_class, processor_type])
+    if not p then
+      p = Platform.new(:kernel_name => kernel_name, :kernel_release => kernel_release, 
+                       :hardware_platform => hardware_platform, :hardware_class => hardware_class,
+                       :processor_type => processor_type)
+      p.save
+      return p
+    end
+  end
   
 end
