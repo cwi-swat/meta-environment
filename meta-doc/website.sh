ba86168ef44218322531d75296c1d987a6dda37e
Paul Klint <Paul.Klint@cwi.nl>
2006-10-05 20:59:13 +0000

    - Fixed website.sh - Fixed some <title>s in docbook files to let getTitle work.

--- meta-doc/website.sh
+++ meta-doc/website.sh
@@ -5,6 +5,7 @@ set -e
 CATEGORIES="getting-started howto demos understanding courses project"
 WEB="./doc/html"
 DOCBOOKXSL="/ufs/sen1/software/installed/docbook-xsl-1.71.0/html/docbook.xsl"
+# DOCBOOKXSL="/home/paulk/software/source/docbook-xsl-1.71.0/html/docbook.xsl"
 
 mkdir -p ${WEB} || true
 
@@ -17,28 +18,31 @@ getTitle() {
 
 INDEX="${WEB}/index.html"
 echo "<html><head><title>" > ${INDEX}
-echo "  Meta-Environment Online Documentation" >> ${INDEX}
+echo " Online Documentation of The Meta-Environment" >> ${INDEX}
 echo "</title></head><body>" >> ${INDEX}
 
-for i in ${CATEGORIES}; do
-  echo "<h1>`cat $i/TITLE`</h1><ul>" >> ${INDEX}
-  BOOKS=`ls $i`
-  mkdir -p ${WEB}/$i || true
-  for b in ${BOOKS}; do
-    if [ -d $i/$b ]; then
-      cp $i/$b/*.{png,jpg,gif} ${WEB}/$i/$b >& /dev/null || true
-      if [ -f $i/$b/$b.xml ]; then
-	(xsltproc ${DOCBOOKXSL} --output ${WEB}/$i/$b $i/$b/$b.xml)	
-	title=`getTitle $i/$b/$b.xml`
-	echo "<li><a href=\"./$i/$b/$b.html\">${title} (html)</a></li>" >> ${INDEX}
-      elif [ -f $i/$b/$b.pdf ]; then
-	  mkdir -p ${WEB}/$i/$b
-	  cp $i/$b/$b.pdf ${WEB}/$i/$b/$b.pdf
-	  title=`cat $i/$b/TITLE`
-	  echo "<li><a href=\"./$i/$b/$b.pdf\">${title} (pdf)</a></li>" >> ${INDEX}
-      elif [ -f $i/$b/$b.url ]; then
-	  title=`cat $i/$b/TITLE`
-	  url=`cat $i/$b/$b.url`
+for cat in ${CATEGORIES}; do
+  echo "<h1>`cat $cat/TITLE`</h1><ul>" >> ${INDEX}
+  BOOKS=`ls $cat`
+  mkdir -p ${WEB}/$cat || true
+  for book in ${BOOKS}; do
+    if [ -d $cat/$book ]; then
+      if [ ! -d ${WEB}/$cat/$book ]; then
+         mkdir ${WEB}/$cat/$book
+      fi
+      cp $cat/$book/*.{png,jpg,gif} ${WEB}/$cat/$book >& /dev/null || true
+      if [ -f $cat/$book/$book.xml ]; then
+	(xsltproc  --output ${WEB}/$cat/$book/$book.html ${DOCBOOKXSL} $cat/$book/$book.xml)	
+	title=`getTitle $cat/$book/$book.xml`
+	echo "<li><a href=\"./$cat/$book/$book.html\">${title} (html)</a></li>" >> ${INDEX}
+      elif [ -f $cat/$book/$book.pdf ]; then
+	  mkdir -p ${WEB}/$cat/$book
+	  cp $cat/$book/$book.pdf ${WEB}/$cat/$book/$book.pdf
+	  title=`cat $cat/$book/TITLE`
+	  echo "<li><a href=\"./$cat/$book/$book.pdf\">${title} (pdf)</a></li>" >> ${INDEX}
+      elif [ -f $cat/$book/$book.url ]; then
+	  title=`cat $cat/$book/TITLE`
+	  url=`cat $cat/$book/$book.url`
 	  echo "<li><a href=\"${url}\">${title}</a></li>" >> ${INDEX}
       fi
     fi
