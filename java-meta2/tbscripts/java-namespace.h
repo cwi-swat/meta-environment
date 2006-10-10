bedfd860e6cfe76a972af49a90037e70b170f703
Davy Landman <Davy.Landman@cwi.nl>
2006-10-10 07:03:25 +0000

    Simulated removal of subversion repository

--- java-meta2/tbscripts/java-namespace.h
+++
@@ -1,8 +0,0 @@
-#ifndef __JAVA_NAMESPACE_H__
-#define __JAVA_NAMESPACE_H__
-
-#define JAVA_NAMESPACE "java"
-#define JAVA_EXTENSION ".java"
-#define JAVA_TOPSORT "CompilationUnit"
-
-#endif /* __JAVA_NAMESPACE_H__ */
