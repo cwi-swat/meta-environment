63121170d93e8ef3bc5f238bae08276b0a044650
Taeke Kooiker <taeke@kooiker.info>
2007-02-20 08:32:49 +0000

    Remove unused file

--- meta-studio/src/nl/cwi/sen1/gui/component/NameChangedEvent.java
+++
@@ -1,9 +0,0 @@
-package nl.cwi.sen1.gui.component;
-
-import java.util.EventObject;
-
-public class NameChangedEvent extends EventObject {
-    public NameChangedEvent(Object source) {
-        super(source);
-    }
-}
