ddc3e2b368330bd30ed20f490e49840d91d5223e
Taeke Kooiker <taeke@kooiker.info>
2007-02-23 09:45:58 +0000

    Generics

--- editor-plugin/src/nl/cwi/sen1/gui/plugin/EditorPluginTool.java
+++ editor-plugin/src/nl/cwi/sen1/gui/plugin/EditorPluginTool.java
@@ -111,7 +111,7 @@ abstract public class EditorPluginTool
   // The generic handler calls the specific handlers
   public ATerm handler(ATerm term)
   {
-    List result;
+    List<?> result;
 
     result = term.match(PgetContents0);
     if (result != null) {
