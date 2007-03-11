7eca86d786d7b140c367e5a19ed3ec8b3c9bc919
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2007-03-11 13:36:55 +0000

    generated ToolBus interface

--- svg-gui/src/nl/cwi/sen1/gui/plugin/SVGPlugin.java
+++ svg-gui/src/nl/cwi/sen1/gui/plugin/SVGPlugin.java
@@ -1,5 +1,30 @@
-package src.nl.cwi.sen1.gui.plugin;
+package nl.cwi.sen1.gui.plugin;
 
-public class SVGPlugin {
+import nl.cwi.sen1.gui.Studio;
+import aterm.ATerm;
+
+public class SVGPlugin extends DefaultStudioPlugin implements SvgTif {
+	private Studio studio;
+	private SvgBridge bridge;
+
+	public void display(String s0, String s1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void recTerminate(ATerm t0) {
+		// TODO Auto-generated method stub		
+	}
+
+	public String getName() {
+		return "SVG";
+	}
+
+	public void initStudioPlugin(Studio studio) {
+		this.studio = studio;
+		bridge = new SvgBridge(studio.getATermFactory(), this);
+		bridge.setLockObject(this);
+		studio.connect(getName(), bridge);
+	}
 
 }
---
+++ svg-gui/src/nl/cwi/sen1/gui/plugin/SvgTif.java
@@ -0,0 +1,13 @@
+// Java tool interface SvgTif
+// This file is generated automatically, please do not edit!
+// generation time: Mar 11, 2007 2:34:36 PM
+
+package nl.cwi.sen1.gui.plugin;
+
+import aterm.*;
+
+public interface SvgTif
+{
+  public void display(String s0, String s1);
+  public void recTerminate(ATerm t0);
+}
