3a40621e082f698e48d9475933dae7144e549ee9
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2007-03-12 10:01:39 +0000

    * debugging

--- svg-gui/src/nl/cwi/sen1/gui/plugin/SVGCanvas.java
+++ svg-gui/src/nl/cwi/sen1/gui/plugin/SVGCanvas.java
@@ -1,16 +1,32 @@
 package nl.cwi.sen1.gui.plugin;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+
 import javax.swing.JComponent;
 
 import nl.cwi.sen1.gui.component.StudioComponentImpl;
 
 import org.apache.batik.swing.JSVGCanvas;
+import org.apache.xerces.parsers.SAXParser;
 
 public class SVGCanvas extends StudioComponentImpl {
 	private JSVGCanvas svgCanvas = new JSVGCanvas();
 
 	public SVGCanvas(String name, String file) {
 		super(name, null);
+		ClassLoader sysClassLoader = ClassLoader.getSystemClassLoader();
+		sysClassLoader = SVGCanvas.class.getClassLoader();
+
+		sysClassLoader = JSVGCanvas.class.getClassLoader();
+		
+        //Get the URLs
+        URL[] urls = ((URLClassLoader)sysClassLoader).getURLs();
+
+        for(int i=0; i< urls.length; i++)
+        {
+            System.out.println(urls[i].getFile());
+        }       
 		svgCanvas.loadSVGDocument("file://" + file);
 	}
 
--- svg-gui/src/nl/cwi/sen1/gui/plugin/SVGPlugin.java
+++ svg-gui/src/nl/cwi/sen1/gui/plugin/SVGPlugin.java
@@ -1,5 +1,9 @@
 package nl.cwi.sen1.gui.plugin;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
 import nl.cwi.sen1.gui.Studio;
 import nl.cwi.sen1.gui.StudioImplWithPredefinedLayout;
 import aterm.ATerm;
@@ -28,5 +32,12 @@ public class SVGPlugin extends DefaultStudioPlugin implements SvgTif {
 		bridge.setLockObject(this);
 		studio.connect(getName(), bridge);
 	}
-
+	
+	public static void main(String[] args) {
+		SVGCanvas canvas = new SVGCanvas("Test", args[0]);
+		JFrame f = new JFrame();
+		f.setLayout(new BorderLayout());
+		f.add(canvas.getViewComponent(), BorderLayout.CENTER);
+		f.setVisible(true);
+	}
 }
