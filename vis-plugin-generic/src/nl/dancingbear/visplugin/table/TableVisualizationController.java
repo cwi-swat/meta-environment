486d72d7772372c913ea4d14a59ba2de4964fa7e
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2007-03-20 07:36:00 +0000

    Tue Mar 20 08:08:08 CET 2007 <jurgenv@cwi.nl>  * *: initial import from Dancing Bear project

---
+++ vis-plugin-generic/src/nl/dancingbear/visplugin/table/TableVisualizationController.java
@@ -0,0 +1,172 @@
+package nl.dancingbear.visplugin.table;
+
+import nl.cwi.sen1.relationstores.types.RType;
+import nl.cwi.sen1.relationstores.types.RTypeColumnTypes;
+import nl.cwi.sen1.relationstores.types.rtype.Bag;
+import nl.cwi.sen1.relationstores.types.rtype.Relation;
+import nl.cwi.sen1.relationstores.types.rtype.Set;
+import nl.dancingbear.visplugin.VisualizationPluginController;
+import nl.dancingbear.visplugin.VisualizationPluginWindow;
+import aterm.ATerm;
+
+/**
+ * The TableVisualizationController class
+ *
+ * @author Srinivasan Tharmarajah
+ * @author Wasim Alsaqaf
+ * @author Antoine Savelkoul
+ * @author Arend van Beelen
+ * @date 07-03-2007
+ */
+public class TableVisualizationController extends VisualizationPluginController {
+
+    private TableVisualizationWindow m_window;
+
+    /**
+     * Default constructor.
+     *
+     * @author Arend van Beelen
+     * @date 13-03-2007
+     */
+    public TableVisualizationController() {
+        super();
+
+        m_window = null;
+    }
+
+    /**
+     * Return Table visualization window
+     *
+     * @author Srinivasan Tharmarajah
+     * @author Wasim Alsaqaf
+     * @author Antoine Savelkoul (reviewer)
+     * @author Arend van Beelen (refactoring)
+     * @date 06-03-2007
+     */
+    public VisualizationPluginWindow createWindow() {
+        m_window = new TableVisualizationWindow();
+        return m_window;
+    }
+
+    /**
+     * Gets the plugin name [Table Visualization Plugin]
+     *
+     * @author Srinivasan Tharmarajah
+     * @author Wasim Alsaqaf
+     * @date 20-2-2007
+     */
+    public String getPluginName() {
+        return "Table Visualization Plugin";
+    }
+
+    /**
+     * Called when a file open dialog is accepted.
+     *
+     * @param windowId ID of the window that requested the export.
+     * @param filename Filename returned by the file open dialog.
+     *
+     * @author Arend van Beelen
+     * @date 13-03-2007
+     */
+    public void vpFileDialogResult(int windowId, String filename) {
+        TableVisualizationWindow window = (TableVisualizationWindow) getWindowById(windowId);
+
+        switch (getWindowExportRequestType(windowId)) {
+        case rStore:
+            window.exportToRStore(filename);
+            break;
+
+        default:
+            super.vpFileDialogResult(windowId, filename);
+        }
+    }
+
+    /**
+     * Define the supported types. Not implemented because we override the
+     * isTypeSupported() function.
+     *
+     * @author Srinivasan Tharmarajah
+     * @author Wasim Alsaqaf
+     * @author Antoine Savelkoul
+     * @author Anton Lycklama
+     * @author Bas Basten
+     * @date 09-03-2007
+     */
+    public ATerm[] getSupportedTypes() {
+        ATerm[] atermList = new ATerm[0];
+        return atermList;
+    }
+
+    /**
+     * Check if a type is supported
+     *
+     * @param type The type to check
+     * @author Anton Lycklama
+     * @author Bas Basten
+     * @date 09-03-2007
+     */
+    protected boolean isTypeSupported(ATerm type) {
+        RType rtype = m_factory.RTypeFromTerm(type);
+
+        boolean isBag = rtype.isBag();
+        boolean isRelation = rtype.isRelation();
+        boolean isSet = rtype.isSet();
+
+        if (isRelation) {
+            Relation relation = (Relation) rtype;
+            RTypeColumnTypes columnTypes = relation.getColumnTypes();
+
+            int numColumns = columnTypes.getLength();
+            if (numColumns > 0) {
+                for (int i = 0; i < numColumns; i++) {
+                    RType childType = columnTypes.getRTypeAt(i);
+                    // We don't support complex types.
+                    if (isComplexType(childType)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        // Maybe we can refactor this when we have more time.
+        // The set and the bag use the same code to determine
+        // if the type is supported or not.
+        else if (isSet) {
+            Set set = (Set) rtype;
+            RType elementType = set.getElementType();
+            // If the type is complex we don't support it,
+            // otherwise we do support it.
+            return !isComplexType(elementType);
+        } else if (isBag) {
+            Bag bag = (Bag) rtype;
+            RType elementType = bag.getElementType();
+            // If the type is complex we don't support it,
+            // otherwise we do support it.
+            return !isComplexType(elementType);
+        }
+
+        return false;
+    }
+    /**
+     * Returns if the given Rtype is either a bag, relation, set
+     * or tuple.
+     *
+     * @param rtype the Rtpe to check
+     * @return returns true if the given Rtype is complex
+     * @author Anton Lycklama
+     * @author Bas Basten
+     * @date 09-03-2007
+     */
+    private boolean isComplexType(RType rtype) {
+        boolean isBag = rtype.isBag();
+        boolean isRelation = rtype.isRelation();
+        boolean isSet = rtype.isSet();
+        boolean isTuple = rtype.isTuple();
+
+        if (isBag || isRelation || isSet || isTuple) {
+            return true;
+        }
+
+        return false;
+    }
+}
