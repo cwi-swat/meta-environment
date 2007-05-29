f710ebb8e5bba5988f1cc8d5defaeff5c36083f1
Arnold Lankamp <arnold.lankamp@gmail.com>
2007-05-29 13:48:16 +0000

    -Added make files and stuff.

--- prefuse-plugins/src/nl/dancingbear/visplugin/PluginLoader.java
+++ prefuse-plugins/src/nl/dancingbear/visplugin/PluginLoader.java
@@ -2,6 +2,7 @@ package nl.dancingbear.visplugin;
 
 import nl.cwi.sen1.gui.Studio;
 import nl.cwi.sen1.gui.plugin.DefaultStudioPlugin;
+import nl.cwi.sen1.gui.plugin.StudioPlugin;
 import nl.dancingbear.visplugin.barchartplugin.BarChartVisualizationController;
 import nl.dancingbear.visplugin.csvexport.CsvExportController;
 import nl.dancingbear.visplugin.graphplugin.GraphVisualizationController;
@@ -11,17 +12,31 @@ import nl.dancingbear.visplugin.scatterchartplugin.ScatterChartVisualizationCont
 import nl.dancingbear.visplugin.table.TableVisualizationController;
 import aterm.pure.PureFactory;
 
+/**
+ * This class loads all visualization plugins.
+ * 
+ * @author Arnold Lankamp
+ */
 public class PluginLoader extends DefaultStudioPlugin{
 	private final static String VISUALIZATIONPLUGIN = "Visualization plugin collection";
 	
+	/**
+	 * Default constructor.
+	 */
 	public PluginLoader(){
-		// Intentionally left empty.
+		super();
 	}
 	
+	/**
+	 * @see StudioPlugin#getName()
+	 */
 	public String getName(){
 		return VISUALIZATIONPLUGIN;
 	}
 
+	/**
+	 * @see StudioPlugin#initStudioPlugin(Studio)
+	 */
 	public void initStudioPlugin(Studio studio){
 		PureFactory pureFactory = (PureFactory) studio.getATermFactory();
 		
