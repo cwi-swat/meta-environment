fe304ed7e100b8ffd935942729ab5317f9cd604c
Taeke Kooiker <taeke@kooiker.info>
2007-08-20 12:36:48 +0000

    * src/**/ModuleTree.java: use overlay for creating error icons

--- navigator-gui/src/nl/cwi/sen1/gui/plugin/ModuleTree.java
+++ navigator-gui/src/nl/cwi/sen1/gui/plugin/ModuleTree.java
@@ -2,9 +2,14 @@ package nl.cwi.sen1.gui.plugin;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.Enumeration;
 
+import javax.imageio.ImageIO;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
@@ -48,12 +53,15 @@ public class ModuleTree extends JPanel {
 		ImageIcon folderIcon = new ImageIcon(getClass().getResource(
 				"/resources/images/folder.png"));
 		ImageIcon documentIcon = new ImageIcon(getClass().getResource(
-		"/resources/images/document.png"));
-		ImageIcon errorFolderIcon = new ImageIcon(getClass().getResource(
-		"/resources/images/folder-error.png"));
-		ImageIcon errorDocumentIcon = new ImageIcon(getClass().getResource(
-		"/resources/images/document-error.png"));
-		tree.setCellRenderer(new TreeIconCellRenderer(folderIcon, documentIcon, errorFolderIcon, errorDocumentIcon));
+				"/resources/images/document.png"));
+		ImageIcon errorOverlayIcon = new ImageIcon(getClass().getResource(
+				"/resources/images/error-overlay.png"));
+
+		ImageIcon errorFolderIcon = createOverlayIcon(folderIcon, errorOverlayIcon);
+		ImageIcon errorDocumentIcon = createOverlayIcon(documentIcon, errorOverlayIcon);
+
+		tree.setCellRenderer(new TreeIconCellRenderer(folderIcon, documentIcon,
+				errorFolderIcon, errorDocumentIcon));
 
 		add(new JScrollPane(tree));
 
@@ -68,6 +76,18 @@ public class ModuleTree extends JPanel {
 				}
 			}
 		});
+	}
+
+	private ImageIcon createOverlayIcon(ImageIcon icon, ImageIcon overlay) {
+		Image image = icon.getImage();
+		BufferedImage bufferedImage = new BufferedImage(image.getWidth(null),
+				image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+
+		Graphics2D g = bufferedImage.createGraphics();
+		g.drawImage(image, null, null);
+		g.drawImage(overlay.getImage(), 0, 0, null);
+		g.dispose();
+		return new ImageIcon(bufferedImage);
 	}
 
 	public void saveExpansionState() {
@@ -110,53 +130,51 @@ public class ModuleTree extends JPanel {
 	public void addMouseListener(MouseListener l) {
 		tree.addMouseListener(l);
 	}
-	
+
 	/**
-     * Internal class responsible for painting the icons on the nodes in the
-     * Tree. For different types of nodes different icons will be used
-     * 
-     * @author Renze de Vries
-     * @date 14-02-2007
-     */
-    class TreeIconCellRenderer extends DefaultTreeCellRenderer {
-        private Icon folderIcon;
-        private Icon documentIcon;
-        private Icon errorFolderIcon;
-        private Icon errorDocumentIcon;
+	 * Internal class responsible for painting the icons on the nodes in the
+	 * Tree. For different types of nodes different icons will be used
+	 * 
+	 * @author Renze de Vries
+	 * @date 14-02-2007
+	 */
+	class TreeIconCellRenderer extends DefaultTreeCellRenderer {
+		private Icon folderIcon;
+		private Icon documentIcon;
+		private Icon errorFolderIcon;
+		private Icon errorDocumentIcon;
 
-        public TreeIconCellRenderer(Icon folderIcon, Icon documentIcon, Icon errorFolderIcon, Icon errorDocumentIcon) {
-        	this.folderIcon = folderIcon;
-        	this.documentIcon = documentIcon;
-        	this.errorFolderIcon = errorFolderIcon;
-        	this.errorDocumentIcon = errorDocumentIcon;
-        }
+		public TreeIconCellRenderer(Icon folderIcon, Icon documentIcon,
+				Icon errorFolderIcon, Icon errorDocumentIcon) {
+			this.folderIcon = folderIcon;
+			this.documentIcon = documentIcon;
+			this.errorFolderIcon = errorFolderIcon;
+			this.errorDocumentIcon = errorDocumentIcon;
+		}
 
-        public Component getTreeCellRendererComponent(JTree tree, Object value,
-                boolean sel, boolean expanded, boolean leaf, int row,
-                boolean hasFocus) {
+		public Component getTreeCellRendererComponent(JTree tree, Object value,
+				boolean sel, boolean expanded, boolean leaf, int row,
+				boolean hasFocus) {
 
-            super.getTreeCellRendererComponent(tree, value, sel, expanded,
-                    leaf, row, hasFocus);
+			super.getTreeCellRendererComponent(tree, value, sel, expanded,
+					leaf, row, hasFocus);
 
-            ModuleTreeNode treeNode = (ModuleTreeNode) value;
-            if (treeNode.hasError() == true) {
-            	if (treeNode.isLeaf() == true) {
-            		setIcon(errorDocumentIcon);
-            	}
-            	else {
-            		setIcon(errorFolderIcon);
-            	}
-            }
-            else {
-            	if (treeNode.isLeaf() == true) {
-            		setIcon(documentIcon);
-            	}
-            	else {
-            		setIcon(folderIcon);
-            	}
-            }
+			ModuleTreeNode treeNode = (ModuleTreeNode) value;
+			if (treeNode.hasError() == true) {
+				if (treeNode.isLeaf() == true) {
+					setIcon(errorDocumentIcon);
+				} else {
+					setIcon(errorFolderIcon);
+				}
+			} else {
+				if (treeNode.isLeaf() == true) {
+					setIcon(documentIcon);
+				} else {
+					setIcon(folderIcon);
+				}
+			}
 
-            return this;
-        }
-    }
+			return this;
+		}
+	}
 }
