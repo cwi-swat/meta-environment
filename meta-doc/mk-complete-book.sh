4db8855216555bb1bd4516ecca7118e0d00311da
Paul Klint <Paul.Klint@cwi.nl>
2007-10-12 20:42:13 +0000

    Forgotten file.

---
+++ meta-doc/mk-complete-book.sh
@@ -0,0 +1,22 @@
+
+#! /bin/sh
+
+set -e
+
+CATEGORIES="learning-about howto understanding courses project"
+WEB="./doc/html"
+# FOPPREFIX="/home/paulk/software/source/fop-0.93"
+DOCBOOKXSLPREFIX="/home/paulk/software/source/docbook-xsl-1.71.0"
+FOP="${FOPPREFIX}/fop"
+DOCBOOKXSLHTML=${DOCBOOKXSLPREFIX}/html/docbook.xsl
+DOCBOOKXSLFO=${DOCBOOKXSLPREFIX}/fo/docbook.xsl
+STYLESHEET="meta-doc-style.css"
+
+	(xsltproc  --xinclude \
+                   --output book.fo \
+                   --stringparam icurrent.docid complete-book \
+                   fo-customization.xsl \
+                   complete-book.xml)
+        ${FOP} -fo book.fo -pdf book.pdf
+
+ 
