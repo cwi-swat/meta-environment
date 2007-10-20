7cd9e7de4afdf7954aaa62aaae89f532ddc1d6fd
Paul Klint <Paul.Klint@cwi.nl>
2007-10-20 20:28:31 +0000

    Moving ...

--- meta-doc/website.sh
+++ meta-doc/website.sh
@@ -11,7 +11,7 @@
 set -e
 
 # CATEGORIES="learning-about howto understanding courses project"
-CATEGORIES="getting-started syntax extraction-transformation"
+CATEGORIES="getting-started syntax  extraction-transformation formatting analysis"
 WEB="./doc/html"
 FOP="${FOPPREFIX}/fop"
 DOCBOOKXSLHTML=${DOCBOOKXSLPREFIX}/html/docbook.xsl
