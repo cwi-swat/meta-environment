9c97d28700bad49526309ec7853dd0a3d91b96e5
Paul Klint <Paul.Klint@cwi.nl>
2007-10-20 21:44:48 +0000

    Moving ...

--- meta-doc/website.sh
+++ meta-doc/website.sh
@@ -54,51 +54,55 @@ for cat in ${CATEGORIES}; do
   echo "<ul>" >> ${INDEX}
   BOOKS=`ls $cat`
   mkdir -p ${WEB}/$cat || true
-  for book in ${BOOKS}; do
+  for obook in ${BOOKS}; do
+    if [ $obook == TITLE ]; then
+       continue
+    fi
+    book=`echo $obook | sed "s@^[0-9]*-@@"`
     echo --- processing $book ---
-    if [ -d $cat/$book ]; then
-      mkdir -p ${WEB}/$cat/$book
-      cp $cat/$book/*.{png,jpg,gif} ${WEB}/$cat/$book >& /dev/null || true
-      if [ -f $cat/$book/$book.xml ]; then
-	pubdate=`getPubdate ${cat}/${book}/${book}.xml`
-	# svn log --xml ${cat}/${book}/${book}.xml  | \
-	# xsltproc  --output ${WEB}/$cat/$book/$book.log.html ./svnlog2html.xsl -
-	(xsltproc --xinclude --stringparam now "${pubdate}"  ./pubdate.xsl $cat/$book/$book.xml | \
-	 xsltproc  --output ${WEB}/$cat/$book/$book.html \
+    if [ -d $cat/$obook ]; then
+      mkdir -p ${WEB}/$cat/$obook
+      cp $cat/$obook/*.{png,jpg,gif} ${WEB}/$cat/$obook >& /dev/null || true
+      if [ -f $cat/$obook/$book.xml ]; then
+	pubdate=`getPubdate ${cat}/${obook}/${book}.xml`
+	# svn log --xml ${cat}/${obook}/${book}.xml  | \
+	# xsltproc  --output ${WEB}/$cat/$obook/$book.log.html ./svnlog2html.xsl -
+	(xsltproc --xinclude --stringparam now "${pubdate}"  ./pubdate.xsl $cat/$obook/$book.xml | \
+	 xsltproc  --output ${WEB}/$cat/$obook/$book.html \
 		   --xinclude \
                    --stringparam current.docid $book \
                    ./html-customization.xsl  -)
-	(xsltproc  --xinclude --stringparam now "${pubdate}" ./pubdate.xsl $cat/$book/$book.xml | \
-	 xsltproc  --output ${WEB}/$cat/$book/$book.fo \
+	(xsltproc  --xinclude --stringparam now "${pubdate}" ./pubdate.xsl $cat/$obook/$book.xml | \
+	 xsltproc  --output ${WEB}/$cat/$obook/$book.fo \
                    --xinclude \
                    --stringparam current.docid $book \
                    ./fo-customization.xsl -)
-        ${FOP} -fo ${WEB}/$cat/$book/$book.fo -pdf ${WEB}/$cat/$book/$book.pdf
-        rm ${WEB}/$cat/$book/$book.fo
+        ${FOP} -fo ${WEB}/$cat/$obook/$book.fo -pdf ${WEB}/$cat/$obook/$book.pdf
+        rm ${WEB}/$cat/$obook/$book.fo
         
-	title=`getTitle $cat/$book/$book.xml`
+	title=`getTitle $cat/$obook/$book.xml`
 
 	echo "<li> ${title}" >> ${INDEX}
-        echo "(<a href=\"./$cat/$book/$book.html\">html</a>,"  >> ${INDEX}
-	echo "<a href=\"./$cat/$book/$book.pdf\">pdf</a>" >> ${INDEX}
-	echo ",<a href=\"./$cat/$book/$book.log.html\">changes</a>)" >> ${INDEX}
+        echo "(<a href=\"./$cat/$obook/$book.html\">html</a>,"  >> ${INDEX}
+	echo "<a href=\"./$cat/$obook/$book.pdf\">pdf</a>" >> ${INDEX}
+	echo ",<a href=\"./$cat/$obook/$book.log.html\">changes</a>)" >> ${INDEX}
         echo "</li>" >> ${INDEX}
 
-        cp ${STYLESHEET} ${WEB}/$cat/$book/${STYLESHEET}
-      elif [ -f $cat/$book/$book.pdf ]; then
-	  mkdir -p ${WEB}/$cat/$book
-	  cp $cat/$book/$book.pdf ${WEB}/$cat/$book/$book.pdf
-	  title=`cat $cat/$book/TITLE`
-	  echo "<li>${title} (<a href=\"./$cat/$book/$book.pdf\">pdf</a>)</li>" >> ${INDEX}
-      elif [ -f $cat/$book/$book.swf ]; then
-          mkdir -p ${WEB}/$cat/$book
-          cp $cat/$book/$book.swf ${WEB}/$cat/$book/$book.swf
-          cp $cat/$book/$book.htm ${WEB}/$cat/$book/$book.htm
-          title=`cat $cat/$book/TITLE`
-          echo "<li>${title} (<a href=\"./$cat/$book/$book.htm\">flash</a>)</li>" >> ${INDEX}
-      elif [ -f $cat/$book/$book.url ]; then
-	  title=`cat $cat/$book/TITLE`
-	  url=`cat $cat/$book/$book.url`
+        cp ${STYLESHEET} ${WEB}/$cat/$obook/${STYLESHEET}
+      elif [ -f $cat/$obook/$book.pdf ]; then
+	  mkdir -p ${WEB}/$cat/$obook
+	  cp $cat/$obook/$book.pdf ${WEB}/$cat/$obook/$book.pdf
+	  title=`cat $cat/$obook/TITLE`
+	  echo "<li>${title} (<a href=\"./$cat/$obook/$book.pdf\">pdf</a>)</li>" >> ${INDEX}
+      elif [ -f $cat/$obook/$book.swf ]; then
+          mkdir -p ${WEB}/$cat/$obook
+          cp $cat/$obook/$book.swf ${WEB}/$cat/$book/$book.swf
+          cp $cat/$obook/$book.htm ${WEB}/$cat/$book/$book.htm
+          title=`cat $cat/$obook/TITLE`
+          echo "<li>${title} (<a href=\"./$cat/$obook/$book.htm\">flash</a>)</li>" >> ${INDEX}
+      elif [ -f $cat/$obook/$book.url ]; then
+	  title=`cat $cat/$obook/TITLE`
+	  url=`cat $cat/$obook/$book.url`
 	  echo "<li><a href=\"${url}\">${title}</a></li>" >> ${INDEX}
       fi
     fi
