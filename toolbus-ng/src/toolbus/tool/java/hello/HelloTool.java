6fe0847052745a7e1f605b18e81702c20e0d8ab6
Paul Klint <Paul.Klint@cwi.nl>
2007-11-24 22:39:02 +0000

    Added the Java hello example

---
+++ toolbus-ng/src/toolbus/tool/java/hello/HelloTool.java
@@ -0,0 +1,36 @@
+package toolbus.tool.java.hello;
+
+import toolbus.adapter.AbstractTool;
+import aterm.ATerm;
+import aterm.ATermFactory;
+
+public class HelloTool extends AbstractTool
+{
+  public HelloTool(String[] args){
+    super();
+    try{
+		connect(args);
+	}catch(Exception ex){
+		throw new RuntimeException(ex);
+	}
+  }
+
+  protected ATerm getText() 
+  {
+	  ATermFactory factory = getFactory();
+	  return factory.make("text(<str>)", "Hello world in Java!\n");
+  }
+
+	public void receiveAckEvent(ATerm aTerm){
+		// Left blank intentionally.
+	}
+	
+	public void receiveTerminate(ATerm msg){
+		   System.out.print("rec-terminate received: " + msg);
+	}
+  
+  public static void main(String[] args)
+  {
+    new HelloTool(args);
+  }
+}
