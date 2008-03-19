700ad099eb1c767e52a73e004eb0707c7bd92185
Arnold Lankamp <arnold.lankamp@gmail.com>
2008-03-19 09:17:55 +0000

    -Removed deprecated stuff, since we no longer need the old version of gentifs (which depended on them).

--- toolbuslib/src/atb-tool.h
+++ toolbuslib/src/atb-tool.h
@@ -8,14 +8,14 @@
 typedef ATerm (*ATBhandler)(int file_desc, ATerm input_term);
 
 /* ToolBus tool functions */
-int   ATBinitialize(int argc, char *argv[]);
+int    ATBinitialize(int argc, char *argv[]);
 int    ATBinit(int argc, char *argv[], ATerm *stack_bottom);
 int    ATBconnect(char *toolname, char *host, int port, ATBhandler h);
 void   ATBdisconnect(int file_desc);
 int    ATBeventloop(void);
 
-ATerm ATBpack(ATerm t);
-ATerm ATBunpack(ATerm t);
+ATerm  ATBpack(ATerm t);
+ATerm  ATBunpack(ATerm t);
 
 typedef struct _OperationTermPair
 {
@@ -23,15 +23,12 @@ typedef struct _OperationTermPair
 	ATerm term;
 } *OperationTermPair;
 
-ATerm  ATBreadTerm(int file_desc);
-int    ATBwriteTerm(int file_desc, ATerm term);
 void   ATBpostEvent(int file_desc, ATerm event);
 
 ATbool ATBpeekOne(int file_desc);
 int    ATBpeekAny(void);
 int    ATBhandleOne(int file_desc);
 int    ATBhandleAny(void);
-
 
 ATbool ATBisValidConnection(int cid);
 int    ATBgetPort(int cid);
@@ -42,14 +39,6 @@ ATerm ATBcheckSignature(ATerm signature, char *sigs[], int nrsigs);
 
 #ifndef streq
 #  define streq(s,t)	(!(strcmp(s,t)))
-#endif
-
-#ifndef MIN
-#  define MIN(a,b)	((a)<(b) ? (a) : (b))
-#endif
-
-#ifndef MAX
-#  define MAX(a,b)	((a)>(b) ? (a) : (b))
 #endif
 
 #endif /* ATB_TOOL_H */
