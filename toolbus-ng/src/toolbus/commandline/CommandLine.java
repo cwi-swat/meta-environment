f342ed5d767c41815cd4fe0bbbc0f6c7f9897c13
Arnold Lankamp <arnold.lankamp@gmail.com>
2008-07-10 09:34:39 +0000

    -Updated a command. -Updated the help message.

--- toolbus-ng/src/toolbus/commandline/CommandLine.java
+++ toolbus-ng/src/toolbus/commandline/CommandLine.java
@@ -56,7 +56,7 @@ public class CommandLine{
 						toolbus.getToolInstanceManager().printQueueTerms();
 					}else if(line.equals("dump toolbus state")){
 						toolbus.showStatus();
-					}else if(line.equals("dump partnerless senders")){
+					}else if(line.equals("dump partnerless communication atoms")){
 						toolbus.getMatchStore().printPartnerlessCommunicationAtoms();
 					}else if(line.equals("shutdown")){
 						toolbus.shutdown(toolbus.getTBTermFactory().makeList());
@@ -66,6 +66,7 @@ public class CommandLine{
 						System.err.println("dump tools status: Prints a listing of currently connected tools with their corresponding state.");
 						System.err.println("dump queued messages: Prints all (currently) queued values and events.");
 						System.err.println("dump toolbus state: Dumps all available information about the current state of the process logic to the command line.");
+						System.err.println("dump partnerless communication atoms: Prints a list of all communication atoms of which we can guarantee that they can never be executed.");
 						System.err.println("shutdown: Initiates the forcefull termination of the ToolBus.");
 					}else if(line.equals("")){
 						// Ignore this.
