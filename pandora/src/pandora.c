11b74e6f77fa7fdecb9f685bb34b1b2518a0ba73
Arnold Lankamp <arnold.lankamp@gmail.com>
2008-07-11 11:29:13 +0000

    -SVN:ignore -Update generated files

--- pandora/src/pandora.c
+++ pandora/src/pandora.c
@@ -8,8 +8,8 @@
 #define NR_SIG_ENTRIES	2
 
 static char *signature[NR_SIG_ENTRIES] = {
-  "rec-eval(<pandora>,pretty-print(<term>))",
   "rec-terminate(<pandora>,<term>)",
+  "rec-eval(<pandora>,pretty-print(<term>))",
 };
 
 /* Event handler for tool 'pandora' */
@@ -19,12 +19,12 @@ ATerm pandora_handler(int conn, ATerm term)
   /* We need some temporary variables during matching */
   ATerm t0;
 
-  if(ATmatch(term, "rec-eval(pretty-print(<term>))", &t0)) {
-    return pretty_print(conn, t0);
-  }
   if(ATmatch(term, "rec-terminate(<term>)", &t0)) {
     rec_terminate(conn, t0);
     return NULL;
+  }
+  if(ATmatch(term, "rec-eval(pretty-print(<term>))", &t0)) {
+    return pretty_print(conn, t0);
   }
   if(ATmatch(term, "rec-do(signature(<term>,<term>))", &in, &out)) {
     ATerm result = pandora_checker(conn, in);
--- pandora/src/pandora.h
+++ pandora/src/pandora.h
@@ -9,8 +9,8 @@
 #include <atb-tool.h>
 
 /* Prototypes for functions called from the event handler */
-ATerm pretty_print(int conn, ATerm);
 void rec_terminate(int conn, ATerm);
+ATerm pretty_print(int conn, ATerm);
 extern ATerm pandora_handler(int conn, ATerm term);
 extern ATerm pandora_checker(int conn, ATerm sigs);
 
